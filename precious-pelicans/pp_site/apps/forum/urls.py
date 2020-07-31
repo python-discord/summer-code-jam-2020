b77302c15b24aae9e8d59794fbec95ac0c5c3bf4
Morris Li <li081197@gmail.com>
2020-07-31 15:00:16 -0700

    Implemented forum backend

--- precious-pelicans/pp_site/apps/forum/urls.py
+++ precious-pelicans/pp_site/apps/forum/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from . import views
 
 urlpatterns = [
-
+    path('<int:PostId>/', views.post, name='viewpost'),
+    path('', views.index, name='home')
 ]
