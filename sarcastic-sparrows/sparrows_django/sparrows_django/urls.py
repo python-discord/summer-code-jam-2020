85e0eb8a68cc370debd1ec7b5a09f0ba07e84324
ryan cox <ryancox101@gmail.com>
2020-07-31 17:50:12 -0400

    re-creating django project because i messed up

--- sarcastic-sparrows/sparrows_django/sparrows_django/urls.py
+++ sarcastic-sparrows/sparrows_django/sparrows_django/urls.py
@@ -1,4 +1,4 @@
-"""sparrows URL Configuration
+"""sparrows_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- sarcastic-sparrows/sparrows_django/sparrows_django/wsgi.py
+++ sarcastic-sparrows/sparrows_django/sparrows_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sparrows project.
+WSGI config for sparrows_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sparrows.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sparrows_django.settings')
 
 application = get_wsgi_application()
