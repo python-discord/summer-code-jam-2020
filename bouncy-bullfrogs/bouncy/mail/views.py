f6a022c9408f53cb1a14b74c654d2c3ffabf1928
Arth Tyagi <arthtyagi7@gmail.com>
2020-07-31 23:26:53 +0530

    added compose,email and inbox view

--- bouncy-bullfrogs/bouncy/mail/views.py
+++ bouncy-bullfrogs/bouncy/mail/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from .models import Email, User
 from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -8,3 +15,99 @@ def index(request):
         return render(request, "mail/inbox.html")
     else:
         return HttpResponseRedirect(reverse("login"))
+
+
+@csrf_exempt
+@login_required
+def compose(request):
+
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    data = json.loads(request.body)
+    emails = [email.strip() for email in data.get("recipients").split(",")]
+    if emails == [""]:
+        return JsonResponse({
+            "error": "At least one recipient required."
+        }, status=400)
+
+    recipients = []
+    for email in emails:
+        try:
+            user = User.objects.get(email=email)
+            recipients.append(user)
+        except User.DoesNotExist:
+            return JsonResponse({
+                "error": f"User with email {email} does not exist."
+            }, status=400)
+
+    subject = data.get("subject", "")
+    body = data.get("body", "")
+
+    users = set()
+    users.add(request.user)
+    users.update(recipients)
+    for user in users:
+        email = Email(
+            user=user,
+            sender=request.user,
+            subject=subject,
+            body=body,
+            read=user == request.user
+        )
+        email.save()
+        for recipient in recipients:
+            email.recipients.add(recipient)
+        email.save()
+
+    return JsonResponse({"message": "Email sent successfully."}, status=201)
+
+
+@login_required
+def mailbox(request, mailbox):
+
+    if mailbox == "inbox":
+        emails = Email.objects.filter(
+            user=request.user, recipients=request.user, archived=False
+        )
+    elif mailbox == "sent":
+        emails = Email.objects.filter(
+            user=request.user, sender=request.user
+        )
+    elif mailbox == "archive":
+        emails = Email.objects.filter(
+            user=request.user, recipients=request.user, archived=True
+        )
+    else:
+        return JsonResponse({"error": "Invalid mailbox."}, status=400)
+
+    emails = emails.order_by("-timestamp").all()
+    return JsonResponse([email.serialize() for email in emails], safe=False)
+
+# for searching, returning and doing other stuff with mail like archive
+
+
+@csrf_exempt
+@login_required
+def email(request, email_id):
+    try:
+        email = Email.objects.get(user=request.user, pk=email_id)
+    except Email.DoesNotExist:
+        return JsonResponse({"error": "Email not found."}, status=404)
+
+    if request.method == "GET":
+        return JsonResponse(email.serialize())
+
+    elif request.method == "PUT":
+        data = json.loads(request.body)
+        if data.get("read") is not None:
+            email.read = data["read"]
+        if data.get("archived") is not None:
+            email.archived = data["archived"]
+        email.save()
+        return HttpResponse(status=204)
+
+    else:
+        return JsonResponse({
+            "error": "GET or PUT request required."
+        }, status=400)
