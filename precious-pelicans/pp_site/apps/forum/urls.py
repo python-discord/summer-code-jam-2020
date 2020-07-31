480f86ef9c42f5d530b235a8cd4963a3fe9ac82b
Morris Li <li081197@gmail.com>
2020-07-31 16:12:57 -0700

    changes to comply with the linter

--- precious-pelicans/pp_site/apps/forum/urls.py
+++ precious-pelicans/pp_site/apps/forum/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
+
 
 urlpatterns = [
     path('<int:PostId>/', views.post, name='viewpost'),
--- precious-pelicans/pp_site/apps/forum/views.py
+++ precious-pelicans/pp_site/apps/forum/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-from .models import *
+from .models import ForumPostOriginal
+
 
 def post(request, PostId):
+
     post = ForumPostOriginal.objects.get(id=PostId)
     media = post.content
     replyList = post.forumpostreply_set.all()
     context = {
         'originalPost': post,
-        'replies' : replyList,
-        'media' : media
+        'replies': replyList,
+        'media': media
     }
     return render(request, 'forum/post-template.html', context)
+
 
 def index(request):
     postList = ForumPostOriginal.objects.all()
     context = {
-        'postList' : postList
+        'postList': postList
     }
     return render(request, 'forum/index.html', context)
