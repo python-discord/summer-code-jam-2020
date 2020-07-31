8803efe0a19ea58e6fef1007e4999deeb63a9fcb
Colin <ccwbassett@gmail.com>
2020-07-31 12:34:58 -0700

    initial app setup, basic FBV routes

--- melodic-manatees/early_internet/early_internet/settings.py
+++ melodic-manatees/early_internet/early_internet/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
