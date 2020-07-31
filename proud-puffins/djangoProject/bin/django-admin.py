c9d8731758fae8a8c688537ddaf2d7629f00ec7b
BruceBarrett <bruceb@earthreflections.com>
2020-07-31 12:35:19 -0700

    Initial creation of Django structure. App name = early dating.

---
+++ proud-puffins/djangoProject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bruce/dev/git/summer-code-jam-2020/proud-puffins/djangoProject/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
