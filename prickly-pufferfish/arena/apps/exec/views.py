d0ae6baf5251b213bb8ec59bb186a48c478dc2f2
Turreted <gideonmitchell01@gmail.com>
2020-07-30 22:42:41 -0400

    Add POST endpoint at /exec

--- prickly-pufferfish/arena/apps/exec/views.py
+++ prickly-pufferfish/arena/apps/exec/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+import json
 
 
 def index(request):
-    return HttpResponse("Index page coming soon")
+    request = HttpRequest()
+    
+    if request.method == "POST":
+        request_data = request.POST
+        return HttpResponse("Good Job!")
+        
+    else:
+        return HttpResponseBadRequest("POST request required")
+
