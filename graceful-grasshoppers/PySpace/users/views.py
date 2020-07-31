c43032dc4cd96d402da4b58aa6444ec9aed2c069
cswil <cswilson@zoho.com>
2020-07-31 22:08:58 +0100

    added code for login/logout/register endpoint

---
+++ graceful-grasshoppers/PySpace/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from . import serializers
+
+# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = models.CustomUser.objects.all()
+    serializer_class = serializers.UserSerializer
+    
