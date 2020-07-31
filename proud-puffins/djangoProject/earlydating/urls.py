af49785ff6eeaaee43927c04bb931c1f10bca115
BruceBarrett <bruceb@earthreflections.com>
2020-07-31 15:03:27 -0700

    Add sample About page.

--- proud-puffins/djangoProject/earlydating/urls.py
+++ proud-puffins/djangoProject/earlydating/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='earlydating-home'),
+    path('about/', views.about, name='earlydating-about'),
 ]
--- proud-puffins/djangoProject/earlydating/views.py
+++ proud-puffins/djangoProject/earlydating/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Early Dating Home Page</h1>')
+    return HttpResponse('<h1>Early Dating Home Page</h1>'
+                        '<p>'
+                        '<a href="about">About page</a>'
+                        '</p>')
+
+
+def about(request):
+    return HttpResponse('<h1>Early Dating Home Page -- About</h1>')
+
