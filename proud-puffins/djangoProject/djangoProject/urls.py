41fa46007c895f2e5c17e52ad793eac279e3f61c
Ryan <ryanj.johnson@gmail.com>
2020-07-31 14:19:35 -0700

    Corrected the link to the homepage

--- proud-puffins/djangoProject/djangoProject/urls.py
+++ proud-puffins/djangoProject/djangoProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('earlydating/', include('earlydating.urls')),
+    path('', include('earlydating.urls')),
 
 ]
