06c1e1b5e4d8db5b3855be401e8ff53be832758d
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2020-07-31 23:39:14 +0530

    Added signup

--- bouncy-bullfrogs/bouncy/mail/views.py
+++ bouncy-bullfrogs/bouncy/mail/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form= UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form=UserRegisterForm()
+    return render(request, 'signup.html', {'form':form})
