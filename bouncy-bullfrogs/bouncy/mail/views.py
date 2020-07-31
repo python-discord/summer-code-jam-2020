4128ca957d4cd1ec9183fdd14c55e5c844dc8c66
Arth Tyagi <arthtyagi7@gmail.com>
2020-07-31 23:16:12 +0530

    changed project name in the settings to bouncy

--- bouncy-bullfrogs/bouncy/mail/views.py
+++ bouncy-bullfrogs/bouncy/mail/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 # Create your views here.
+
+
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, "mail/inbox.html")
+    else:
+        return HttpResponseRedirect(reverse("login"))
