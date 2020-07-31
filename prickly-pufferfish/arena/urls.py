daf6442a801b0acb3ad7c76a1725d617c5f913ac
Turreted <gideonmitchell01@gmail.com>
2020-07-30 21:38:15 -0400

    Add demo app for testing purposes

--- prickly-pufferfish/arena/urls.py
+++ prickly-pufferfish/arena/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # This is simply for testing purposes
+    path('exec/', include("arena.apps.exec.urls"))
 ]
