519e257fd3943a85ea4635a0115aa3e094f3541d
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-01 13:05:44 +0100

    Added URL for landing page

--- juicy-jaguars/Web95/Web95/urls.py
+++ juicy-jaguars/Web95/Web95/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('/', views.landing_page),
+    
 ]
