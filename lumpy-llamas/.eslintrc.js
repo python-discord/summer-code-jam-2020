7159eaa61952f337693cca1f8be158707e6ec780
Raj Raj <raj@cambridgespark.com>
2020-08-01 10:05:09 +0100

    Added base frontend

---
+++ lumpy-llamas/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "settings": {
+    "import/resolver": {
+      "alias": [
+        ['vue', 'vue/dist/vue.js']
+      ],
+    },
+  },
+  "extends": ["airbnb-base", "plugin:vue/essential"],
+  "parserOptions": {
+    "ecmaVersion": 8,
+  },
+  "rules": {
+    "no-new": "off",
+    "max-len": "off",
+    "no-plusplus": "off",
+    "import/no-extraneous-dependencies": ["error", { "optionalDependencies": true }],
+    "prefer-destructuring": "off",
+    "vue/require-v-for-key": "off",
+    "no-alert": "off"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "mocha": true,
+  },
+  "globals": {
+    "expect": true,
+  },
+};
