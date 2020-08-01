44342c738351d7b7bc7468d44ed48eb7009ed7d4
aviananalyst <kylan@pop-os.localdomain>
2020-08-01 11:50:43 -0700

    Pytest added, home page structure working --co-authored Discoh

--- nimble-newts/askgrieves/askgrieves/urls.py
+++ nimble-newts/askgrieves/askgrieves/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from chatbot import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test, name='home')
 ]
