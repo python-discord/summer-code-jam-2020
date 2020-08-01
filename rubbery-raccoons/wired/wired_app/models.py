6615bb33ab8c1441d185c5308567e20e78e1d509
Umer Shaikh <umer.f.shaikh@gmail.com>
2020-08-01 12:39:28 -0500

    Add a clarifying comment

--- rubbery-raccoons/wired/wired_app/models.py
+++ rubbery-raccoons/wired/wired_app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=150)
     join_date = models.DateTimeField()
-    is_author = models.BooleanField()
+    is_author = models.BooleanField() # can they author articles?
 
     def __str__(self):
         return self.name
