0086548ca5984ee1fad7f71e0f11ac6894dfd409
Kevin Sheng <kevinsheng2005@gmail.com>
2020-08-01 09:53:43 -0700

    ok made some default apps

--- observant-otters/forumthing/forumthing/settings.py
+++ observant-otters/forumthing/forumthing/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forum.apps.ForumConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
