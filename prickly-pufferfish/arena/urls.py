37aa3632573c2330a4db56a7277ba6977e260b5c
Aarush Gupta <aarush.g75@gmail.com>
2020-08-01 11:08:05 -0400

    Tried fixing static files issue, no success

--- prickly-pufferfish/arena/urls.py
+++ prickly-pufferfish/arena/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     # This is simply for testing purposes
     path('exec/', include("arena.apps.exec.urls")),
     path('', include("arena.apps.core.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
