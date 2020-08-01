e7249fb3ebed6e20039d07db8489289904e3549d
Ryan <ryanj.johnson@gmail.com>
2020-07-31 20:48:17 -0700

    Added base html file and favicon

--- proud-puffins/djangoProject/earlydating/views.py
+++ proud-puffins/djangoProject/earlydating/views.py
@@ -4,14 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Early Dating Home Page</h1>'
-                        '<p>'
-                        '<a href="about">About page</a>'
-                        '</p>'
-                        '<p>'
-                        '<a href="admin">Admin page</a>'
-                        '</p>')
+    return render(request, 'dating/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Early Dating Home Page -- About</h1>')
+    return render(request, 'dating/about.html')
