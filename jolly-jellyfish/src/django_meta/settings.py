c5d61646a81f5b7b61bb3b1028399e7dce2a8e89
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2020-08-01 13:20:07 +0200

    Add page_maker projects and initial models

--- jolly-jellyfish/src/django_meta/settings.py
+++ jolly-jellyfish/src/django_meta/settings.py
@@ -6,7 +6,7 @@ import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 def generate_key():
     """Generate a random secret key."""
---
+++ jolly-jellyfish/src/page_maker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PageMakerConfig(AppConfig):
+    name = 'page_maker'
