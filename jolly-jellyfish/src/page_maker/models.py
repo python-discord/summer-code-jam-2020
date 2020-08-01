4027c425d6c514560246137bceb4288f8d84d3d7
tameTNT <30503695+tametnt@users.noreply.github.com>
2020-08-01 12:48:37 +0100

    Install pillow to allow for use of ImageField
    Updated Pipfile and Pipfile.lock alongside installation of pillow -
    required library for use of ImageField in Django models:
    https://docs.djangoproject.com/en/3.0/ref/forms/fields/#django.forms.ImageField

--- jolly-jellyfish/src/page_maker/models.py
+++ jolly-jellyfish/src/page_maker/models.py
@@ -1,20 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+
 
 class Webpage(models.Model):
     name = models.CharField(max_length=80)
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     thumbnail = models.ImageField(null=True, blank=True, upload_to="thumbnails/")
-    template = models.ForeignKey(Template, on_delete=models.DO_NOTHING)
+    template = models.IntegerField()
     # TODO add template fields
+
 
 class Template(models.Model):
     name = models.CharField(max_length=80)
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     style_sheet = models.FileField(null=False, blank=False, upload_to="styles/")
+
 
 class Comment(models.Model):
     page = models.ForeignKey(Webpage, on_delete=models.CASCADE)
