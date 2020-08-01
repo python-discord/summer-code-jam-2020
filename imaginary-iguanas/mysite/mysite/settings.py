7b2d73d43e8f3db30b5190f0635e8fb177c783bb
Arthlight <arthurpbh@gmail.com>
2020-08-01 22:30:53 +0200

    feat: (Authentication) Added users app, templates directory with registration subfolder and accounts url path

--- imaginary-iguanas/mysite/mysite/settings.py
+++ imaginary-iguanas/mysite/mysite/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+# TODO: 'home' url doesn't exist yet
