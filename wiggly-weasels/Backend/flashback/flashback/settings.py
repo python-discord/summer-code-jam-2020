b834f5dfdc420f4317f9b5159b9098830f109ae0
JetDeveloping <jetjadeja@gmail.com>
2020-08-01 16:23:26 -0700

    Setup Postgres DB in settings

--- wiggly-weasels/Backend/flashback/flashback/settings.py
+++ wiggly-weasels/Backend/flashback/flashback/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'flashback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fbdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5443',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
