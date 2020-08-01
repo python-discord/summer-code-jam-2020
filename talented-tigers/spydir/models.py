0addb41f6d789325b728e8c8d33e80aa77907924
alex t <sloopooicecream@gmail.com>
2020-07-31 20:37:37 -0400

    added topic field and author field in GeneratedPage

--- talented-tigers/spydir/models.py
+++ talented-tigers/spydir/models.py
@@ -9,25 +9,30 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
+
 class GeneratedPage(models.Model):
-    page_title = models.CharField(max_length=50, blank=True)
+    page_author = models.CharField(max_length=20, blank=True)
     page_type_choices = [
-    ('BLOG', 'Blog'),
-    ('INFO', 'Information'),
-    ('BIZ', 'Buisness'),
-    ('FOOD', 'Food Recipe'),
-    ('SCAM', 'Scam'),
+        ('BLOG', 'Blog'),
+        ('INFO', 'Information'),
+        ('BIZ', 'Business'),
+        ('FOOD', 'Food Recipe'),
+        ('SCAM', 'Scam'),
     ]
     page_type = models.CharField(max_length=4, choices=page_type_choices, blank=True)
+    # The page title will be based on the page type
+    page_title = models.CharField(max_length=50, blank=True)
 
-    ##Blog Fields
+
+    # Blog Fields
     blog_posts = models.ManyToManyField(BlogPost)
-    blog_introduction = models.TextField(default="Welcome to my blog!", blank = True)
+    blog_introduction = models.TextField(default="Welcome to my blog!", blank=True)
 
-    ##Information Fields
-    
-    ##Buisness Fields
+    # Information Fields
+    topic = models.CharField(max_length=20, blank=True)
 
-    ##Food Recipe Fields
+    # Business Fields
 
-    ##Scam Fields
+    # Food Recipe Fields
+
+    # Scam Fields
--- talented-tigers/spydir/views.py
+++ talented-tigers/spydir/views.py
@@ -6,8 +6,10 @@ from .models import GeneratedPage
 def homepage(request):
     return render(request, 'spydir/home.html')
 
+
 def info_view(request):
     return render(request, 'spydir/generators/info.html')
+
 
 def load_generated_page(request, pagename):
     """Loads either an already generated page or generates one and loads it"""
@@ -15,8 +17,8 @@ def load_generated_page(request, pagename):
     try:
         page = GeneratedPage.objects.get(page_title=pagename)
     except GeneratedPage.DoesNotExist:
-        #generate_page(pagename)
-        #page = GeneratedPage.objects.get(page_title=pagename)
+        # generate_page(pagename)
+        # page = GeneratedPage.objects.get(page_title=pagename)
         page = None
 
     response = "<h1>This page would be about {0}.</h1>".format(pagename)
