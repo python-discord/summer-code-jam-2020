a0127ffd78480638ccd9f3b72cde914c8eb53826
fisher60 <wakefollower@gmail.com>
2020-07-31 18:29:30 -0700

    added users app and profile model, profile template placeholder

--- hysterical-horses/code_jam/code_jam/settings.py
+++ hysterical-horses/code_jam/code_jam/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ hysterical-horses/code_jam/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("profile/", views.profile, name="profile")
+]
