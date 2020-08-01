e541c7bece32974f34a5b05b1e10c95a680f7e5c
Arthlight <arthurpbh@gmail.com>
2020-08-01 19:02:59 +0200

    chore: updated Template setting so we only need to use one single Template directory and Allowed Hosts for development

--- imaginary-iguanas/mysite/mysite/settings.py
+++ imaginary-iguanas/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7tf&n58u+#)l79=lmo9a7xb=%)e^knbuna7bx@&0$9dyz9&s-7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
