8cbe0991a559496c52932959d6509bf6bcf3d25d
melodicht <marvincht@gmail.com>
2020-08-01 16:53:20 +0800

    Add LoginView

--- hysterical-horses/code_jam/users/urls.py
+++ hysterical-horses/code_jam/users/urls.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
+
 from . import views
 
-urlpatterns = [path("profile/", views.profile, name="profile")]
+
+urlpatterns = [
+    path("profile/", views.profile, name="profile"),
+    path("login/", LoginView.as_view(), name='login')
+]
