e4f4fc1c05421a959f411f279f64d38352ebc994
Raj Raj <raj@cambridgespark.com>
2020-08-01 14:13:13 +0100

    lint pls2

--- lumpy-llamas/core/tests.py
+++ lumpy-llamas/core/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import json
+from unittest.mock import patch
+from django.test import TestCase, Client
+from django.http import JsonResponse
 
-# Create your tests here.
+
+class CoreTestCase(TestCase):
+
+    @patch('core.views.serve')
+    def test_index(self, serve):
+        client = Client()
+        serve.return_value = JsonResponse({'a': 'b'})
+        res = client.get('/')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.content.decode(), json.dumps({'a': 'b'}))
--- lumpy-llamas/core/views.py
+++ lumpy-llamas/core/views.py
@@ -1,6 +1,5 @@
 from django.contrib.staticfiles.views import serve
 
-# Create your views here.
 
 def index(request):
     return serve(request, 'index.html')
