875c0054090a6663b3445e3eb6f3a40c65575270
Kevin Mui <kmui2@wisc.edu>
2020-08-01 13:22:32 -0500

    :rotating_light: Fix lint warnings

--- silky-squirrels/users/apps.py
+++ silky-squirrels/users/apps.py
@@ -1,8 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class UsersConfig(AppConfig):
     name = "users"
 
     def ready(self):
-        import users.signals
+        from django.contrib.auth.models import User
+        from users.signals import create_profile, save_profile
+
+        post_save.connect(create_profile, sender=User)
+        post_save.connect(save_profile, sender=User)
--- silky-squirrels/users/views.py
+++ silky-squirrels/users/views.py
@@ -9,8 +9,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Your account has been created! You are now able to log in")
+            messages.success(request, "Your account has been created! You are now able to log in")
             return redirect("login")
     else:
         form = UserRegisterForm()
@@ -25,7 +24,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f"Your account has been updated!")
+            messages.success(request, "Your account has been updated!")
             return redirect("profile")
 
     else:
