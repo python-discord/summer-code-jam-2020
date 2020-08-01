e46c19bb22e5ff29c8d38fec5064892ee6086cc4
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-08-01 15:50:49 -0400

    Add x,y,z to Player and Room models

--- oscillating-oysters/twentytwentymud/MUD/models.py
+++ oscillating-oysters/twentytwentymud/MUD/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=50)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    z = models.IntegerField(default=0)
 
 
 class Room(models.Model):
-    x = models.IntegerField()
-    y = models.IntegerField()
     description = models.CharField(max_length=512)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    z = models.IntegerField(default=0)
