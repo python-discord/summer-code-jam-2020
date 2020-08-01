daa24ad987c77c73e129cfe446395ee7c230e310
makubob <makupi@icloud.com>
2020-08-01 15:36:08 +0200

    Add MorningConfig to settings.py

--- friendly-frogs/friendly_frogs/friendly_frogs/settings.py
+++ friendly-frogs/friendly_frogs/friendly_frogs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'morning.apps.MorningConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
