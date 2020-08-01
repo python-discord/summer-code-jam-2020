2e8ca58c9091dfbe525afc9c6636e4d7829f4c4d
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-01 16:38:49 +0100

    Linting
    Added docstring to main()

--- juicy-jaguars/Web95/manage.py
+++ juicy-jaguars/Web95/manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    """Main Function."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web95.settings')
     try:
         from django.core.management import execute_from_command_line
