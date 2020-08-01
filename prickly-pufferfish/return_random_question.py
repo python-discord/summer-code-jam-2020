f1d4a6295eddfb22f8be47be4bbc2a2c062a2978
Lakshya <lakshya@Shrutis-MacBook-Air.local>
2020-08-01 15:15:19 -0700

    Fixed linting errors again.

--- prickly-pufferfish/return_random_question.py
+++ prickly-pufferfish/return_random_question.py
@@ -6,13 +6,12 @@ path = "python_questions"
 names = os.listdir(path)
 names = [f for f in names if not f.startswith('.')]
 list_of_paths = []
-for root,dirs,files in os.walk(path):
+for root, dirs, files in os.walk(path):
     for f in files:
-        x = os.path.join(root,f)
+        x = os.path.join(root, f)
         list_of_paths.append(x)
 
 
 question = random.choice(list_of_paths)
-open_question = open(question,'r')
+open_question = open(question, 'r')
 print(open_question.read())
-
