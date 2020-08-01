9c41a636b2d2707be3b6ca4ca8a5891d90d103d2
Kenny <kfonatoko@gmail.com>
2020-08-01 15:03:40 -0400

    Create Rovers app, templates, and models

--- mysterious-mice/spacebook/rovers/urls.py
+++ mysterious-mice/spacebook/rovers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RoverProfileView
 
 urlpatterns = [
-    path("rover/<slug:user_name>/", RoverProfileView.as_view(), name="rover-profile"),
+    path("rover/<slug:username>/", RoverProfileView.as_view(), name="rover-profile"),
     # path("rover/<int:pk>/", RoverProfileView.as_view(), name="rover-profile"),
 ]
--- mysterious-mice/spacebook/rovers/views.py
+++ mysterious-mice/spacebook/rovers/views.py
@@ -6,3 +6,5 @@ from .models import Rover
 class RoverProfileView(DetailView):
     model = Rover
     template_name = "rovers/profile.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
