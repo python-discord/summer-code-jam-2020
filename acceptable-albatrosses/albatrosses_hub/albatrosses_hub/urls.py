ca80231ca3f140ed81659f7fc576db600d5b3591
charliedua <duaanmol2@gmail.com>
2020-08-02 21:18:05 +1000

    added game app with links to gamepage

--- acceptable-albatrosses/albatrosses_hub/albatrosses_hub/urls.py
+++ acceptable-albatrosses/albatrosses_hub/albatrosses_hub/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls"))
+    path("", include("home.urls")),
+    path("gaem", include("game.urls"))
 ]
