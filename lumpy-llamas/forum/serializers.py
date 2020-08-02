f124c4e8a121bd4b840ea494335f421ff08d455d
Terje Espedal <terje.m.espedal@gmail.com>
2020-08-02 14:49:15 +0200

    Rebase on frontend to support vuejs

--- lumpy-llamas/forum/serializers.py
+++
@@ -1,6 +0,0 @@
-from django.core import serializers
-from .models import Thread, ThreadMessage
-
-serialized_threads = serializers.serialize('json', Thread.objects.all())
-serialized_messages = serializers.serialize('json', ThreadMessage.objects.all())
-
