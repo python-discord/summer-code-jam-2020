4485c832a409070bd07c75d5e082368d2bb57360
MW <maksw982@gmail.com>
2020-08-03 01:27:17 +0200

    Create Comment model

--- efficient-eagles/early_internet/main/models.py
+++ efficient-eagles/early_internet/main/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 class CustomUser(AbstractUser):
     pass
 
@@ -14,5 +22,11 @@ class Topic(models.Model):
     pass
 
 
-class Comment(models.Model):
-    pass
+class Comment(TimeStampedModel):
+    upvotes = models.PositiveIntegerField(default=0)
+    downvotes = models.PositiveIntegerField(default=0)
+    body = models.CharField(max_length=10000, default='')
+    
+    author = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
+    comment_thread = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
