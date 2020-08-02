154545dec1305a3775be4183f0e2519de4057151
fisher60 <wakefollower@gmail.com>
2020-08-02 02:22:44 -0700

    added instant messenger, added docker setup files for redis

--- hysterical-horses/code_jam/dashboard/views.py
+++ hysterical-horses/code_jam/dashboard/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'dashboard/index.html')
+
+
+def chat_room(request, room_name):
+    context = {'room_name': room_name}
+    return render(request, 'dashboard/chat_room.html', context)
