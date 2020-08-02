451175b8dba68d27b8809c0fe543283d21943a04
Raj Raj <raj@cambridgespark.com>
2020-08-02 12:41:27 +0100

    Fix catchall

--- lumpy-llamas/lammas/urls.py
+++ lumpy-llamas/lammas/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from lammas.settings import STATIC_URL, STATIC_ROOT
 from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', index),
-] + static(STATIC_URL, document_root=STATIC_ROOT)
+] + static(STATIC_URL, document_root=STATIC_ROOT) + [
+
+    re_path(r'^.*/$', index, name='unmatched'),
+]
