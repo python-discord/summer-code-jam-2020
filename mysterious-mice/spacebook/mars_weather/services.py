216375694faba95171ba32779e3834e35b3b0aae
Ryan Sheppard <Rshep3087@gmail.com>
2020-08-02 11:18:36 -0500

    Weather for the week view added.

--- mysterious-mice/spacebook/mars_weather/services.py
+++ mysterious-mice/spacebook/mars_weather/services.py
@@ -6,6 +6,9 @@ MARS_URL = (
 
 
 def get_current_weather(request):
+    """
+    If user reloads the page, used cached data instead of making call to API again.
+    """
     is_cached = "weather_data" in request.session
 
     if not is_cached:
@@ -21,4 +24,28 @@ def get_current_weather(request):
         "atmospheric_temperature": weather_data[current_sol]["AT"]["av"],
         "atmospheric_pressure": weather_data[current_sol]["PRE"]["av"],
         "horizontal_wind_speed": weather_data[current_sol]["HWS"]["av"],
+    }
+
+
+def get_week_weather(request):
+    """
+    If user reloads the page, used cached data instead of making call to API again.
+    """
+    is_cached = "weather_data" in request.session
+
+    if not is_cached:
+        print("No data cached.")
+        response = requests.get(MARS_URL)
+        request.session["weather_data"] = response.json()
+
+    weather_data = request.session["weather_data"]
+
+    return {
+        "day_one": weather_data[weather_data["sol_keys"][0]],
+        "day_two": weather_data[weather_data["sol_keys"][1]],
+        "day_three": weather_data[weather_data["sol_keys"][2]],
+        "day_four": weather_data[weather_data["sol_keys"][3]],
+        "day_five": weather_data[weather_data["sol_keys"][4]],
+        "day_six": weather_data[weather_data["sol_keys"][5]],
+        "day_seven": weather_data[weather_data["sol_keys"][6]],
     }
--- mysterious-mice/spacebook/mars_weather/views.py
+++ mysterious-mice/spacebook/mars_weather/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .services import get_current_weather
+from .services import get_current_weather, get_week_weather
 
 # Create your views here.
 
@@ -8,12 +8,18 @@ from .services import get_current_weather
 class CurrentWeatherView(View):
     """
     Provides the weather on Mars for the current Sol (Mars Day).
-
-    If user reloads the page, used cached data instead of making call to API again.
     """
-
-    template_name = "mars_weather/weather.html"
 
     def get(self, request):
         context = get_current_weather(request)
-        return render(request, "mars_weather/weather.html", context)
+        return render(request, "mars_weather/current_weather.html", context)
+
+
+class WeekWeatherView(View):
+    """
+    Provides details regarding the weather the past week on Mars.
+    """
+
+    def get(self, request):
+        context = get_week_weather(request)
+        return render(request, "mars_weather/week_weather.html", context)
