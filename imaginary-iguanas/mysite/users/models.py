b0bc9beb10121fdf353de21a9b5a62d9955ce580
Braindead <albertopoljak@gmail.com>
2020-08-02 21:16:32 +0200

    Improved user models.py
    User no longer extends AbstractBaseUser but now has a onetoone field to User.
    Added str dunders to model.

--- imaginary-iguanas/mysite/users/models.py
+++ imaginary-iguanas/mysite/users/models.py
@@ -1,22 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User
 
 
-class User(AbstractBaseUser):
+class UserProfile(models.Model):
     GENDER_CHOICES = (
         ("M", "Male"),
         ("F", "Female"),
     )
 
-    username = models.CharField(max_length=50, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=50)
     gender = models.CharField(null=True, max_length=1, choices=GENDER_CHOICES)
     country = models.CharField(null=True, max_length=2, help_text="ISO 639 country code.")
     city = models.CharField(null=True, max_length=50)
     date_of_birth = models.DateField(null=True)
 
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
-
-
+    def __str__(self):
+        return f"{self.user.username} profile."
