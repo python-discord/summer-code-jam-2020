71e45619a8861919ba5374f37293c73df7f69e1f
Kevin Sheng <kevinsheng2005@gmail.com>
2020-08-01 19:28:51 -0700

    rudimentary models
    there's probably a million things wrong with em

--- observant-otters/forumthing/forum/models.py
+++ observant-otters/forumthing/forum/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Thread(models.Model):
+    """
+    the model for a thread/conversation
+    """
+    title = models.TextField()
+    date_created = models.DateTimeField()
+    author = models.OneToOneField(User, models.CASCADE)
+
+
+class Message(models.Model):
+    """
+    the model for a message inside a thread
+    """
+    content = models.TextField()
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.OneToOneField(User, models.DO_NOTHING)  # it's either this or models.PROTECT
+    date_posted = models.DateTimeField()
+    date_edited = models.DateTimeField(default=date_posted)
+
+
+class UserThreadEvent(models.Model):
+    """
+    we log an event when the user interacts with the thread,
+    whether they be viewing it or making a post
+    """
+    type = models.BooleanField()  # idk, true would be create, and false would be view?
+    thread = models.ForeignKey(Thread, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, models.DO_NOTHING)
--- observant-otters/forumthing/user/models.py
+++ observant-otters/forumthing/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class OurUser(models.Model):
+    """
+    since django's user lacks some stuff that we need, this be another user model
+    nickname's aren't in there yet
+    """
+    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.URLField()  # link to the image of the user's discord avatar
+    acc_token = models.TextField()  # discord access token
+    refresh_token = models.TextField()  # discord refresh token
+    token_exp_date = models.DateTimeField()  # token expiration date
+    discord_id = models.TextField()
