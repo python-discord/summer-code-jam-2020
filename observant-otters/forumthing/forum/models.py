2f4982f81a176c8a522f7b56fb04d34bbc2ca7af
Kevin Sheng <kevinsheng2005@gmail.com>
2020-08-02 12:18:04 -0700

    made smth an enum

--- observant-otters/forumthing/forum/models.py
+++ observant-otters/forumthing/forum/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from enum_field import Enum, EnumField
 from user.models import ForumUser
+
 
 class Thread(models.Model):
     """
@@ -21,11 +23,17 @@ class Message(models.Model):
     date_edited = models.DateTimeField(default=date_posted)
 
 
+EVENT_TYPES = Enum(
+    ('create-message', 'CREATE'),
+    ('view-thread', 'VIEW')
+)
+
+
 class UserThreadEvent(models.Model):
     """
     we log an event when the user interacts with the thread,
     whether they be viewing it or making a post
     """
-    type = models.BooleanField()  # idk, true would be create, and false would be view?
+    type = EnumField(EVENT_TYPES)  # idk, true would be create, and false would be view?
     thread = models.ForeignKey(Thread, on_delete=models.DO_NOTHING)
     user = models.OneToOneField(ForumUser, models.DO_NOTHING)
