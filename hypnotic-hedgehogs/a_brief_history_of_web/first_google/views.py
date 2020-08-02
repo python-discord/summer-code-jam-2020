631d30e189b20bafe93adc9973769905d7341447
zhaos <zhaoshimeng2015@gmail.com>
2020-08-02 21:24:36 +0200

    initiate the app; set up raw html frame work

--- hypnotic-hedgehogs/a_brief_history_of_web/first_google/views.py
+++ hypnotic-hedgehogs/a_brief_history_of_web/first_google/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from pathlib import PurePath
 
 
 # Create your views here.
 def index(requests):
-    return HttpResponse('This page is working!')
+    index_path = PurePath('first_google/index.html')
+    return render(requests, index_path)
