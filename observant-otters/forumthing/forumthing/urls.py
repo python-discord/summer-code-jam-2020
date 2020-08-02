36b62415453e2d9167bfe9ab697118ea41f82f29
Andrew Zhu <me@andrew-zhu.com>
2020-08-01 17:24:35 -0700

    feat: add rudimentary profile page

--- observant-otters/forumthing/forumthing/urls.py
+++ observant-otters/forumthing/forumthing/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forum.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('user.urls')),
 ]
