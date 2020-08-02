88d0b614ab5418b34a2a6bd7525ee413d6b34cde
glyphack <sh.hooshyari@gmail.com>
2020-08-02 19:01:05 +0430

    change app name

--- bulky-butterflies/src/messenger/manage.py
+++ bulky-butterflies/src/messenger/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'butterflies.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
