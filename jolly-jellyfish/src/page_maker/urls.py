5eab42e984c2d9f03b8a8d3e342bbebe767a3615
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2020-08-02 16:35:50 +0200

    Add user view classes and templates

--- jolly-jellyfish/src/page_maker/urls.py
+++ jolly-jellyfish/src/page_maker/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import MainView, UserRegister
+from .views import (
+    MainView,
+    UserRegister,
+    UserDetailView,
+    UserUpdateView,
+    UserDeleteView
+)
 
 urlpatterns = [
     path('', MainView.as_view(), name='page-maker-main'),
     path('users/register', UserRegister.as_view(), name='register'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('users/<int:pk>/update', UserUpdateView.as_view(), name='user-update'),
+    path('users/<int:pk>/delete', UserDeleteView.as_view(), name='user-delete'),
 ]
--- jolly-jellyfish/src/page_maker/views.py
+++ jolly-jellyfish/src/page_maker/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     TemplateView,
-    FormView
+    FormView,
+    DetailView,
+    UpdateView,
+    DeleteView
 )
 
+from .models import User
 from .forms import UserRegisterForm
 
 
@@ -18,3 +23,34 @@ class UserRegister(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class UserDetailView(DetailView):
+    model = User
+    context_object_name = 'viewed_user'
+    template_name = 'page_maker/user_detail.html'
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'page_maker/user_update.html'
+    fields = ['first_name', 'last_name', 'email']
+    success_url = '/'
+
+    def test_func(self):
+        user = self.get_object()
+        if self.request.user == user:
+            return True
+        return False
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = 'page_maker/user_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        user = self.get_object()
+        if self.request.user == user:
+            return True
+        return False
