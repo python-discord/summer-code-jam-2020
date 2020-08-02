7f23842d29a09923eb79b592ea9300361de53c2f
DrEpsilon <58871142+DrEpsilon@users.noreply.github.com>
2020-08-02 19:06:25 +0530

    Basic Login

--- friendly-frogs/friendly_frogs/friendly_frogs/urls.py
+++ friendly-frogs/friendly_frogs/friendly_frogs/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name="register"),
-    path('', include('morning.urls'))
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('morning.urls'), name='home')
 ]
