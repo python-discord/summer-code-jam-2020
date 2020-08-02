70b4bcf8c80b6c91e9e7543ca0f77c2fa1d5477f
Genie <frothsavd@gmail.com>
2020-08-02 04:58:22 -0400

    Added skeletons for thread creation (POST), deletion (DELETE), and editing (PATCH)

--- observant-otters/forumthing/forum/urls.py
+++ observant-otters/forumthing/forum/urls.py
@@ -1,6 +1,20 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='forum-homepage'),
+    path(
+        "",
+        views.home,
+        name="forum-homepage"
+    ),
+    path(
+        'forum/threads',
+        views.threads,
+        name="threads-all"
+    ),
+    re_path(
+        r"^forum/threads/(?P<id>\d+)/$",
+        views.threads,
+        name="threads-single"
+    )
 ]
--- observant-otters/forumthing/forum/views.py
+++ observant-otters/forumthing/forum/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
-    return render(request, 'forum/home.html', {})
+    return redirect("threads-all")
+
+def threads(request, id=None):
+    if request.method == "GET":
+        if id is None:
+            return render(request, 'forum/home.html', {})
+        else:
+            p= request.GET.get("p", default=1)
+            data= {
+                "id": id,
+                "page": p,
+
+                # @TODO: Remove debug values
+                "next_page": int(p)+1,
+                "next_id": int(id)+1
+            }
+            return render(request, 'forum/thread.html', data)
+
+    elif request.method == "POST":
+        if id is None:
+            pass # @TODO: Handle new thread creation
+        else:
+            pass # @TODO: Handle new thread reply creation
+
+    elif request.method == "DELETE":
+        assert id is not None, "Thread deletion without id"
+        # @TODO: Handle thread deletion
+
+    elif request.method == "PATCH":
+        assert id is not None, "Thread edit without id"
+        # @TODO: Handle thread deletion
