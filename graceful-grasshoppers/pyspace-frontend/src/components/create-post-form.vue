30f939df3b580f02479fa16a7eab27a7a2af8dc9
George Munyoro <george.guva@outlook.com>
2020-08-02 22:19:25 +0200

    Setup basic post creation page

---
+++ graceful-grasshoppers/pyspace-frontend/src/components/create-post-form.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <form class="flex-col" @submit.prevent="onSubmit">
+            <label for='title'>Title</label>
+            <input id='title' type="" name="">
+            <label for='content'>Content</label>
+            <textarea id="content"></textarea>
+            <input type="submit" value="Post">
+        </form>
+    </div>
+</template>
+
+<script type="text/javascript">
+import Post from '../models/post';
+export default {
+    name: 'CreatePostForm',
+    data() {
+        return {
+            post: new Post(),
+            message: '',
+        };
+    },
+    methods: {
+        onSubmit() {
+            // on post submit
+        }
+    }
+}
+</script>
+
+<style>
+.flex-col {
+    display: flex;
+    flex-direction: column;
+}
+
+label {
+    text-align: left;
+}
+</style>
