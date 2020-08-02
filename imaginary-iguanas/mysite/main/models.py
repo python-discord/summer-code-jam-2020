da08c74171a309f35756156513ae55e19809c7a3
Braindead <albertopoljak@gmail.com>
2020-08-02 21:17:13 +0200

    Added str dunders to models. User has been renamed to UserProfile.

--- imaginary-iguanas/mysite/main/models.py
+++ imaginary-iguanas/mysite/main/models.py
@@ -1,18 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from users.models import User
+from users.models import UserProfile
+
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
     creation_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"BlogPost {self.title} : {self.description}"
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    author = models.ForeignKey(UserProfile, on_delete=models.SET_NULL)
     comment = models.CharField(max_length=500)
     creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.user.username} content: {self.comment}"
 
 
 class BlogComment(Comment):
@@ -20,4 +26,4 @@ class BlogComment(Comment):
 
 
 class ProfileComment(Comment):
-    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
