be12f3d143f31fdeb26fcaf3ddff75924fae0455
JetDeveloping <jetjadeja@gmail.com>
2020-08-02 12:58:13 -0700

    Created API Views

--- wiggly-weasels/Backend/flashback/fbbackend/views.py
+++ wiggly-weasels/Backend/flashback/fbbackend/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from fbbackend import models, serializers
 
-# Create your views here.
+'''
+Views for our Rest API.
+We went with class based views, because they were a better fit than functions
+'''
+
+class Account_View(viewsets.ModelViewSet):
+    queryset = models.Account.objects.all()
+    serializer_class = serializers.AccountSerializer
+
+class Group_View(viewsets.ModelViewSet):
+    queryset = models.Group.objects.all()
+    serializer_class = serializers.GroupSerializer
+
+class Message_View(viewsets.ModelViewSet):
+    queryset = models.Message.objects.all()
+    serializer_class = serializers.MessageSerializer
+
+class Post_View(viewsets.ModelViewSet):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
