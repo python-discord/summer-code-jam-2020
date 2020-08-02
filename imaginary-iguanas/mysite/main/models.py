5cdb9e1aa509792812ab0f3763e27b214d1d4444
Arthlight <arthurpbh@gmail.com>
2020-08-02 10:28:02 +0200

    Squashed commit of the following:
    commit 288bc78668ba319024e660830276649a141726bc
    Author: Braindead <albertopoljak@gmail.com>
    Date:   Sat Aug 1 21:35:50 2020 +0200
    
        User model now inherits from AbstractBaseUser.
    
        Useful as the model already has built in password hashing functionality.
    
    commit a467d450e12d4f7d8893481afde32187cf66defe
    Author: Braindead <albertopoljak@gmail.com>
    Date:   Sat Aug 1 20:39:45 2020 +0200
    
        Created base models.py
    
        Models for User (base&profile), BlogPost representing articles for user blog, Comment representing base comment and BlogComment&ProfileComment which are linked as their name suggests.

--- imaginary-iguanas/mysite/main/models.py
+++ imaginary-iguanas/mysite/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=50)
+    gender = models.CharField(null=True, max_length=1, choices=GENDER_CHOICES)
+    country = models.CharField(null=True, max_length=2, help_text="ISO 639 country code.")
+    city = models.CharField(null=True, max_length=50)
+    date_of_birth = models.DateField(null=True)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    comment = models.CharField(max_length=500)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class BlogComment(Comment):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+
+
+class ProfileComment(Comment):
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
