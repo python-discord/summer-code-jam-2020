edab429d78d4d335a8a179e8086bd32b80e0abda
François Hardy <snaptraks@gmail.com>
2020-08-02 17:19:13 -0400

    Add game/login and game/register pages

--- wily-wolves/mud/game/urls.py
+++ wily-wolves/mud/game/urls.py
@@ -6,6 +6,6 @@ app_name = 'game'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/login', views.login, name='login'),
-    path('/register', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- wily-wolves/mud/game/views.py
+++ wily-wolves/mud/game/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the game index.")
+
+
+def login(request):
+    return HttpResponse("This is the login page.")
+
+
+def register(request):
+    return HttpResponse("This is the register page.")
