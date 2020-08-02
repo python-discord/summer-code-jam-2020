d4496799d0f48875c17a4ea70187f02a0f36aa78
JetDeveloping <jetjadeja@gmail.com>
2020-08-01 20:03:20 -0700

    Added Groups, Messages, and Accounts

--- wiggly-weasels/Backend/flashback/fbbackend/admin.py
+++ wiggly-weasels/Backend/flashback/fbbackend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from fbbackend.models import Account
 # Register your models here.
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'email',
+        'hashed_pass',
+        'identification',
+        'nickname',
+        'bot'
+    ]
+
+
+admin.site.register(Account, AccountAdmin)
--- wiggly-weasels/Backend/flashback/fbbackend/models.py
+++ wiggly-weasels/Backend/flashback/fbbackend/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Account(models.Model):  # A model containing user information
+    '''
+
+    email: The user's email (which they used to sign up)
+    hashed_pass: A Hashed Version of their password, so they can login
+    identification: A Randomly Generated Unique ID
+    nickname: The user's nickname on the IRC and Forum
+    bot: A boolean to indicate whether a user is a bot
+
+    '''
+    # Technical Information
+    email = models.EmailField(max_length=100)
+    hashed_pass = models.CharField(max_length=64)
+    identification = models.CharField(max_length=100, default=uuid4().hex)
+
+    # Display
+    nickname = models.CharField(max_length=20)
+    bot = models.BooleanField(default=False)
+
+
+class Message(models.Model):
+    '''
+
+    writer: the creator of the message
+    content: the content within the message
+
+    '''
+    writer = models.CharField(max_length=100)  # The ID of the User
+    content = models.CharField(max_length=1000)  # The Content
+
+    def __str__(self):  # Returns "content" when using the str() function
+        return self.content
+
+
+class Group(models.Model):
+    '''
+
+    creator: creator of the chatroom
+    identification: A randomly generated Unique ID
+    messages: A list of current messages on the channel
+    name: The name of group
+
+    '''
+    creator = models.CharField(max_length=100)  # The ID of the User
+    identification = models.CharField(max_length=100, default=uuid4().hex)
+    messages = ArrayField(  # ArrayField Containing Messages
+        ArrayField(
+            models.CharField(max_length=1000),
+            size=2
+        )
+    )
+
+    name = models.CharField(max_length=20)
