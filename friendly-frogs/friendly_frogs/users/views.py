d51c0541f28d0bba65ece14a37a8f11ac1072873
DrEpsilon <58871142+DrEpsilon@users.noreply.github.com>
2020-08-02 14:37:02 +0530

    Created User Registration

---
+++ friendly-frogs/friendly_frogs/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account successfully created for {username}")
+    else:
+        form = UserRegisterForm()
+    return render(request, "register.html", {'form': form})
