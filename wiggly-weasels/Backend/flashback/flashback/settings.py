077290df5440c48709f36a8a86a5f2674e51d5e0
JetDeveloping <jetjadeja@gmail.com>
2020-08-02 11:56:50 -0700

    Setup the Django Rest Framework

--- wiggly-weasels/Backend/flashback/flashback/settings.py
+++ wiggly-weasels/Backend/flashback/flashback/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fbbackend.apps.FbbackendConfig'
+    'fbbackend.apps.FbbackendConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
