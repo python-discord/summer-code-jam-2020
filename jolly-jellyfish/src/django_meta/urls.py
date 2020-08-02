c716decc85fd81a094f20bea7ab314b5975d6ae5
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2020-08-02 21:11:25 +0200

    Fix static files

--- jolly-jellyfish/src/django_meta/urls.py
+++ jolly-jellyfish/src/django_meta/urls.py
@@ -1,4 +1,6 @@
 """early_windows URL Configuration"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -8,4 +10,4 @@ urlpatterns = [
     path('users/login/', auth_views.LoginView.as_view(), name='login'),
     path('users/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('page_maker.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
