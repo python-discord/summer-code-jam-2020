ec5f157dc30a6f705680d352295a745b28d9bca2
makubob <makupi@icloud.com>
2020-08-02 18:31:39 +0200

    Move users urls to it's own urls.py

--- friendly-frogs/friendly_frogs/friendly_frogs/urls.py
+++ friendly-frogs/friendly_frogs/friendly_frogs/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from users import views as user_views
-from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', include('morning.urls'), name='home')
+    path('', include('morning.urls'), name='home'),
+    path('', include('users.urls'), name='users'),
 ]
