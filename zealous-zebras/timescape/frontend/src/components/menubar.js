4930488b8330ab85f4fc2a6bb002079d1425cdaa
Gustav Odinger <gustav@odinger.se>
2020-08-03 13:54:24 +0200

    Created a draggable object for the menu and updated dependencies.

--- zealous-zebras/timescape/frontend/src/components/menubar.js
+++ zealous-zebras/timescape/frontend/src/components/menubar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+
+import './menubar.css'
 
 class MenuBar extends Component {
   render() {
     return (
       <div>
+        <Draggable handle=".handle">
+          <div className="overlay">
+            <div className="d-flex fixed-bottom">
+              <div className="menu-box mx-auto">
+                <span className="handle">::: </span>
+                <span>Some content</span>
+              </div>
+            </div>
+          </div>
+        </Draggable>
       </div>
     );
   }
 }
 
-export default MenuBar
+export default MenuBar;
