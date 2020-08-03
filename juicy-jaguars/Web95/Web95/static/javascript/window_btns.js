6fb88207af1bdbf8fb98c86ffd3b8a3c6d7b663f
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-03 12:25:05 +0100

    Fixed window button pressed effect. Again

--- juicy-jaguars/Web95/Web95/static/javascript/window_btns.js
+++ juicy-jaguars/Web95/Web95/static/javascript/window_btns.js
@@ -1,16 +1,16 @@
 function onPress(e) {
     e.target.classList.add("window-btn-pressed-active");
-    console.log("added");
 }
 
 function onUnpress(e) {
     e.target.classList.remove("window-btn-pressed-active");
-    console.log("removed");
 }
 
-buttons = document.getElementsByClassName("window-btn");
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onmousedown = onPress;
-    buttons[i].onmouseup = onUnpress;
-    buttons[i].onmouseexit = onUnpress;
+function loadButtonEffects() {
+    buttons = document.getElementsByClassName("window-btn");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onmousedown = onPress;
+        buttons[i].onmouseup = onUnpress;
+        buttons[i].onmouseexit = onUnpress;
+    }
 }
