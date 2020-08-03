fb4fbb97dfb4f4e60390a2af10ca4b081e1a6430
zhaos <zhaoshimeng2015@gmail.com>
2020-08-04 01:13:34 +0200

    add results page front end

--- hypnotic-hedgehogs/a_brief_history_of_web/first_google/urls.py
+++ hypnotic-hedgehogs/a_brief_history_of_web/first_google/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'first_google'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/results', views.results, name='results')
 ]
--- hypnotic-hedgehogs/a_brief_history_of_web/first_google/views.py
+++ hypnotic-hedgehogs/a_brief_history_of_web/first_google/views.py
@@ -7,3 +7,8 @@ from pathlib import PurePath
 def index(requests):
     index_path = PurePath('first_google/index.html')
     return render(requests, index_path)
+
+
+def results(requests):
+    results_path = PurePath('first_google/results.html')
+    return render(requests, results_path)
