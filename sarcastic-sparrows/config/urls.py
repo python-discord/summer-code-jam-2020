3ec308366c7bdd700b2fb6a0c29e881626f60731
sschr15 <steven.sschr15@hotmail.com>
2020-08-03 14:53:00 -0500

    Created done

--- sarcastic-sparrows/config/urls.py
+++ sarcastic-sparrows/config/urls.py
@@ -21,6 +21,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("main", TemplateView.as_view(template_name="mainapp.html"), name="main"),
+    path("market", TemplateView.as_view(template_name="marketplace.html"), name="market")
 ]
 
 if "debug_toolbar" in settings.INSTALLED_APPS:
