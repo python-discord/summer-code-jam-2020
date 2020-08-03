c0c43d1249e9052af7a4845017215811317cf159
caleb park <calebparky@gmail.com>
2020-08-03 14:22:51 -0500

    Reformat Chat Page, Tabs for Chat Options

--- silky-squirrels/chat/views.py
+++ silky-squirrels/chat/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 
+# Will need to implement the Friends model    
 def index(request):
-    return render(request, "chat/index.html")
-
+    # context = {"friends": Friends.objects.all()}
+    return render(request, "chat/index.html") 
+    #return render(request, "blog/home.html", context)
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
