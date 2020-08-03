edb47e827a40d4703beecb15971394af131de9d7
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-03 20:44:41 +0530

    Add login url to urls.py

--- glossy-gorillas/glossy_gorillas/urls.py
+++ glossy-gorillas/glossy_gorillas/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="core/login.html"), name="login")
 ]
