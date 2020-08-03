1148ed2c45112562d59af8c6236208c4adc1f7a3
alex t <sloopooicecream@gmail.com>
2020-08-03 02:38:22 -0400

    added wikipedia library to pipfile, implimented wikipedia webscrape for info pages

--- talented-tigers/spydir/generate_page.py
+++ talented-tigers/spydir/generate_page.py
@@ -1,12 +1,13 @@
 from .models import GeneratedPage
-import requests
+import requests, wikipedia
 
 
 # This is where the text will be scraped. Right now, this function only generates an info page with a title
 def generate_page(page_name):
     page_author = generate_name()
-    return GeneratedPage.objects.create(page_title=page_name, page_author=page_author, page_type='INFO',
-                                        scam_type='ROMANCE')
+    page_content = generate_information(page_name)
+    return GeneratedPage.objects.create(page_title=page_name, page_content=page_content, page_author=page_author,
+                                        page_type='INFO', scam_type='ROMANCE')
 
 
 # Returns a randomly generated name
@@ -15,6 +16,18 @@ def generate_name():
     return person.json()['name']
 
 
-# This function will be used to generate text for the info style pages
-def generate_information():
-    pass
+# This function will be used to generate text for the info style pages. For now, it downloads from wikipedia
+def generate_information(page_name):
+    # First searches noun in wikipedia search
+    search = wikipedia.search(page_name, results=1)
+
+    # Tries to return the contents of the first result
+    try:
+        result = wikipedia.summary(search[0])
+    except IndexError:
+        # If the search yielded no results
+        result = "Under Construction"
+    except wikipedia.DisambiguationError as e:
+        # If the page it enters is a wikipedia "disambiguation" page
+        result = wikipedia.summary(e.options[0])
+    return result
