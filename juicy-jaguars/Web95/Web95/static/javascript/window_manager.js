6f692d86da7665e6215c7e1c4da3b73a913c1aeb
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-03 12:59:08 +0100

    Added Maximise functionality to windows

--- juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
+++ juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
@@ -8,6 +8,33 @@ function wm_restore(divId) {
     x.style.display = "block";
 }
 
+function wm_maximise(divId) {
+    var x = document.getElementById(divId);
+
+    if (x.maximised == "true") {
+        x.style.top = x.oldtop, x.style.left = x.oldleft;
+        x.style.width = x.oldwidth.toString()+"px";
+        x.style.height = x.oldheight.toString()+"px";
+
+        x.maximised = "false";
+
+        console.log("unmax");
+    } else {
+        x.oldtop = x.style.top, x.oldleft = x.style.left;
+        x.style.top = "0px", x.style.left = "0px";
+
+        x.oldwidth = x.offsetWidth, x.oldheight = x.offsetHeight;
+        x.style.width = (window.innerWidth).toString()+"px";
+        x.style.height = (window.innerHeight - document.getElementById("taskbar").offsetHeight).toString()+"px";
+
+        x.maximised = "true";
+
+        console.log(window.innerWidth.toString()+"px");
+
+        console.log("max");
+    }
+}
+
 /*
 function wm_minimise(divClass) {
     var x = document.getElementsByClassName(divClass);
