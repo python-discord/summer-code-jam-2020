929f1b33a22155846d502e6a7209237ef43bfe70
Justus Sieweke <justus.sieweke@gmail.com>
2020-08-03 20:32:03 +0200

    Added Basic Profiles and swapped UserProfile.country to be a select box. Updated pipfile (now includes pycountry)

--- imaginary-iguanas/mysite/users/models.py
+++ imaginary-iguanas/mysite/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+import pycountry
 
 
 class UserProfile(User):
@@ -8,12 +9,18 @@ class UserProfile(User):
         ("F", "Female"),
         ("D", "Other")
     )
+    COUNTRY_CHOICES = list((country.alpha_2, country.name) for country in pycountry.countries)
 
     image = models.ImageField(default="default_pfp.jpg", upload_to="profile_pics")
     gender = models.CharField(null=True, max_length=1, choices=GENDER_CHOICES)
-    country = models.CharField(null=True, max_length=2, help_text="ISO 639 country code.")
+    country = models.CharField(null=True, max_length=2, choices=COUNTRY_CHOICES)
     city = models.CharField(null=True, max_length=50)
     date_of_birth = models.DateField(null=True)
 
     def __str__(self):
-        return f"{self.username} profile."
+        return f"{self.username}'s profile"
+
+    def __repr__(self):
+        return f"<UserProfile> {repr(self.username)} {repr(self.email)} {repr(self.image)} {repr(self.gender)} {repr(self.country)} {repr(self.city)} {repr(self.date_of_birth)}"
+
+
--- imaginary-iguanas/mysite/users/views.py
+++ imaginary-iguanas/mysite/users/views.py
@@ -4,6 +4,10 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .models import UserProfile
 
+import pycountry
+
+
+
 from .forms import MySiteUserCreationForm
 
 
@@ -19,9 +23,15 @@ def home(request):
 
 def user(request, user_id):
     try:
-        user_prof = UserProfile.objects.get(id=user_id)
-        return render(request, 'users/user.html', {'user_obj': user_prof, 'title': user_prof.username})
+        user_prof_obj = UserProfile.objects.get(id=user_id)
+        return render(request, 'users/user.html', {'user_prof': {'username': user_prof_obj.username,
+                                                                 'email': user_prof_obj.email,
+                                                                 'image': user_prof_obj.image,
+                                                                 'gender': {'M': 'Male', 'F': 'Female', 'D': 'Other'}[user_prof_obj.gender],
+                                                                 'country': pycountry.countries.get(alpha_2=user_prof_obj.country).name,
+                                                                 'city': user_prof_obj.city,
+                                                                 'date_of_birth': user_prof_obj.date_of_birth},
+                                                   'title': user_prof_obj})
     except UserProfile.DoesNotExist:
         # PyCharm flags this as Unknown Attribute Reference but it still does what it's supposed to
         return redirect('users-home')
-
