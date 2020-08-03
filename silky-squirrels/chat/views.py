201fddc2bf73e3d226b91f43a4ae78a73f0901a3
caleb park <calebparky@gmail.com>
2020-08-03 14:47:03 -0500

    real last fix lint

--- silky-squirrels/chat/views.py
+++ silky-squirrels/chat/views.py
@@ -5,7 +5,7 @@ def index(request):
     # Post-Friends model implementation:
     # context = {"friends": Friends.objects.all()}
     # return render(request, "blog/home.html", context)
-    return render(request, "chat/index.html") 
+    return render(request, "chat/index.html")
 
 
 def room(request, room_name):
