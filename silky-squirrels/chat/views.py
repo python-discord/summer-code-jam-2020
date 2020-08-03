a4bbabfd94038b5cfaad7cf3717d872d84b9f3c0
caleb park <calebparky@gmail.com>
2020-08-03 14:41:52 -0500

    fix lint?

--- silky-squirrels/chat/views.py
+++ silky-squirrels/chat/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-
-# Will need to implement the Friends model    
+  
 def index(request):
+    # Post-Friends model implementation:
     # context = {"friends": Friends.objects.all()}
+    # return render(request, "blog/home.html", context)
     return render(request, "chat/index.html") 
-    #return render(request, "blog/home.html", context)
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
