57644d04595d7751519ae952d4f52940c6c714e9
George Munyoro <george.guva@outlook.com>
2020-08-03 21:09:00 +0200

    Added post creation functionality

--- graceful-grasshoppers/PySpace/posts/urls.py
+++ graceful-grasshoppers/PySpace/posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.get_posts),
-	path('create', views.create_post),
-	path('delete/<int:post_id>', views.delete_post),
-	path('update/<int:post_id>', views.update_post),
+	path('create/', views.create_post),
+	path('delete/<int:post_id>/', views.delete_post),
+	path('update/<int:post_id>/', views.update_post),
 ]
--- graceful-grasshoppers/PySpace/posts/views.py
+++ graceful-grasshoppers/PySpace/posts/views.py
@@ -1,10 +1,12 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
+from users.models import CustomUser
 
 # Create your views here.
 
@@ -22,18 +24,38 @@ def get_posts(request):
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def create_post(request):
-	pass
+    payload = request.data
+    user = request.user
+    try:
+        author = CustomUser.objects.get(id=user.id)
+        post = Post.objects.create(
+            title=payload["title"], content=payload["content"], author=author,
+        )
+        serializer = PostSerializer(post)
+        return JsonResponse(
+            {"books": serializer.data}, safe=False, status=status.HTTP_201_CREATED
+        )
+    except ObjectDoesNotExist as e:
+        return JsonResponse(
+            {"error": str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND
+        )
+    except Exception:
+        return JsonResponse(
+            {"error": "Something went terribly wrong!"},
+            safe=False,
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @api_view(["DELETE"])
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def delete_post(request):
-	pass
+    pass
 
 
 @api_view(["PATCH"])
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def update_post(request):
-	pass
+    pass
