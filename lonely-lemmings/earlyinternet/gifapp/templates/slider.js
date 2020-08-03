fb0757b780c964c648c6b85dabf219fb6ab0b07c
William Shen <shenpaiwilliam@gmail.com>
2020-08-02 23:14:56 -0600

    Feat: Slider Change Button Color Red, Green, Blue sliders dynamically change button color based on rgb values

--- lonely-lemmings/earlyinternet/gifapp/templates/slider.js
+++ lonely-lemmings/earlyinternet/gifapp/templates/slider.js
@@ -1,4 +1,22 @@
 //slider scripts
+//button color values
+var buttonRed = "0";
+var buttonGreen = "0";
+var buttonBlue = "0";
+
+//REQUIRES: color to be "red", "green", "blue", 0<= value <=255
+function setButtonColor(value, color){
+    if(color === "red") {
+        buttonRed = value;
+    } else if (color === "green") {
+        buttonGreen = value;
+    } else if (color === "blue") {
+        buttonBlue = value;
+    }
+
+    document.getElementById("color-button").style.backgroundColor =
+    'rgb(' + buttonRed + ',' + buttonGreen + ',' + buttonBlue + ')';
+}
 
 //red slider
 var redSlider = document.getElementById("redSlider");
@@ -7,6 +25,7 @@ redOutput.innerHTML = redSlider.value;
 
 redSlider.oninput = function() {
     redOutput.innerHTML = this.value;
+    setButtonColor(this.value, "red");
 }
 
 //green slider
@@ -16,6 +35,7 @@ greenOutput.innerHTML = greenSlider.value;
 
 greenSlider.oninput = function() {
     greenOutput.innerHTML = this.value;
+    setButtonColor(this.value, "green");
 }
 
 //blue slider
@@ -25,6 +45,7 @@ blueOutput.innerHTML = blueSlider.value;
 
 blueSlider.oninput = function() {
     blueOutput.innerHTML = this.value;
+    setButtonColor(this.value, "blue");
 }
 
 //width slider
