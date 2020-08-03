d1c9422ed1e870c4ec94c3183de9e7d8f3600bcb
JetDeveloping <jetjadeja@gmail.com>
2020-08-02 23:01:26 -0700

    Updated Serializers to have Identification Field

--- wiggly-weasels/Backend/flashback/fbbackend/serializers.py
+++ wiggly-weasels/Backend/flashback/fbbackend/serializers.py
@@ -8,22 +8,23 @@ Serializers for the "Model" objects located in Models.py
 class AccountSerializer(serializers.ModelSerializer): #Serializer for accounts
     class Meta:
         model = models.Account
-        fields = ('email', 'hashed_pass', 'nickname', 'bot')
+        fields = ('email', 'hashed_pass', 'nickname', 'bot', 'identification')
 
 class MessageSerializer(serializers.ModelSerializer): #Serializer for Messages
     class Meta:
         model = models.Message
-        fields = ('writer', 'content')
+        fields = ('writer', 'content', 'identification')
 
 
 class GroupSerializer(serializers.ModelSerializer): #Serializer for Groups
     class Meta:
         model = models.Group
-        fields = ('creator', 'messages', 'name')
+        fields = ('creator', 'messages', 'name', 'identification')
 
 
 class PostSerializer(serializers.ModelSerializer): #Serializer for Posts
     class Meta:
         model = models.Post
-        fields = ('creator', 'content', 'title', 'comments')
+        fields = ('creator', 'content', 'title', 'comments', 'identification')
+
 
