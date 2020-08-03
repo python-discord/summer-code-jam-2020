fc6d200e0e5df20aa0c9f17852e0a48074403247
Kenny <kfonatoko@gmail.com>
2020-08-03 19:28:27 -0400

    Fix guestbook model

--- mysterious-mice/spacebook/guestbook/forms.py
+++ mysterious-mice/spacebook/guestbook/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
+
 class Guestbook(forms.Form):
-   name = forms.CharField(max_length = 100)
-   email = forms.EmailField()
-   text = forms.CharField()
+    author = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    text = forms.CharField()
--- mysterious-mice/spacebook/guestbook/models.py
+++ mysterious-mice/spacebook/guestbook/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Guestbook(models.Model):
-    author = models.TextField()
+    author = models.CharField(max_length=100)
     text = models.TextField()
     email = models.EmailField()
     published_date = models.DateTimeField(blank=True, null=True)
