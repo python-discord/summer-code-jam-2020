fdd5d4dc6df00c4245fe50462252da8b45b6d3f4
Braindead <albertopoljak@gmail.com>
2020-08-03 23:07:23 +0200

    Moved templates from global root folder to their respective app template folder.
    Apps helps organize files so templates should be in their respective apps.

--- imaginary-iguanas/mysite/mysite/settings.py
+++ imaginary-iguanas/mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
