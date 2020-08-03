0086a73e6c71955a809fa22b707b55a5905ced4f
caleb park <calebparky@gmail.com>
2020-08-03 17:19:21 -0500

    Reformatting

--- silky-squirrels/users/views.py
+++ silky-squirrels/users/views.py
@@ -9,7 +9,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Your account has been created! You are now able to log in")
+            messages.success(request, "Your account has been created! You are now able to log in.")
             return redirect("login")
     else:
         form = UserRegisterForm()
