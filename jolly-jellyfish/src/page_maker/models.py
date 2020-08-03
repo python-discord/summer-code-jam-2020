ec8d1a13fb4c90f96edf5086e2c3efba3b1a1f22
tameTNT <30503695+tametnt@users.noreply.github.com>
2020-08-03 16:47:51 +0100

    Update models.py and adapt templates accordingly.
    Change models.py (and be extension forms.py) with some path changes and
    use of RelativeFilePathField (pipenv install required) instead of
    models.FilePathField for easier stylesheet access within templates.
    
    Reload the database files too -
    delete original db and migrations and start afresh.
    Delete residual file: generic_template.html no longer required.
    
    Edit base.html to be able to access stylesheet by static path.
    Edit webpage_list.html and webpage_view.html with minor formatting
    changes for when there are no sites and to actually load user images +
    prevent errors when no images present.

--- jolly-jellyfish/src/page_maker/models.py
+++ jolly-jellyfish/src/page_maker/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 from django.core.validators import validate_image_file_extension
 from django.db import models
 from django.utils import timezone
+from relativefilepathfield.fields import RelativeFilePathField
 
 
 def get_theme_path():
-    return str(Path(settings.BASE_DIR) / 'static' / 'themes')
+    return str(Path(settings.BASE_DIR) / 'static')
 
 
 class Template(models.Model):
@@ -16,11 +17,17 @@ class Template(models.Model):
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # todo: option to upload own stylesheet rather than select from existing
+    #  potentially upload to user_themes within static not media? -
+    #  makes finding easier below in RelativeFilePathField
     # style_sheet = models.FileField(null=False, blank=False,
     #                      upload_to='themes/', validators=[FileExtensionValidator(['css'])])
 
+    # model.FilePathField() does not have a url attribute,
+    # so accessing the files within a html template is awkward.
+    # Using RelativeFilePathField allows for using django static support easily within the template
+
     # match uses regex applied to base filename only(.*=any characters, \.=., $=end of string)
-    style_sheet = models.FilePathField(path=get_theme_path, recursive=True, match='.*\.css$')
+    style_sheet = RelativeFilePathField(path=get_theme_path, recursive=True, match='.*\.css$')
 
     def __str__(self):
         return self.name
@@ -31,7 +38,7 @@ def get_user_dir_path(instance, filename):
 
 
 class Webpage(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
     date_created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # todo: default to imgkit generated thumbnail
@@ -42,26 +49,20 @@ class Webpage(models.Model):
     votes = models.IntegerField(default=0)
 
     user_title = models.CharField(max_length=100, verbose_name='page title')
-    # todo: does text input need validation/sanitization? (e.g. removing < >)
     user_text_1 = models.TextField(verbose_name='subtitle paragraph')
     user_text_2 = models.TextField(verbose_name='main body paragraph')
     user_text_3 = models.TextField(verbose_name='closing paragraph')
 
-    # todo: make sure background images repeat/tessellate in css if a background image is uploaded
+    # todo: support for background images with custom templates
     user_image_1 = models.ImageField(
-        null=True, blank=True, verbose_name='background image',
+        null=True, blank=True, verbose_name='header image', max_length=200,
         upload_to=get_user_dir_path, validators=[validate_image_file_extension])
     user_image_2 = models.ImageField(
-        null=True, blank=True, verbose_name='header image',
+        null=True, blank=True, verbose_name='main image', max_length=200,
         upload_to=get_user_dir_path, validators=[validate_image_file_extension])
     user_image_3 = models.ImageField(
-        null=True, blank=True, verbose_name='main image',
+        null=True, blank=True, verbose_name='footer image', max_length=200,
         upload_to=get_user_dir_path, validators=[validate_image_file_extension])
-    user_image_4 = models.ImageField(
-        null=True, blank=True, verbose_name='footer image',
-        upload_to=get_user_dir_path, validators=[validate_image_file_extension])
-
-    # todo: for tameTNT's reference: https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/
 
     def was_created_recently(self):
         pass
