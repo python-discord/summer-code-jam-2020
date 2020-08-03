c80015c9c31c85c142caf60d24564e0ed1023286
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2020-08-03 11:03:45 +0100

    Added minimise button onclick function

--- juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
+++ juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
@@ -1,11 +1,11 @@
-function wm_hide(divclass) {
+function wm_minimise(divclass) {
     var x = document.getElementsByClassName(divclass);
     for (var i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
 }
 
-function wm_show(divclass) {
+function wm_restore(divclass) {
     var x = document.getElementsByClassName(divclass);
     for (var i = 0; i < x.length; i++) {
         x[i].style.display = "block";
