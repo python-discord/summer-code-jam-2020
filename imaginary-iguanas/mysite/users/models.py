ba42b806cb00251388277aaaf05b218e013addec
Braindead <albertopoljak@gmail.com>
2020-08-04 01:04:14 +0200

    Bugfix Profile model.
    As there's OneToOne field it should inherit from Model not User.
    Fixed Profile dunders.

--- imaginary-iguanas/mysite/users/models.py
+++ imaginary-iguanas/mysite/users/models.py
@@ -3,14 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(User):
+class Profile(models.Model):
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
         ('D', 'Other')
     )
-    COUNTRY_CHOICES = list((country.alpha_2, country.name) for country in pycountry.countries)
 
+    # TODO maybe use django-countries that does this for us, there are nice flags too
+    COUNTRY_CHOICES = ((country.alpha_2, country.name) for country in pycountry.countries)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_pfp.jpg', upload_to='profile_pics')
     gender = models.CharField(null=True, max_length=1, choices=GENDER_CHOICES)
     country = models.CharField(null=True, max_length=2, choices=COUNTRY_CHOICES)
@@ -18,7 +21,10 @@ class Profile(User):
     date_of_birth = models.DateField(null=True)
 
     def __str__(self):
-        return f'{self.username}\'s profile'
+        return f'{self.user.username}\'s profile'
 
     def __repr__(self):
-        return f'<Profile> {repr(self.username)} {repr(self.email)} {repr(self.image)} {repr(self.gender)} {repr(self.country)} {repr(self.city)} {repr(self.date_of_birth)}'
+        return (
+            f'<Profile> {repr(self.user.username)} {repr(self.user.email)} {repr(self.image)} {repr(self.gender)} '
+            f'{repr(self.country)} {repr(self.city)} {repr(self.date_of_birth)}'
+        )
