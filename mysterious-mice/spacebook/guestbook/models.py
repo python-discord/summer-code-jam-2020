0b1d03c69827e8409bee87fa7d1b8ff92690f5f0
Cameron Smart <smartcameron@gmail.com>
2020-08-03 10:10:17 -0700

    fixed error in guestbook/models.py

--- mysterious-mice/spacebook/guestbook/models.py
+++ mysterious-mice/spacebook/guestbook/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Guestbook(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    email = models.EmailField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
