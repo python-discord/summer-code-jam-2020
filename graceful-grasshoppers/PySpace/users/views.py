aa92638b12efcfc3f4d01d2ffc69213d11516211
George Munyoro <george.guva@outlook.com>
2020-08-03 16:16:41 +0200

    Added user list filtering with  query parameter

--- graceful-grasshoppers/PySpace/users/views.py
+++ graceful-grasshoppers/PySpace/users/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import generics
+import api.models as api_models
 from . import models
 from . import serializers
 
@@ -7,5 +8,13 @@ from . import serializers
 
 
 class UserListView(generics.ListAPIView):
-    queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
+
+    def get_queryset(self):
+        """Optionally restricts the returned users to a given user 
+        by filtering against a `username` query parameter in the URL"""
+        queryset = models.CustomUser.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        return queryset
