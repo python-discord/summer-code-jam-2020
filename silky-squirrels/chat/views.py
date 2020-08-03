8c61d6991a7f06c67f32288ae5cd5375ad35d46b
Kevin Mui <kmui2@wisc.edu>
2020-08-02 20:59:41 -0500

    :sparkles: Add username to messages

--- silky-squirrels/chat/views.py
+++ silky-squirrels/chat/views.py
@@ -6,4 +6,4 @@ def index(request):
 
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    return render(request, "chat/room.html", {"room_name": room_name, "username": request.user.username})
