268c04034ae7a74f503060454edec2a10ca52d2d
alex t <sloopooicecream@gmail.com>
2020-08-03 15:36:25 -0400

    improved wikipedia webscrape with using the search bar, testing if the page exists first

--- talented-tigers/spydir/generate_page.py
+++ talented-tigers/spydir/generate_page.py
@@ -18,16 +18,28 @@ def generate_name():
 
 # This function will be used to generate text for the info style pages. For now, it downloads from wikipedia
 def generate_information(page_name):
-    # First searches noun in wikipedia search
-    search = wikipedia.search(page_name, results=1)
+    try:
+        # First tries to go to the page url
+        result = wikipedia.summary(page_name)
+        return result
+    except wikipedia.exceptions.DisambiguationError as e:
+        # If the page it enters is a wikipedia "disambiguation" page
+        print(e.options[0])
+        result = wikipedia.summary(e.options[0])
+        return result
+    except wikipedia.exceptions.PageError:
+        # If the page doesnt exist, performs a search
+        search = wikipedia.search(page_name, results=1)
 
-    # Tries to return the contents of the first result
+    # Tries to return the contents of the first search result
     try:
         result = wikipedia.summary(search[0])
     except IndexError:
         # If the search yielded no results
         result = "Under Construction"
-    except wikipedia.DisambiguationError as e:
+    except wikipedia.exceptions.DisambiguationError as e:
         # If the page it enters is a wikipedia "disambiguation" page
+        print(e.options[0])
         result = wikipedia.summary(e.options[0])
+
     return result
