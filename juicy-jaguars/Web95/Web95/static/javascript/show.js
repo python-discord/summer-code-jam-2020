a7cdf4f290347fe9a490bf8b5f3d61ba30cedc8a
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2020-08-03 10:49:00 +0100

    Added window manager

--- juicy-jaguars/Web95/Web95/static/javascript/show.js
+++
@@ -1,8 +0,0 @@
-function toggleShow(div) {
-    var x = document.getElementById(div);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}
---
+++ juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
@@ -0,0 +1,9 @@
+function hide(div) {
+    var x = document.getElementById(div);
+    x.style.display = "none";
+}
+
+function show(div) {
+    var x = document.getElementById(div);
+    x.style.display = "block";
+}
