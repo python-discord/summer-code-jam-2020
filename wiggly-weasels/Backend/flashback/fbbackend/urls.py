8a93a8e997b01bcd7e66f7ddda9c78675265740d
JetDeveloping <jetjadeja@gmail.com>
2020-08-02 23:02:18 -0700

    Fixed Account View Glitch

--- wiggly-weasels/Backend/flashback/fbbackend/urls.py
+++ wiggly-weasels/Backend/flashback/fbbackend/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from fbbackend import views
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
 router.register('account', views.Account_View)
-router.register('group', views.Account_View)
+router.register('group', views.Group_View)
 router.register('message', views.Message_View)
 router.register('forum_post', views.Post_View)
+
 
 urlpatterns = [
     path('', include(router.urls))
--- wiggly-weasels/Backend/flashback/fbbackend/views.py
+++ wiggly-weasels/Backend/flashback/fbbackend/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from fbbackend import models, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 '''
 Views for our Rest API.
 We went with class based views, because they were a better fit than functions
 '''
 
-class Account_View(viewsets.ModelViewSet):
+class Account_View(viewsets.ModelViewSet): #Allow you to view all Accounts and Create New ones
     queryset = models.Account.objects.all()
     serializer_class = serializers.AccountSerializer
 
-class Group_View(viewsets.ModelViewSet):
+class Group_View(viewsets.ModelViewSet): #Allow you to view all Groups and Create New ones
     queryset = models.Group.objects.all()
     serializer_class = serializers.GroupSerializer
 
-class Message_View(viewsets.ModelViewSet):
+class Message_View(viewsets.ModelViewSet): #Allow you to view all Messages and Create New ones
     queryset = models.Message.objects.all()
     serializer_class = serializers.MessageSerializer
 
-class Post_View(viewsets.ModelViewSet):
+class Post_View(viewsets.ModelViewSet): #Allow you to view all Posts and Create New ones
     queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
+
+
+
+
+
+
 
