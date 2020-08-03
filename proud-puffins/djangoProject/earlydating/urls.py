cb1b7979d37ddc9d8632f1d3bed536122fbbc4c5
Ryan <ryanj.johnson@gmail.com>
2020-08-02 18:52:17 -0700

    Fixed some linting issues.

--- proud-puffins/djangoProject/earlydating/urls.py
+++ proud-puffins/djangoProject/earlydating/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='earlydating-home'),
-    path('DateMatcher/',views.DateMatcher,name='earlydating-DateMatcher'),
+    path('DateMatcher/', views.DateMatcher, name='earlydating-DateMatcher'),
     path('about/', views.about, name='earlydating-about'),
     path('YourProfile/<str:pk>/', views.your_profile, name='earlydating-yourprofile')
 ]
