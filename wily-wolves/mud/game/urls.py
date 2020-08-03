8e2dfc2cda38585cc9d1a27d97e2d01d667b93b5
François Hardy <snaptraks@gmail.com>
2020-08-02 21:34:02 -0400

    Create register view

--- wily-wolves/mud/game/urls.py
+++ wily-wolves/mud/game/urls.py
@@ -7,6 +7,6 @@ app_name = 'game'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='game/login.html'), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- wily-wolves/mud/game/views.py
+++ wily-wolves/mud/game/views.py
@@ -1,10 +1,15 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the game index.")
 
 
-def register(request):
-    return HttpResponse("This is the register page.")
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+    template_name = 'game/register.html'
