0f0a62070b1cb30b8b9ab7425b388ff71d3c4ee3
martinkozle <martinkozle@users.noreply.github.com>
2020-08-03 20:23:47 +0200

    Added dummy index view and added url

--- festive-ferrets/backend/backend/urls.py
+++ festive-ferrets/backend/backend/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from nchan import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('nchan/', views.index),
+    path('', views.index)
 ]
--- festive-ferrets/backend/nchan/views.py
+++ festive-ferrets/backend/nchan/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Index loaded successfully')
