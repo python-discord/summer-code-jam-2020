215cdf50161eeedca9021d8bf2cb6f3cd2d01465
caleb park <calebparky@gmail.com>
2020-08-03 14:45:09 -0500

    real fix lint

--- silky-squirrels/chat/views.py
+++ silky-squirrels/chat/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-  
+
+
 def index(request):
     # Post-Friends model implementation:
     # context = {"friends": Friends.objects.all()}
     # return render(request, "blog/home.html", context)
     return render(request, "chat/index.html") 
+
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
