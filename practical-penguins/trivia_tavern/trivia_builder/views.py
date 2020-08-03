ac232be45f6ff6b93304cead521f4e8eb566bd9b
kkawabat <kakwabat@asu.edu>
2020-08-02 21:54:38 -0700

    Added barebone trivia_quiz/question models and forms.

--- practical-penguins/trivia_tavern/trivia_builder/views.py
+++ practical-penguins/trivia_tavern/trivia_builder/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
+from django.db import transaction, IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,7 +11,16 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from trivia_builder.models import TriviaQuiz
+
+from trivia_builder.forms import TriviaQuizForm, TriviaQuestionFormSet
+from trivia_builder.models import TriviaQuiz, TriviaQuestion
+
+
+class PassRequestToFormViewMixin:
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
 
 
 class UserTriviaQuizListView(ListView):
@@ -26,13 +38,39 @@ class TriviaQuizDetailView(DetailView):
     model = TriviaQuiz
 
 
-class TriviaQuizCreateView(LoginRequiredMixin, CreateView):
+class TriviaQuizCreateView(PassRequestToFormViewMixin, LoginRequiredMixin, CreateView):
     model = TriviaQuiz
-    fields = ['name', 'questions']
+    fields = ['name']
+    template_name = 'trivia_builder/triviaquiz_form.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        quiz_form = TriviaQuizForm(request.POST)
+        question_formset = TriviaQuestionFormSet(request.POST)
+        new_questions = []
+        if quiz_form.is_valid() and question_formset.is_valid():
+            quiz_form.instance.author = self.request.user
+            quiz = quiz_form.save()
+            for question_form in question_formset.forms:
+                question_form.instance.quiz = quiz
+                question = question_form.save()
+                new_questions.append(question)
+
+            try:
+                with transaction.atomic():
+                    TriviaQuestion.objects.bulk_create(new_questions)
+                    quiz.save()
+
+                    # And notify our users that it worked
+                    messages.success(request, 'You have created your quiz.')
+
+            except IntegrityError:  # If the transaction failed
+                messages.error(request, 'There was an error saving your profile.')
+                return redirect(reverse('main_hub'))
+
+    def get(self, request, *args, **kwargs):
+        quiz_form = TriviaQuizForm()
+        question_formset = TriviaQuestionFormSet()
+        return render(request, self.template_name, {'quiz_form': quiz_form, 'question_formset': question_formset})
 
 
 class TriviaQuizUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
--- practical-penguins/trivia_tavern/trivia_tavern/urls.py
+++ practical-penguins/trivia_tavern/trivia_tavern/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('trivia_hub.urls')),
+    path('', include('trivia_builder.urls')),
 ]
 
 
