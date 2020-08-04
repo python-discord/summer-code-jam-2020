7d94fdff3eea154f19a7d13d07e69e0f7d65928e
Kevin Sheng <kevinsheng2005@gmail.com>
2020-08-04 07:36:02 -0700

    ok simple mistake
    the redirects work now

--- observant-otters/forumthing/forum/models.py
+++ observant-otters/forumthing/forum/models.py
@@ -17,7 +17,7 @@ class Thread(models.Model):
         return f'Thread with title {self.title} created on {self.date_created}'
 
     def get_absolute_url(self):
-        return reverse('threads-single', kwargs={'pk': self.pk})
+        return reverse('threads-single', kwargs={'id': self.pk})
 
 
 class Message(models.Model):
