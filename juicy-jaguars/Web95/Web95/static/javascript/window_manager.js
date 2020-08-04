20c8e339c7807bc685de4d8836299eb188d728ad
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-04 13:15:14 +0100

    Modified window_manager.js to change approach to setting up window button funcs

--- juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
+++ juicy-jaguars/Web95/Web95/static/javascript/window_manager.js
@@ -1,68 +1,70 @@
-function wm_minimise(divId) {
-    var x = document.getElementById(divId);
-    x.style.display = "none";
-    x.windowstate = "minimised";
-}
+function wm_setup(elem) {
+    var x = elem;
 
-function wm_restore(divId) {
-    var x = document.getElementById(divId);
-    x.style.display = "block";
-    x.windowstate = "focused";
-}
+    x.getElementsByClassName("window-btn-minimise")[0].onclick = wm_minimise;
+    x.getElementsByClassName("window-btn-maximise")[0].onclick = wm_maximise;
 
-function wm_restore_minimise_toggle(divId) {
-    var x = document.getElementById(divId);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        x.windowstate = "focused";
-    } else {
+    function wm_minimise(e) {
         x.style.display = "none";
         x.windowstate = "minimised";
     }
-}
 
-function wm_maximise(divId) {
-    var x = document.getElementById(divId);
+    function wm_restore(e) {
+        x.style.display = "block";
+        x.windowstate = "focused";
+    }
 
-    if (x.maximised == "true") {
-        x.style.top = x.oldtop, x.style.left = x.oldleft;
-        x.style.width = x.oldwidth.toString()+"px";
-        x.style.height = x.oldheight.toString()+"px";
+    function wm_maximise(e) {
+        if (x.maximised == "true") {
+            x.style.top = x.oldtop, x.style.left = x.oldleft;
+            x.style.width = x.oldwidth.toString()+"px";
+            x.style.height = x.oldheight.toString()+"px";
 
-        x.maximised = "false";
-    } else {
-        x.oldtop = x.style.top, x.oldleft = x.style.left;
-        x.style.top = "0px", x.style.left = "0px";
+            x.maximised = "false";
+        } else {
+            x.oldtop = x.style.top, x.oldleft = x.style.left;
+            x.style.top = "0px", x.style.left = "0px";
 
-        x.oldwidth = x.offsetWidth, x.oldheight = x.offsetHeight;
-        x.style.width = (window.innerWidth).toString()+"px";
-        x.style.height = (window.innerHeight - document.getElementById("taskbar").offsetHeight).toString()+"px";
+            x.oldwidth = x.offsetWidth, x.oldheight = x.offsetHeight;
+            x.style.width = (window.innerWidth).toString()+"px";
+            x.style.height = (window.innerHeight - document.getElementById("taskbar").offsetHeight).toString()+"px";
 
-        x.maximised = "true";
+            x.maximised = "true";
 
-        console.log(window.innerWidth.toString()+"px");
+            console.log(window.innerWidth.toString()+"px");
+        }
+    }
+
+    function wm_focus(e) {
+        x.windowstate = "focused";
+        x.classList.add("card-tertiary");
+    }
+
+    function wm_unfocus(e) {
+        x.windowstate = "unfocused";
+        x.classList.remove("card-tertiary");
     }
 }
 
-function wm_focus(divId) {
-    var x = document.getElementById(divId);
-
-    x.windowstate = "focused";
-}
-
-function wm_unfocus(divId) {
-    var x = document.getElementById(divId);
-
-    x.windowstate = "unfocused";
+function wl_setup(elem, w_id) {
+    var x = document.getElementById(w_id);
+    elem.onclick = wm_restore_minimise_toggle;
+    function wm_restore_minimise_toggle(e) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+            x.windowstate = "focused";
+        } else {
+            x.style.display = "none";
+            x.windowstate = "minimised";
+        }
+    }
 }
 
 function setupWindowManagement(class_name) {
     var windows = document.getElementsByClassName(class_name);
-    console.log(windows.length);
 
     for (var i = 0; i < windows.length; i++) {
-        windows[i].getElementsByClassName("window-btn-minimise")[0].onclick = function(e) {wm_minimise(e.target.parentElement.parentElement.id)};
-        windows[i].getElementsByClassName("window-btn-maximise")[0].onclick = function(e) {wm_maximise(e.target.parentElement.parentElement.id)};
+        wm_setup(windows[i])
     }
 }
 
