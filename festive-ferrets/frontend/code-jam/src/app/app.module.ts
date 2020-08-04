5091c1f8a8bfa0e9abc81ea804ea3fc257f665d0
AnixDrone <tomislavignjatov@rocketmail.com>
2020-08-04 18:56:25 +0200

    emergency commit

--- festive-ferrets/frontend/code-jam/src/app/app.module.ts
+++ festive-ferrets/frontend/code-jam/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainSiteComponent } from './main-site/main-site.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSiteComponent
   ],
   imports: [
     BrowserModule,
---
+++ festive-ferrets/frontend/code-jam/src/app/main-site/main-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-site',
+  templateUrl: './main-site.component.html',
+  styleUrls: ['./main-site.component.scss']
+})
+export class MainSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
