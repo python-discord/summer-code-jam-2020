cd69bbe2edd143598808685a97b828359cc699ce
George Parhomenko <parhomenkogeorge@gmail.com>
2020-08-04 17:21:03 +0300

    Change host to work with docker

--- dreamy-dingos/app/settings.py
+++ dreamy-dingos/app/settings.py
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)]
+            "hosts": [('redis', 6379)]
         },
     },
 }
