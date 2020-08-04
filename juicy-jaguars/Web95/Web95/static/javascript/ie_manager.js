dad0e2a6199d72b1088b48dbb02c0e764de001f6
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2020-08-04 20:03:53 +0100

    IE now shows websites

---
+++ juicy-jaguars/Web95/Web95/static/javascript/ie_manager.js
@@ -0,0 +1,11 @@
+function addr_bar_enter() {
+    //document.getElementById("ie-iframe").src = "https://test.com/";
+    document.getElementById("ie-iframe").src = document.getElementById("ie-addr-bar").value;
+}
+
+document.getElementById("ie-addr-bar").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addr_bar_enter();
+    }
+});
