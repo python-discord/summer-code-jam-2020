a54ac701a9dfb9df939804b901bf103ac458c366
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-08-04 13:31:10 -0400

    Rewrote basic models

--- oscillating-oysters/twentytwentymud/MUD/admin.py
+++ oscillating-oysters/twentytwentymud/MUD/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Room, Player
+
+admin.site.register(Room)
+admin.site.register(Player)
+
 # Register your models here.
--- oscillating-oysters/twentytwentymud/MUD/models.py
+++ oscillating-oysters/twentytwentymud/MUD/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=512)
+    connections = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Player(models.Model):
     name = models.CharField(max_length=50)
-    x = models.IntegerField(default=0)
-    y = models.IntegerField(default=0)
-    z = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, null=True, on_delete=models.SET_NULL)
 
-
-class Room(models.Model):
-    description = models.CharField(max_length=512)
-    x = models.IntegerField(default=0)
-    y = models.IntegerField(default=0)
-    z = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
