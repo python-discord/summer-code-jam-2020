3201291f558bf0fd25cfbf4718a1ba92405904e5
William Shen <shenpaiwilliam@gmail.com>
2020-08-03 20:23:47 -0600

    Feat: Add Class Stubs For GifApp - Create class stubs for gifapp - Todo: establish concrete relationships for fields with None values - Todo: implement classes

--- lonely-lemmings/earlyinternet/gifapp/models.py
+++ lonely-lemmings/earlyinternet/gifapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    author = None
+    date_created = models.DateTimeField()
+    date_last_modified = None
+    picture = None
+
+    class Meta:
+        abstract = True
+
+    def get_absolute_url(self):
+        pass
+
+
+class GifProject(Project):
+    image_list = None
+    image_gif = None
+
+    def __str__(self):
+        pass
+
+    def compile_images(self):
+        pass
+
+
+class ImageProject(Project):
+    def __str__(self):
+        pass
+
+
+class Image(models.Model):
+    project = None
+    image_name = models.CharField(max_length=50)
+    image_data = models.ImageField()
+
+    def __str__(self):
+        pass
+
+
+class User(models.Model):
+    projects = None
+    registration_date = None
+
+    def __str__(self):
+        pass
+
+    def new_project(self):
+        pass
