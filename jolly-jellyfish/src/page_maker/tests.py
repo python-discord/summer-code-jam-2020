ca51c67fe1c543c8eab2c88dbce121e6ca164900
A5rocks <40616000+A5rocks@users.noreply.github.com>
2020-08-04 09:47:34 +0900

    Test logging in and the home view
    While not much, this test can act like a template for others.
    Here's some notes on the tests for my later perusal:
     - `client.<method>(<relative url>).context` will tell you what was
       passed into the returned template. This way, we don't have to
       hard-code html into our tests!
    
     - `client.login(username=<username>, password=<password>)` just
       sets the authentication cookie (or something?) Then
       `client.logout()` will remove said authentication. Seems useful!
    
     - cookies persist through calls automatically!
    
     - CSRF protection is disabled - no need to inspect a form or
       something cumbersome to do like that.
    
     - `django.tests.TestCase` is a subclassed `unittest.TestCase`,
       which lets you use your fancy `self.assertIn`, `self.assertIs`,
       etc.

--- jolly-jellyfish/src/page_maker/tests.py
+++ jolly-jellyfish/src/page_maker/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+USERNAME = 'abcdef'
+
+
+class TestUserRegistration(TestCase):
+    def test_register_and_home(self):
+        # assumptions (aka what this tests):
+        #  - `/user/register` exists.
+        #  - it takes certain params.
+        #  - it also logs us in.
+        #  - `/` exists.
+        #  - it tells us our username.
+        #  - we use django's built-in auth system.
+
+        # we don't want to leak our
+        # signed in user to other
+        # tests that might be running
+        client = Client()
+        password = 'foobar < xyzzy'
+
+        params = {
+            'username': 'foo',
+            'email': 'foo@bar.com',
+            'password1': password,
+            'password2': password
+        }
+
+        # note(a5): it seems the following POST request does
+        #   not set the user credentials?
+        client.post('/users/register', params, follow=True)
+        client.login(username='foo', password=password)
+        res = client.get('/').content
+
+        self.assertIn(b'foo', res, 'Our username does not appear in the home view.')
+
+        client.logout()
+        res = client.get('/').content
+
+        self.assertNotIn(b'foo', res, 'Our username was already in the home view.')
