292fcf686b4c7b69ad341e074340923c90f0a368
MW <maksw982@gmail.com>
2020-08-04 23:37:55 +0200

    Fix migrations file issue

--- efficient-eagles/early_internet/main/models.py
+++ efficient-eagles/early_internet/main/models.py
@@ -86,6 +86,6 @@ class Comment(TimeStampedModel):
     downvotes = models.PositiveIntegerField(default=0)
     body = models.CharField(max_length=10000, default='')
 
-    author = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
     comment_thread = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
