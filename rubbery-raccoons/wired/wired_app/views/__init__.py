f643618d7d623e766fd7a102afd3adff8996fb47
Rob Harrigan <rob.harrigan89@gmail.com>
2020-08-03 21:58:21 -0500

    Basic working detail and list views

---
+++ rubbery-raccoons/wired/wired_app/views/__init__.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from wired_app.views.detail import ArticleDetailView
+from wired_app.views.homepage import HomepageView
+
+
+
+def compose(request):
+    return HttpResponse("XXX TODO")
---
+++ rubbery-raccoons/wired/wired_app/views/homepage.py
@@ -0,0 +1,14 @@
+from django.views.generic.list import ListView
+from django.utils import timezone
+from wired_app.models import Article
+
+
+class HomepageView(ListView):
+    model = Article
+    paginate_by = 10
+    template_name = "wired_app/homepage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
