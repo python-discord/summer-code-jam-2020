d7f4f13150a7808c4bbf8f00cdd0b0e3878ce8f0
Yaro Kifor <yarokifor@fastmail.com>
2020-08-05 11:56:07 -0400

    Added consumer for chat

--- config/channels_routing.py
+++ config/channels_routing.py
@@ -1,5 +1,12 @@
 from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+import shinny_sheep.chat.routing as chat.routing
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
