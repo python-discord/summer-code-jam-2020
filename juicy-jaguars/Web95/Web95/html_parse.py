5369f33975760396447fe006a4e5d32171354625
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-05 12:53:45 +0100

    Added basic structure of html_parse.py

--- juicy-jaguars/Web95/Web95/html_parse.py
+++ juicy-jaguars/Web95/Web95/html_parse.py
@@ -1,1 +1,19 @@
 """Module used to parse HTML pages."""
+
+from bs4 import BeautifulSoup
+
+
+class HtmlParser:
+    """Class used to parse HTML."""
+
+    def __init__(self, html):
+        """Store HTML, setup Soup."""
+        self.html = html
+        self.soup = BeautifulSoup(html, "html.parser")
+
+    def parse(self):
+        """Run all parsing functions."""
+        for name in dir(self):
+            if name.startswith("parse_"):
+                method = getattr(self, name)
+                method()
