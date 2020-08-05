250821ff285e16b77f67aa699f2772437adb7b57
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-05 23:11:12 +0300

    Add logout URL to Django

--- cuddly-chameleons/cuddly_chameleons/retro_news/urls.py
+++ cuddly-chameleons/cuddly_chameleons/retro_news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views
 
-from retro_news.views import CustomUserCreate
+from retro_news.views import CustomUserCreate, LogOutView
 
 urlpatterns = [
     path('token/obtain/', views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
     path('user/create/', CustomUserCreate.as_view(), name='create_user'),
+    path('user/logout/', LogOutView.as_view(), name='logout'),
 ]
