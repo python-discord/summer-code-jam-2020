b0c111a39ee98a660efda921dab6d20beadc6789
akshgpt7 <akshgpt7@gmail.com>
2020-08-05 15:13:50 +0530

    extend existing templates with base.html

--- annoyed-alligators/socl_media/settings.py
+++ annoyed-alligators/socl_media/settings.py
@@ -138,4 +138,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/'
--- annoyed-alligators/socl_media/urls.py
+++ annoyed-alligators/socl_media/urls.py
@@ -21,7 +21,8 @@ from socl_media.apps.users import views as users_views
 
 urlpatterns = [
     path('', include('socl_media.apps.feed.urls')),
-    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'), name="login"),
     path('signup/', users_views.signup, name="signup"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls),
