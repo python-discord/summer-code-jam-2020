886952d7578b40a7cb811f66b632f7745d7bb7bd
A5rocks <40616000+A5rocks@users.noreply.github.com>
2020-08-05 09:48:42 +0900

    Make a second test for templates
    I also tried making a test around page creation / commenting, but I
    couldn't get Django to figure out an image was an image...
    Hopefully I find some article about doing this before the end of
    the code jam...

--- jolly-jellyfish/src/page_maker/tests.py
+++ jolly-jellyfish/src/page_maker/tests.py
@@ -1,10 +1,26 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+import io
+import base64
 
 
-USERNAME = 'abcdef'
+USERNAME = 'haha69420'
+PASSWORD = 'foobar < xyzzy'
 
 
-class TestUserRegistration(TestCase):
+# FIXME: NAME TBD
+class TestStuff(TestCase):
+    def setUp(self):
+        # we create an account with the `USERNAME` and `PASSWORD` vars
+        params = {
+            'username': USERNAME,
+            'email': 'foo@example.com',
+            'password1': PASSWORD,
+            'password2': PASSWORD
+        }
+
+        self.client.post('/users/register', params)
+        self.client.login(username=USERNAME, password=PASSWORD)
+
     def test_register_and_home(self):
         # assumptions (aka what this tests):
         #  - `/user/register` exists.
@@ -14,28 +30,34 @@ class TestUserRegistration(TestCase):
         #  - it tells us our username.
         #  - we use django's built-in auth system.
 
-        # we don't want to leak our
-        # signed in user to other
-        # tests that might be running
-        client = Client()
-        password = 'foobar < xyzzy'
+        res = self.client.get('/').content
+        self.assertIn(USERNAME.encode(), res, 'Our username does not appear in the home view.')
 
-        params = {
-            'username': 'foo',
-            'email': 'foo@bar.com',
-            'password1': password,
-            'password2': password
-        }
+        self.client.logout()
+        res = self.client.get('/').content
 
-        # note(a5): it seems the following POST request does
-        #   not set the user credentials?
-        client.post('/users/register', params, follow=True)
-        client.login(username='foo', password=password)
-        res = client.get('/').content
+        self.assertNotIn(USERNAME.encode(), res, 'Our username was already in the home view.')
 
-        self.assertIn(b'foo', res, 'Our username does not appear in the home view.')
+    def test_upload_templates(self):
+        # what this assumes:
+        # - the above tests go through
+        # - when you create a page the
+        #   available templates are
+        #   passed in. and in a certain
+        #   way.
+        # - posting `/templates/new`
+        #   creates a specified template
 
-        client.logout()
-        res = client.get('/').content
+        # example stylesheet
+        style = io.StringIO('body { display: hidden; }')
+        template_name = 'blahblahblah'
 
-        self.assertNotIn(b'foo', res, 'Our username was already in the home view.')
+        res = self.client.get('/pages/new')
+        template_entries = [entry[1] for entry in res.context['form'].fields['template_used'].choices]
+        self.assertNotIn(template_name, template_entries)
+
+        self.client.post('/templates/new', {'name': template_name, 'style_sheet': style})
+
+        res = self.client.get('/pages/new')
+        template_entries = [entry[1] for entry in res.context['form'].fields['template_used'].choices]
+        self.assertIn(template_name, template_entries)
