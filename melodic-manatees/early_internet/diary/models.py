0707de3da70c481f9c6cd672375882f0054a83c5
Colin <ccwbassett@gmail.com>
2020-08-05 09:35:21 -0700

    created diary model, added to admin

--- melodic-manatees/early_internet/diary/models.py
+++ melodic-manatees/early_internet/diary/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class DiaryEntry(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField
+    date_created = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.title
