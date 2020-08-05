ee3976d17043fa67fb2ff6a8139a8aa53f88d9f5
Ryuga <vaishnav2353@gmail.com>
2020-08-05 21:30:12 +0530

    migrations done

--- bite-sized-bears/Website/syndication_app/models.py
+++ bite-sized-bears/Website/syndication_app/models.py
@@ -13,16 +13,16 @@ class Community(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     description = models.TextField(blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    admin = models.ManyToManyField(User)
-    subscribers = models.ManyToManyField(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='community_owner')
+    admin = models.ManyToManyField(User, related_name='community_admin')
+    subscribers = models.ManyToManyField(User, related_name='community_subscribers')
     location = models.CharField(max_length=20)
 
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    publisher = models.ForeignKey(Community, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
+    publisher = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='post_publisher')
     title = models.CharField(max_length=100)
     description = models.TextField()
     views = models.ManyToManyField(User)
@@ -31,7 +31,7 @@ class Post(models.Model):
 
 class Comments(models.Model):
     id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment_post')
     content = models.TextField()
     creation_date = models.DateTimeField()
