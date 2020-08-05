02d9ce561107699f8dbd9e9fdea32af2f31bd608
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-08-05 17:07:20 +0100

    Adds jsonbody decorator to make views require POST requests, and parse the json body of the request to retrieve the parameters

--- lumpy-llamas/chat/views.py
+++ lumpy-llamas/chat/views.py
@@ -1,4 +1,16 @@
+import json
+from functools import wraps
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+
+def jsonbody(func):
+    @wraps(func)
+    @require_http_methods(['POST'])
+    def wrapper(request, *args, **kwargs):
+        data = json.loads(request.body.decode())
+        return func(request, data, *args, **kwargs)
+    return wrapper
 
 
 def chat_lobby(request):
@@ -9,7 +21,9 @@ def chat_room(request, room_name):
     return JsonResponse({'room_name': room_name})
 
 
-def check_chat_room_name(request, room_name):
-    is_valid = room_name.isalnum()
+@jsonbody
+def check_chat_room_name(request, data):
+    room_name = data.get('roomName')
+    is_valid = isinstance(room_name, str) and room_name.isalnum()
     message = 'Ok' if is_valid else 'Invalid chat room name. Names must be alphanumeric.'
     return JsonResponse({'valid': is_valid, 'message': message})
