987c4dc62fa6a914cd14be2aeedc8c3a773dc801
Dodson <dodson.dominic@gmail.com>
2020-08-05 18:09:44 -0400

    Created messages component and moved to its own file

--- whimsical-woodpeckers/www/src/py/components/messages/__init__.py
+++ whimsical-woodpeckers/www/src/py/components/messages/__init__.py
@@ -1,4 +1,24 @@
+from datetime import datetime
+
 from pyvue import Component
 
+
 class Messages(Component):
-    template = "<div>messages</div>"
+    messages = [
+        {"sender": "John Doe", "content": "lol hi", "time": datetime.now().strftime("%B %d, %Y , %H:%M")},
+        {"sender": "John Doe", "content": "what are you up to?", "time": datetime.now().strftime("%B %d, %Y , %H:%M")},
+        {"sender": "Jane Doe", "content": "nothing much. eating some ramen lol", "time": datetime.now().strftime("%B %d, %Y , %H:%M")},
+        {"sender": "John Doe", "content": "thats cool", "time": datetime.now().strftime("%B %d, %Y , %H:%M")},
+        {"sender": "John Doe", "content": "oh. brb", "time": datetime.now().strftime("%B %d, %Y , %H:%M")},
+    ]
+
+    data = {
+        'messages': messages,
+        'current_user': 'Jane Doe',
+    }
+
+    template = '<div><div v-for="message in messages" :key="message.content" v-bind:class="{ \'alert-primary\': (current_user==message.sender), \'alert-info\':(current_user!=message.sender) }" class="alert">' \
+        '<span data-toggle="tooltip" data-placement="top" v-bind:title="message.time">{{ message.sender }}</span>' \
+        ': {{ message.content }}</div></div>'
+
+    # template = "#messages"
