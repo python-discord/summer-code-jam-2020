d2e169dec75375e73a42e0aa08b1aaca89d932a8
Ryan Baker <ryan.da.baker@gmail.com>
2020-08-05 08:19:18 -0400

    Adds locations and NPCs to the DB

--- wily-wolves/mud/game/models.py
+++ wily-wolves/mud/game/models.py
@@ -1,6 +1,31 @@
 """The game models."""
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+class Location(models.Model):
+    """A game location object."""
+
+    x_coord = models.BigIntegerField(default=0, null=False)
+    y_coord = models.BigIntegerField(default=0, null=False)
+    z_coord = models.BigIntegerField(default=0, null=False)
+    description = models.TextField()
+
+
+class NPC(models.Model):
+    """A representation for NPCs."""
+
+    class NPCTypes(models.TextChoices):
+        MERCHANT = 'MC', _('Merchant')
+        ENEMY = 'EN', _('Enemy')
+        CIVILIAN = 'CV', _('Civilian')
+
+    name = models.TextField()
+    max_health = models.BigIntegerField(default=100)
+    current_health = models.BigIntegerField(default=100)
+    npc_type = models.CharField(max_length=2, choices=NPCTypes.choices, default=NPCTypes.ENEMY)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
 
 
 class Player(models.Model):
@@ -11,3 +36,4 @@ class Player(models.Model):
     max_health = models.BigIntegerField(default=100)
     current_health = models.BigIntegerField(default=100)
     experience = models.BigIntegerField(default=0)
+    location_id = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
