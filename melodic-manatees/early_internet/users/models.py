6164a6be17a752a5791e30f10303722843d03419
Eunmin Lee <58356028+eunminlee@users.noreply.github.com>
2020-08-04 15:53:24 -1000

    added background entity in user profile

--- melodic-manatees/early_internet/users/models.py
+++ melodic-manatees/early_internet/users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    background_image = models.ImageField(upload_to="backgrounds", default='backgrounds/sunrise.jpg')
 
     def __str__(self):
         return f'{self.user.username} Profile'
