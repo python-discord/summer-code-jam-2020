4e412c629f2274e99b006cb4ef7dc2dafe0370b3
MushroomMaula <maxrd79@gmail.com>
2020-08-05 11:33:48 +0200

    Add WikipediaArticle model

--- concerned-coyotes/earlyinternet/wikipedia/models.py
+++ concerned-coyotes/earlyinternet/wikipedia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class WikipediaArticle(models.Model):
+
+    title = models.CharField(max_length=256)
+    date = models.DateField()
+    url = models.URLField()
+    content = models.TextField()
