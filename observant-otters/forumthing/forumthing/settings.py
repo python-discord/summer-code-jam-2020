8c36e3f35446fe9f4f901ce96f6fcce0afdd8903
Literal Genie <frothsavd@gmail.com>
2020-08-04 21:11:13 -0400

    Enabled APPEND_SLASH in settings.

--- observant-otters/forumthing/forumthing/settings.py
+++ observant-otters/forumthing/forumthing/settings.py
@@ -101,6 +101,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+APPEND_SLASH = True
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
