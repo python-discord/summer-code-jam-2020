d5068510206f2acaba8741c46bb7d861c5828cf8
Peter DeVita <mewtwo2643@yahoo.com>
2020-08-04 23:23:43 -0400

    Remove extra space

--- whimsical-woodpeckers/www/src/py/pyvue.py
+++ whimsical-woodpeckers/www/src/py/pyvue.py
@@ -1,5 +1,6 @@
 __pragma__ ('js', "{}", "import * as VuePkg from 'vue';")
 __pragma__ ('noalias', 'default')
+__pragma__ ('noalias', 'name')
 
 JSVue = VuePkg.default
 
@@ -22,6 +23,7 @@ def clone_data(data):
 
 class Vue:
     data = {}
+    template = ""
 
     def __init__(self, el):
         self.el = el
@@ -34,3 +36,87 @@ class Vue:
         }))
 
 
+def making_component(comp):
+    def func(one, two, three):
+        console.log(one, two, three)
+        return __new__(comp(one, two))
+    return func
+
+
+class Component:
+    data = {}
+    template = ""
+    props = []
+
+    def __init__(self):
+        self.data = clone_data(self.data)
+        self._data = 0
+        methods = {func: getattr(self, func) for func in dir(self) if callable(getattr(self, func)) and not func.startswith("__")}
+        console.log(self.template)
+        self._vue = JSVue.extend({
+            "data": self.get_data,
+            "methods": methods,
+            "template": self.template,
+            "props": self.props
+        })
+        console.log(self.__class__.__name__, self._vue)
+        JSVue.component(self.__class__.__name__.lower(), self._vue)
+
+    def get_data(self):
+        self._data += 1
+        console.log(self._data)
+        return clone_data(self.data)
+
+#
+#
+# def createComponent(one, two):
+#     console.log(this, one, two)
+#
+# class Component:
+#     data = {}
+#     props = []
+#     template = ""
+#     _registration = None
+#     name = ""
+#     methods = {}
+#
+#     def __init__(self):
+#         self.name = self.name
+#         self.data = clone_data(self.data)
+#         self.methods = {func: getattr(self, func) for func in dir(self) if callable(getattr(self, func)) and not func.startswith("__")}
+#         # self._vue = __new__(JSVue.component(self.name, self.__class__))
+#
+#     @classmethod
+#     def get_data(cls):
+#         return clone_data(cls.data)
+#
+#     @classmethod
+#     def create_data(cls):
+#         # methods = {func: getattr(cls, func) for func in dir(cls) if callable(getattr(cls, func)) and not func.startswith("__")}
+#         data = clone_data(cls.data)
+#         methods = dict(cls.methods)
+#         methods['created'] = createComponent
+#         console.log(methods)
+#         return {
+#             "name": cls.name,
+#             "data": cls.get_data,
+#             "methods": methods,
+#             "template": cls.template
+#         }
+#
+#
+#
+#
+#     @classmethod
+#     def register(cls, name):
+#         name = name
+#         if not name:
+#             name = cls.__name__
+#         cls.name = name
+#         console.log(name, cls)
+#         data = cls.create_data()
+#         console.log(data)
+#         cls._registration = JSVue.component(name, data)
+#
+#
+#
