8d16a7bd90eea70cb3bc0c0a6b9f86b9f08aec7d
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-05 22:58:01 +0300

    Add logout functionality at Django side

--- cuddly-chameleons/cuddly_chameleons/cuddly_chameleons/settings.py
+++ cuddly-chameleons/cuddly_chameleons/cuddly_chameleons/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
--- cuddly-chameleons/cuddly_chameleons/retro_news/views.py
+++ cuddly-chameleons/cuddly_chameleons/retro_news/views.py
@@ -2,6 +2,7 @@ from rest_framework import permissions, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from retro_news.serializers import CustomUserSerializer
 
@@ -10,8 +11,9 @@ class CustomUserCreate(APIView):
     """Handles user creation."""
 
     permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
 
-    def post(self, request: Request, format=None):
+    def post(self, request: Request):
         """Create new user."""
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,3 +21,17 @@ class CustomUserCreate(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogOutView(APIView):
+    """Handles JWT Token deactivating."""
+
+    def post(self, request: Request):
+        """Deactivate user JWT token."""
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_200_OK)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
