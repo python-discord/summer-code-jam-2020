41cb3dd11409b535582295a71ace78c581b82178
George Parhomenko <parhomenkogeorge@gmail.com>
2020-08-05 21:09:25 +0300

    Add models for chat

---
+++ dreamy-dingos/chat/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class SimpleUser(models.Model):
+    username = models.CharField(max_length=50)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
