4444e0090e1bcfbc6205e54c4d5a043141e09cc9
martinkozle <martinkozle@users.noreply.github.com>
2020-08-05 04:03:15 +0200

    Fixed lint issues

--- festive-ferrets/backend/nchan/views.py
+++ festive-ferrets/backend/nchan/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, mixins, generics
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 
