79cd2c7e94e31a67c11e78a8ed8fd05d378b0982
tameTNT <30503695+tameTNT@users.noreply.github.com>
2020-08-05 14:00:49 +0100

    Upgrade render_img_thumbnail.py to use Selenium
    Selenium offers a major functional improvement over the previous imgkit.
    Although it may be slightly slower (probs. due to browser load).
    
    Update Pipfile and .lock with the new package requirements.
    Update README.md with new download and env variable requirements.
    Update models.py path gen function for Template.style_sheet due to newly
    added site-wide style.css which should not be accessible
    for user template creation.
    Change affected stylesheet paths used in base.html

--- jolly-jellyfish/src/page_maker/templatetags/render_img_thumbnail.py
+++ jolly-jellyfish/src/page_maker/templatetags/render_img_thumbnail.py
@@ -1,23 +1,30 @@
 import os
 from pathlib import Path
 
-import imgkit
 from PIL import Image
 from django import template
 from django.conf import settings
 from django.utils import timezone
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 register = template.Library()
 
+
 try:
-    CONFIG = imgkit.config(wkhtmltoimage=os.environ['WKHTML_TO_IMAGE'])
+    DRIVER_PATH = os.environ['SELENIUM_DRIVER']
 except KeyError:
-    raise KeyError('WKHTML_TO_IMAGE system environment variable not found.\n'
+    raise KeyError('SELENIUM_DRIVER system environment variable not found.\n'
                    'Check the README for details on setting it.\n'
                    'If you have already done so and this error persists, try restarting your PC.')
 
 
-# todo: functionality to regenerate thumbnail if page has changed
+OPTIONS = Options()
+OPTIONS.add_argument("--headless")
+OPTIONS.add_argument("--window-size={}".format("1920,1080"))
+OPTIONS.add_argument("--hide-scrollbars")
+
+
 @register.simple_tag()
 def render_img_thumbnail(request, url_to_render, webpage_obj, example=False):
     """
@@ -35,23 +42,22 @@ def render_img_thumbnail(request, url_to_render, webpage_obj, example=False):
     # To avoid infinite loops as the page calls itself to render,
     # only run the function if url doesn't end with '?rendering=true' (added below)
     rendering_bool = bool(request.GET.get('rendering', ''))
+
+    # i.e. thumbnail is outdated or does not exist yet (thumbnail_edit_date datetime set to 1, 1, 1)
     if webpage_obj.thumbnail_edit_date < webpage_obj.last_edit_date and not rendering_bool:
-        # i.e. thumbnail is outdated or does not exist yet (datetime set to 1, 1, 1)
 
         base_dir = Path(settings.BASE_DIR)
         media_dir = Path(settings.MEDIA_ROOT)
 
         url = request.build_absolute_uri(url_to_render) + '?rendering=true'
         # Can't use only the obj's name as this may contain characters not permitted by file system
-        image_path = Path('thumbnails') / f'pg-tb-{str(webpage_obj.id)}.png'
+        image_path = Path('thumbnails') / f'pg-thumb-{str(webpage_obj.id)}.png'
         full_os_path = str(base_dir / media_dir / image_path)
 
-        # Note: this function had exited with 'code 1, due to unknown error' in the past.
-        # At the time, this was deduced to be due to an output_path that was too long
-        # (i.e. over Windows 10's 256 character file path limit).
-        #
-        # It also raises: "ConnectionResetError: [WinError 10054] ...
-        # An existing connection was forcibly closed by the remote host" in console.
+        driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=OPTIONS)
+
+        # NB: This function raises: "ConnectionResetError: [WinError 10054] ...
+        # An existing connection was forcibly closed by the remote host" in console."
         # This has no functional effect on the program and
         # is likely just a side-effect of the server being pinged from within itself essentially.
         #
@@ -59,8 +65,9 @@ def render_img_thumbnail(request, url_to_render, webpage_obj, example=False):
         # This means that, in this current dev environment where there are multiple OSs at play,
         # having a functioning webpage, while this function executes in the background,
         # doesn't seem possible 😭
-        imgkit.from_url(url, output_path=full_os_path, config=CONFIG,
-                        options={'quiet': '', 'format': 'png'})
+        driver.get(url)
+        driver.save_screenshot(full_os_path)
+        driver.close()
 
         img = Image.open(full_os_path)
         img.thumbnail(size=(500, 500))
