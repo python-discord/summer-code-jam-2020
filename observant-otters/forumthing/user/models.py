1b4ce7934bb99a548a66de414b091a2d82c4593c
Literal Genie <frothsavd@gmail.com>
2020-08-04 20:32:08 -0400

    restructured forum/views.py

--- observant-otters/forumthing/user/models.py
+++ observant-otters/forumthing/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class ForumUser(AbstractUser):
     username = models.TextField(unique=True)
     nickname = models.TextField(default='')
