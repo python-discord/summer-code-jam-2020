93b1cf7c78c69ca138c261d592653fee48a444f1
kkawabat <kakwabat@asu.edu>
2020-08-05 01:01:04 -0700

    added views and templates to trivia_runner app

--- practical-penguins/trivia_tavern/trivia_runner/views.py
+++ practical-penguins/trivia_tavern/trivia_runner/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from trivia_runner.models import ActiveTriviaQuiz
+
+
+def setup(request, pk):
+    active_trivia_quiz = get_object_or_404(ActiveTriviaQuiz, pk=pk)
+    return render(request, 'activequiz_setup.html', {'active_trivia_quiz': active_trivia_quiz})
+
+
+def question(request):
+    pass
+
+
+def end_screen(request):
+    pass
