e3df4b5c4db49ba30adb780cc0719548bf0f749c
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-08-05 22:28:57 +0100

    Fix bug in chat urls; string matching saw 'checkname' as chat room name instead of endpoint

--- lumpy-llamas/chat/urls.py
+++ lumpy-llamas/chat/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.chat_lobby, name='chat_lobby'),
-    path('<str:room_name>/', views.chat_room, name='chat_room'),
-    path('checkname/', views.check_chat_room_name, name='check_chat_room_name')
+    path('checkname/', views.check_chat_room_name, name='check_chat_room_name'),
+    path('room/<str:room_name>/', views.chat_room, name='chat_room'),
 ]
