9455dbf1f3e330de4f50ff248e9d2e7d1f299de6
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-05 12:23:58 +0100

    Fixed issue where some URL characters werent encoded by IE

--- juicy-jaguars/Web95/Web95/static/javascript/ie_manager.js
+++ juicy-jaguars/Web95/Web95/static/javascript/ie_manager.js
@@ -1,6 +1,7 @@
 function addr_bar_enter() {
     //document.getElementById("ie-iframe").src = "https://test.com/";
-    document.getElementById("ie-iframe").src = "/page/"+encodeURI(document.getElementById("ie-addr-bar").value);
+    document.getElementById("ie-iframe").src = "/page/"+encodeURIComponent(document.getElementById("ie-addr-bar").value);
+    console.log("/page/"+encodeURI(document.getElementById("ie-addr-bar").value));
 }
 
 document.getElementById("ie-addr-bar").addEventListener("keyup", function(event) {
