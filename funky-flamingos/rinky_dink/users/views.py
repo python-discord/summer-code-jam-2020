0bd865e63c0194500e3a1839cf8f9e6e13e7bbaa
volt9801 <pranshuj73@gmail.com>
2020-08-06 01:54:51 +0530

    tweaked index and dashboard

--- funky-flamingos/rinky_dink/users/views.py
+++ funky-flamingos/rinky_dink/users/views.py
@@ -3,7 +3,6 @@ import os
 import random
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
