ebc53c5c8ff5843014805dcec9d03cc3003be5ef
Ryuga <vaishnav2353@gmail.com>
2020-08-05 16:12:42 +0530

    project name changed

--- bite-sized-bears/Website/manage.py
+++ bite-sized-bears/Website/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Website.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rss_it.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
