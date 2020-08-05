299a1c8defe244994e3938be0303c1fc960f092b
Dodson <dodson.dominic@gmail.com>
2020-08-05 14:00:20 -0400

    Created basic message component

--- whimsical-woodpeckers/www/src/py/main.py
+++ whimsical-woodpeckers/www/src/py/main.py
@@ -1,27 +1,52 @@
-from pyvue import Vue
+from datetime import datetime
+
+from pyvue import Vue, Component
+
+messages = [
+    {"sender": "John Doe", "content": "lol hi", "time": str(datetime.now())},
+    {"sender": "John Doe", "content": "what are you up to?", "time": str(datetime.now())},
+    {"sender": "Jane Doe", "content": "nothing much. eating some ramen lol", "time": str(datetime.now())},
+    {"sender": "John Doe", "content": "thats cool", "time": str(datetime.now())},
+    {"sender": "John Doe", "content": "oh. brb", "time": str(datetime.now())},
+]
 
 
-class MessageList(Vue):
+class App(Vue):
+    data = {}
+
+
+class MessageComponent(Component):
     data = {
-        "current": 0,
-        "textbox": "",
-        "messages": [
-            "Hi there!",
-            "Welcome to PyVue!",
-            "Hope it goes well for you!"
-        ]
+        'messages': messages,
     }
 
-    def next_message(self):
-        self.data['current'] += 1
-        if self.data['current'] > len(self.data['messages']) - 1:
-            self.data['current'] = 0
-
-    def add_message(self):
-        self.data['messages'].append(self.data['textbox'])
-        self.data['textbox'] = ""
+    template = '<div><div v-for="message in messages" :key="message.content">{{ message.sender }}:' \
+               '{{ message.time }}: {{ message.content }}</div></div>'
 
 
-test2 = MessageList("#app1")
+message_component = MessageComponent()
+app = App('#app')
 
-test3 = MessageList("#app2")
+# class MessageList(Vue):
+#     data = {
+#         "current": 0,
+#         "textbox": "",
+#         "messages": [
+#             "Hi there!",
+#             "Welcome to PyVue!",
+#             "Hope it goes well for you!"
+#         ]
+#     }
+#
+#     def next_message(self):
+#         self.data['current'] += 1
+#         if self.data['current'] > len(self.data['messages']) - 1:
+#             self.data['current'] = 0
+#
+#     def add_message(self):
+#         self.data['messages'].append(self.data['textbox'])
+#         self.data['textbox'] = ""
+
+
+# test2 = MessageList("#app1")
+# test3 = MessageList("#app2")
