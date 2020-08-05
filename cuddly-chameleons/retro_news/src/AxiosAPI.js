0ce3f1e893b7b714fcbb28bd7e27dc98a78e6cef
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-05 18:01:56 +0300

    Add Axios automatically regenerate access token automatically

--- cuddly-chameleons/retro_news/src/AxiosAPI.js
+++ cuddly-chameleons/retro_news/src/AxiosAPI.js
@@ -10,4 +10,32 @@ const axios = Axios.create({
     }
 });
 
+// Implement automatic Access Token refreshing
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        const origin = error.config;
+
+        if (error.response.status === 401 && error.response.statusText === "Unauthorized") {
+            return axios.post('/token/refresh/', {refresh: localStorage.getItem('refresh_token')})
+                .then((response) => {
+                    localStorage.setItem('access_token', response.data.access);
+                    localStorage.setItem('refresh_token', response.data.refresh);
+                    axios.defaults.headers['Authorization'] = "JWT " + response.data.access;
+                    origin.headers['Authorization'] = "JWT " + response.data.access;
+
+                    return axios(origin);
+                })
+                .catch(err => {
+                    console.log(err);
+                    localStorage.removeItem('access_token');
+                    localStorage.removeItem('refresh_token');
+                    localStorage.removeItem('username');
+                });
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default axios;
