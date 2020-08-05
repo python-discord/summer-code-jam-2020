c565b6ce9a301bcd9ad5a3ea669d515bd56f09c9
Yaro Kifor <yarokifor@fastmail.com>
2020-08-05 12:07:19 -0400

    Revert "Converted react to next.js project"
    This reverts commit 5c551b08594703953bb7b913ab2d0003dda15518.

--- shiny_sheep/frontend/views.py
+++ shiny_sheep/frontend/views.py
@@ -1,15 +1,5 @@
-from django.conf import settings
-from django.http import HttpResponse
+from django.shortcuts import render
 
-index_file_path = str(settings.REACT_APP_DIR / 'out' / 'index.html')
 
 def index(request):
-    try:
-        with open(index_file_path) as f:
-            return HttpResponse(f.read())
-    except FileNotFoundError:
-        logging.exception('Production build of app not found')
-        return HttpResponse(
-            status=501,
-        )
-
+    return render(request, 'frontend/index.html')
