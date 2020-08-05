69b6d3322aee5f6fc582d4ee904eed598d4ef9f4
bogdan_tudorache <bogdanmihait10@gmail.com>
2020-08-05 18:02:26 +0300

    added individual chat room functionality

--- fancy-falcons/fancy_falcons-proj/chat/urls.py
+++ fancy-falcons/fancy_falcons-proj/chat/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UsersChatView
+from .views import (
+    UsersChatView,
+    UserChatRoom,
+)
 
 urlpatterns = [
     path('', UsersChatView.as_view(), name='chat-list'),
+    path('room/<int:pk>/', UserChatRoom.as_view(), name='chat-room'),
 ]
--- fancy-falcons/fancy_falcons-proj/chat/views.py
+++ fancy-falcons/fancy_falcons-proj/chat/views.py
@@ -1,7 +1,11 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+from .models import Message
 
 class UsersChatView(ListView):
     model = User
@@ -12,3 +16,15 @@ class UsersChatView(ListView):
         user = get_object_or_404(get_user_model(), username=self.request.user)
         return get_user_model().objects.exclude(username=user)
 
+
+class UserChatRoom(DetailView):
+    model = get_user_model()
+    template_name = 'chat/chat_room.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = Message.objects.filter(
+            (Q(sender=self.request.user) & Q(reciever=self.object)) |
+            (Q(sender=self.object) & Q(reciever=self.request.user))
+        ).order_by('-date')
+        return context
