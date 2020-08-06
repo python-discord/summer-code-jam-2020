2d922120ad684f393e86cf49c4f199da066448e7
Nimish Verma <nimishverma@ymail.com>
2020-08-06 17:25:21 -0400

    added view for saving images, need to update models and fix minor fixes on html

--- lonely-lemmings/earlyinternet/gifapp/urls.py
+++ lonely-lemmings/earlyinternet/gifapp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import paint
+from .views import paint, parse_save_request
 
 urlpatterns = [
     url(r"^$", paint),
+    url(r"save/",parse_save_request)
 ]
 
