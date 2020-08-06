a57b45bf5d98a50f8f2d20ca69e5a50d82fe6a26
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-06 10:17:02 +0700

    Reset some commits

--- acceptable-albatrosses/albatrosses_hub/home/urls.py
+++ acceptable-albatrosses/albatrosses_hub/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, aboutus
 
 urlpatterns = [
-    path("", homepage, name="homepage")
+    path("", homepage, name="homepage"),
+    path("aboutus", aboutus, name="aboutus"),
 ]
--- acceptable-albatrosses/albatrosses_hub/home/views.py
+++ acceptable-albatrosses/albatrosses_hub/home/views.py
@@ -6,3 +6,9 @@ def homepage(request):
     """ Views to render the homepage. """
 
     return render(request, "home.html")
+
+def aboutus(request):
+    """ Views to render the About Us page. """
+    template = "about.html"
+    context = aboutus
+    return render(request, template)
