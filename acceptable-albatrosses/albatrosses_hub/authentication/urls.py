391a9f8f2eda90a0bd4fb12b29e28ae760c97094
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-06 11:15:18 +0700

    Add login and logout page

--- acceptable-albatrosses/albatrosses_hub/authentication/urls.py
+++ acceptable-albatrosses/albatrosses_hub/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import register_page
+from .views import register_page, login_page, logout_page
 
 
 urlpatterns = [
-    path("register/", register_page, name="register_page")
+    path("register/", register_page, name="register_page"),
+    path("login/", login_page, name="login_page"),
+    path("logout/", logout_page, name="logout_page")
 ]
--- acceptable-albatrosses/albatrosses_hub/authentication/views.py
+++ acceptable-albatrosses/albatrosses_hub/authentication/views.py
@@ -6,3 +6,15 @@ def register_page(request):
     """ Views to render the register page. """
 
     return render(request, "register.html")
+
+
+def login_page(request):
+    """ Views to render the login page. """
+
+    return render(request, "login.html")
+
+
+def logout_page(request):
+    """ Views to render the logout page. """
+
+    return render(request, "logout.html")
