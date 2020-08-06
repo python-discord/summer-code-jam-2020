220ffbd1739b8cc450df00467865fb48eb43525c
Rafael Cortês <mrfyda@gmail.com>
2020-08-06 10:55:31 +0200

    Add models to django admin

--- glossy-gorillas/core/admin.py
+++ glossy-gorillas/core/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 from core import models
 
 admin.site.register(models.Review)
+
+admin.site.register(models.Listing)
+admin.site.register(models.Trade)
+
+admin.site.register(models.Product)
+
+admin.site.register(models.Trader)
+admin.site.register(models.InventoryRecord)
