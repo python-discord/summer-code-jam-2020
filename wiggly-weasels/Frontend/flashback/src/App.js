09de5ac27e2da70d06675cb3fa6199aedae91f42
JetDeveloping <jetjadeja@gmail.com>
2020-08-05 19:31:37 -0700

    Created New Themes

--- wiggly-weasels/Frontend/flashback/src/App.js
+++ wiggly-weasels/Frontend/flashback/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-import { styleReset, List, ListItem, Divider, AppBar, Toolbar, Button, TextField} from 'react95';
+import { styleReset, List, ListItem, Divider, AppBar, Toolbar, Button, TextField, Hourglass, Tabs, Tab, TabBody} from 'react95';
 // pick a theme of your choice
 import original from "react95/dist/themes/original";
 // original Windows95 font (optionally)
@@ -31,7 +31,8 @@ function postRequest(url, opts) {
   fetch(url, {
     method: 'post',
     body: JSON.stringify(opts),
-    Headers: {'Accept': 'application/json'}
+    Headers: {'Content-Type': 'application/json',
+            'Accept': '*/*'}
   }).then(function(response) {
     return response.json();
   }).then(function(data) {
@@ -43,33 +44,117 @@ const App = () => {
   const [state, setState] = useState({
     value: '',
     email_value: '',
-    user_value: ''
+    user_value: '',
+    activeTab: 0
   });
   const handleChange = e => setState({ email_value: e.target.value });
+  const tabChange = (e, value) => setState({ activeTab: value });
 
+  const { activeTab } = state;
   return (
-  <div>
+    <div
+    style={{
+      padding: '5rem',
+      background: 'teal'
+    }}>
+
+    <div>
     <GlobalStyles />
     <ThemeProvider theme={original}>
-    <AppBar>
-    <Toolbar style={{ justifyContent: 'space-between' }}> </Toolbar>
-    <Button
-            onClick={() => {
-              console.log(state.email_value)
-              postRequest('http://127.0.0.1:8000/account/', {'email': state.email_value})
-            }}
-            style={{ fontWeight: 'bold' }}
-          >Button</Button>
-          <TextField
-            value={state.email_value}
-            placeholder='Your Email'
-            onChange={handleChange}
-            >
-          </TextField>
-    </AppBar>
+      
+      <AppBar>
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+
+        <Button
+        onClick={() => {
+            console.log(state.email_value)
+            postRequest('http://127.0.0.1:8000/account/', {'email': state.email_value})
+         }}
+          style={{ marginLeft: '2px' }}>Login</Button>
+
+        </div>
+
+        <Hourglass size={32} />
+
+        </Toolbar>
+      </AppBar>
+
+      <Tabs value={activeTab} onChange={tabChange}>
+          <Tab value={0}>Email</Tab>
+          <Tab value={1}>Username</Tab>
+          <Tab value={2}>Password</Tab>
+      </Tabs>
+      
+      <TabBody style={{ height: 300 }}>
+        
+        {activeTab == 0 &&(
+          <div>
+            <TextField
+        value={state.email_value}
+        placeholder='Your Email'
+        onChange={handleChange}
+        ></TextField>
+          </div>
+        )}
+
+        {activeTab == 1 &&(
+        <TextField
+        value={state.email_value}
+        placeholder='Your Username'
+        onChange={handleChange}
+        >
+      </TextField>)}
+
+      {activeTab == 2 &&(
+        <TextField
+        value={state.email_value}
+        placeholder='Your Password'
+        onChange={handleChange}
+        >
+      </TextField>
+      )}
+
+      </TabBody>
+
     </ThemeProvider>
   </div>
-)
+  </div>
+  )
 };
 
 export default App;
+
+/*
+<div>
+<GlobalStyles />
+<ThemeProvider theme={original}>
+  <div>
+  <AppBar>
+    <Toolbar style={{ justifyContent: 'space-between' }}> </Toolbar>
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+  <Button
+        onClick={() => {
+          console.log(state.email_value)
+          postRequest('http://127.0.0.1:8000/account/', {'email': state.email_value})
+         }}
+          style={{ marginLeft: '2px' }}>Login</Button>
+      </div>
+</AppBar>
+</div>
+<div>
+<TextField
+        value={state.email_value}
+        placeholder='Your Email'
+        onChange={handleChange}
+        >
+      </TextField>
+      
+</div>
+</ThemeProvider>
+
+
+</div>
+*/
+
