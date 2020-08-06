76746a67f562aabf15d0c1f11f3d05b69788fdd4
jannis <jannisko99@gmail.com>
2020-08-06 07:37:52 +0000

    fix linting errors

--- festive-ferrets/backend/nchan/tests.py
+++ festive-ferrets/backend/nchan/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.urls import reverse
 
 from .models import Board, Post, Comment
+
 
 class BoardsTests(TestCase):
     def test_no_boards(self):
@@ -41,13 +41,15 @@ class BoardsTests(TestCase):
 
     def test_returns_board_posts(self):
         """
-        Tests if 'boards/<board>/posts/' returns only 
+        Tests if 'boards/<board>/posts/' returns only
         that boards posts
         """
         Board.objects.create(board='01', title='test-board-01')
         Board.objects.create(board='02', title='test-board-02')
-        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret', text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
-        Post.objects.create(title='second post', board=Board.objects.get(pk='02'), poster='friendly-frogs', text='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
+        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret',
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+        Post.objects.create(title='second post', board=Board.objects.get(pk='02'), poster='friendly-frogs',
+                            text='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
 
         resp = self.client.get('/nchan/boards/01/posts/')
         self.assertEqual(resp.data['count'], 1)
@@ -70,21 +72,23 @@ class PostsTests(TestCase):
         Tests if 'posts' endpoint returns created posts
         """
         Board.objects.create(board='01', title='test-board-01')
-        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret', text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
-        Post.objects.create(title='second post', board=Board.objects.get(pk='01'), poster='friendly-frogs', text='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
-        
+        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret',
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+        Post.objects.create(title='second post', board=Board.objects.get(pk='01'), poster='friendly-frogs',
+                            text='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
+
         resp = self.client.get('/nchan/posts/')
         self.assertEqual(resp.data['count'], 2)
         self.assertIn('first post', str(resp.data['results']))
         self.assertIn('second post', str(resp.data['results']))
 
-        
     def test_returns_single_post(self):
         """
         Tests if 'posts/<id>/' endpoint returns the correct post
         """
         Board.objects.create(board='01', title='test-board-01')
-        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret', text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret',
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
         resp = self.client.get('/nchan/posts/1/')
         self.assertEqual(resp.data['title'], 'first post')
 
@@ -93,19 +97,25 @@ class PostsTests(TestCase):
         Tests if 'posts/<id>/comments/' endpoint returns correct comments
         """
         Board.objects.create(board='01', title='test-board-01')
-        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret', text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
-        Comment.objects.create(post=Post.objects.get(pk=1), commenter='glossy-gorillas', text='URL namespace "admin" isn"t unique. You may not be able to reverse all URLs in this namespace')
+        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret',
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+        Comment.objects.create(post=Post.objects.get(pk=1), commenter='glossy-gorillas',
+                               text='URL namespace "admin" isn"t unique. You may not be'
+                               'able to reverse all URLs in this namespace')
         resp = self.client.get('/nchan/posts/1/comments/')
         self.assertIn('glossy-gorillas', str(resp.data))
-    
+
+
 class CommentsTests(TestCase):
     def test_returns_comment(self):
         """
         Tests if 'comments/' endpoint returns comments
         """
         Board.objects.create(board='01', title='test-board-01')
-        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret', text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
-        Comment.objects.create(post=Post.objects.get(pk=1), commenter='glossy-gorillas', text='URL namespace "admin" isn"t unique. You may not be able to reverse all URLs in this namespace')
+        Post.objects.create(title='first post', board=Board.objects.get(pk='01'), poster='festive-ferret',
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+        Comment.objects.create(post=Post.objects.get(pk=1), commenter='glossy-gorillas',
+                               text='URL namespace "admin" isn"t unique. You may'
+                               'not be able to reverse all URLs in this namespace')
         resp = self.client.get('/nchan/comments/')
         self.assertIn('glossy-gorillas', str(resp.data))
-
