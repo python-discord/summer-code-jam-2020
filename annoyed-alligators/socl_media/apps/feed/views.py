fb7717a1b0f8da75c5c3d5cbc6c3d09436051bf4
Nishith Showri <58651995+nishithshowri006@users.noreply.github.com>
2020-08-06 21:23:08 +0530

    add loginrequired mixin and removed comments

--- annoyed-alligators/socl_media/apps/feed/views.py
+++ annoyed-alligators/socl_media/apps/feed/views.py
@@ -1,32 +1,14 @@
-# from django.shortcuts import render
 from .models import Post
-from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-# @login_required
-# def feed(request):
-
-#     """
-#     This view shows posts
-#     for the logged in user.
-
-#     """
-#     posts_object = Post.objects.all()
-#     posts_list = []
-#     for post in posts_object:
-#         post_struct = {'Content': post.post_content,
-#                        'Author': post.posted_by,
-#                        'Date': post.post_date_posted}
-#         posts_list.append(post_struct)
-#     context = {"Feed": posts_list}
-#     return render(request, 'feed/home.html', context=context)
-
-@method_decorator(login_required, name="dispatch")
-class PostList(ListView):
+class PostList(LoginRequiredMixin, ListView):
+    """
+    This view is used for creating
+    posts and showring posts in th feed.
+    """
     model = Post
-    # template_name = "feed/Post_list.html"
     queryset = Post.objects.all()
