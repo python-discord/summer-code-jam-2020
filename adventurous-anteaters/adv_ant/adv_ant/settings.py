1a2cd1c61423f4d3a55d0274e2644919a1041114
sarzz2 <sarthaksingh2001@gmail.com>
2020-08-06 18:26:01 +0530

    added tweet functionality

--- adventurous-anteaters/adv_ant/adv_ant/settings.py
+++ adventurous-anteaters/adv_ant/adv_ant/settings.py
@@ -115,5 +115,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # login and logout url
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'post'
 LOGOUT_REDIRECT_URL = 'login'
--- adventurous-anteaters/adv_ant/retroNet/views.py
+++ adventurous-anteaters/adv_ant/retroNet/views.py
@@ -1,16 +1,25 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.template.context_processors import request
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-
-from .forms import RegisterForm
+from .models import Tweet
 
 
 # index view when user logs in
+
+def createpost(request):
+    if request.method == 'POST':
+        if request.POST.get('title') and request.POST.get('content'):
+            post = Tweet()
+            post.content = request.POST.get('content')
+            post.save()
+            return render(request, 'index.html')
+    else:
+        return render(request, 'tweet.html')
+
+
 def index(request):
-    return render(request, 'index.html')
+    queryset = Tweet.objects.all()
+    return render(request, 'index.html', {"data": queryset})
 
 
 # for registration of a new user
@@ -21,6 +30,6 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return render(request, 'index.html')
+            return render(request, 'tweet.html')
     context['form'] = form
     return render(request, 'register.html', context)
