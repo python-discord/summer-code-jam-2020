ef8d6c06dd493111ede7190fc42aeee98d4fc204
Eliot <eliotpersonal77@gmail.com>
2020-08-06 14:04:25 -0700

    Add forms.py, which contains the class UserRegisterForm.
    UserRegisterForm inherits from UserCreationForm, and allows for the
    registration form to ask for extra information, like first name,
    last name and email.

---
+++ glossy-gorillas/core/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.TextInput()
+    last_name = forms.TextInput()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        ]
