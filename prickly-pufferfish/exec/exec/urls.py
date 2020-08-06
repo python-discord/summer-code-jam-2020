435b7c02b7a0b8936829f7789191cf012bd1ce42
TruDev <29785998+Tru-Dev@users.noreply.github.com>
2020-08-06 11:43:26 -0700

    Removed app artifacts from exec

--- prickly-pufferfish/exec/exec/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('battle', views.battle_page, name='battle-page'),
-    path('about/', views.about_page, name = 'about-page')
-]
