2b5ddf14233f0634f32ed66bdb9a29b8a05610f2
fisher60 <wakefollower@gmail.com>
2020-08-06 12:38:54 -0700

    comments on posts are working, front end needs fixing (each comment pushes the container up)

--- hysterical-horses/code_jam/blogs/views.py
+++ hysterical-horses/code_jam/blogs/views.py
@@ -32,25 +32,40 @@ class BlogDetailView(DetailView):
     template_name = "blog_detail.html"
 
     def post(self, request, *args, **kwargs):
+        print("POST METHOD RUNNING")
+
         self.comment_form = CommentForm(request.POST)
+
         if self.comment_form.is_valid():
             parent_obj = None
+
             try:
                 parent_id = int(self.request.POST.get('parent_id'))
-            except:
+            except Exception as e:
+                print(e)
                 parent_id = None
+
+            self.object = self.get_object()
+            context = super(BlogDetailView, self).get_context_data(**kwargs)
+            comment_post = context['post']
+
             if parent_id is not None:
                 parent_obj = Comment.objects.get(id=parent_id)
+
                 if parent_obj:
                     reply_comment = self.comment_form.save(commit=False)
                     reply_comment.parent = parent_obj
+
             self.new_comment = self.comment_form.save(commit=False)
+            print(args)
             self.new_comment.post = comment_post  #  Problem here
             # `comment_post` can only exist in `get_context_data`
+            self.new_comment.author = request.user
             self.new_comment.save()
             return HttpResponseRedirect(self.request.path_info)  # Refresh
 
     def get_context_data(self, **kwargs):
+        print("CONTEXT DATA RUNNING")
         if self.request.method == 'GET':
             self.comment_form = CommentForm()
 
