4026b0fc71f52272888cb15918a791a281cc95d7
Kevin Sheng <kevinsheng2005@gmail.com>
2020-08-06 11:44:49 -0700

    the login thing broke was a simple url problem

--- observant-otters/forumthing/forumthing/urls.py
+++ observant-otters/forumthing/forumthing/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forum.urls')),
     path('accounts/', include('allauth.urls')),
-    path('user/', include('user.urls')),
+    path('accounts/', include('user.urls')),
 ]
