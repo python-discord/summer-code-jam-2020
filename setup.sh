faa284fe95a1e010dd7a2ae881635830faf7befd
Yaro Kifor <yarokifor@fastmail.com>
2020-08-06 18:57:42 -0400

    Added installing webpack to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -e
+
+echo "Installing webpack..."
+sudo npm install -g webpack webpack-cli 
+
 cd shiny_sheep/frontend
+echo "Install frontend dependencies..."
 npm install
+echo "Building frontend..."
 npm run dev
 
+echo "Building docker images..."
+cd ../..
 docker-compose -f local.yml build
