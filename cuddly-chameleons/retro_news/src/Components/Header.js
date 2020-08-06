76395194cb8c8b742002c39803c77aac7d2e22d0
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-06 10:31:51 +0300

    Add header inside container

--- cuddly-chameleons/retro_news/src/Components/Header.js
+++ cuddly-chameleons/retro_news/src/Components/Header.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
 import UserNav from "./UserNav";
+import {Container} from "nes-react/dist";
 
 class Header extends React.Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">Retro News</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <UserNav status={this.props.status} handler={this.props.handler} />
-                </Navbar.Collapse>
-            </Navbar>
+            <Container>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="/">Retro News</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <UserNav status={this.props.status} handler={this.props.handler} />
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
         );
     }
 }
