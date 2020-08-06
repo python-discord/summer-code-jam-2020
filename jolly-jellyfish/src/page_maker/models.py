5161560d81ca96e4074c47971059b9e89628dfbd
tameTNT <30503695+tametnt@users.noreply.github.com>
2020-08-06 21:13:20 +0100

    Add Like model to models.py
    Add new model to allow for like functionality where a user can like a
    webpage to improve its ordering on the 'best pages' page.

--- jolly-jellyfish/src/page_maker/models.py
+++ jolly-jellyfish/src/page_maker/models.py
@@ -94,3 +94,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    parent_page = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author} likes {self.parent_page}'
