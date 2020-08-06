6f7bf033d679669aa3397dbeb50dfc681b5974a6
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2020-08-06 22:23:01 +0200

    Add UserViewsTest

--- jolly-jellyfish/src/page_maker/tests.py
+++ jolly-jellyfish/src/page_maker/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 import io
 import base64
 
+from .models import User, Webpage, Template
 
 USERNAME = 'haha69420'
 PASSWORD = 'foobar < xyzzy'
@@ -20,6 +21,11 @@ class UserRegisterTest(TestCase):
         self.client.post('/users/register', params)
         self.client.login(username=USERNAME, password=PASSWORD)
 
+    def test_user_exists(self):
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.username, USERNAME)
+
     def test_register_and_home(self):
         # assumptions (aka what this tests):
         #  - `/user/register` exists.
@@ -36,6 +42,58 @@ class UserRegisterTest(TestCase):
         res = self.client.get('/').content
 
         self.assertNotIn(USERNAME.encode(), res, 'Our username was already in the home view.')
+
+
+class UserViewsTest(TestCase):
+    fixtures = ['users.yaml', 'webpages.yaml', 'templates.yaml']
+
+    def test_user_detail(self):
+        user = User.objects.get(pk=1)
+        res = self.client.get('/users/1/')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.context['viewed_user'], user)
+        self.assertIsInstance(res.context['user_pages'][0], Webpage)
+
+    def test_user_update(self):
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/users/2/update', {
+            'first_name': 'john', 'last_name': 'lemon', 'email': 'jl@sth.com'
+        })
+        self.assertRedirects(res, '/')
+        user = User.objects.get(pk=2)
+        self.assertEqual(user.last_name, 'lemon')
+
+    def test_user_delete(self):
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/users/2/delete')
+        self.assertRedirects(res, '/')
+        self.assertEqual(User.objects.count(), 1)
+
+
+class WebpageViewsTest(TestCase):
+    fixtures = ['users.yaml']
+
+    def test_webpage_create(self):
+        pass
+
+    def test_webpage_preview(self):
+        # TODO test if proper theme is loaded
+        # TODO test if user data is inside
+        pass
+
+    def test_webpage_detail(self):
+        # TODO check if form and comments are in context
+        pass
+
+    def test_webpage_list(self):
+        pass
+
+    def test_webpage_update(self):
+        pass
+
+    def test_webpage_delete(self):
+        # TODO test delete permissions
+        pass
 
 
 class TemplateViewsTest(TestCase):
