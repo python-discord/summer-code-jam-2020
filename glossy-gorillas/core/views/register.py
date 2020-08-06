8627af235d33fa54b15c93e6ffdd5a1879b1ad56
Eliot <eliotpersonal77@gmail.com>
2020-08-06 10:25:45 -0700

    Support responses for whether the request is POST or GET
    Originally, the register page would always return itself upon
    submission. This was problematic as:
    
    If the form submission was a POST request then:
    	- The form was not saved.
    	- The form was not checked for validity.
    	- The registration form would be confusingly returned again.
    Now, if the method is POST, the form is checked to be valid, and if it
    is, then it's saved, and the user's redirected back to home.
    Any other cases have the registration form returned again.

--- glossy-gorillas/core/views/register.py
+++ glossy-gorillas/core/views/register.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
     return render(request, "core/register.html", {"form": form})
