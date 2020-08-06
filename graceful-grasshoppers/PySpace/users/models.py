6325348d6ecb2581d2512d09ac95c1aedd4b8467
Vivaan Parashar <parashar.vivaan@gmail.com>
2020-08-06 08:04:26 +0530

    added friend request compatibility

--- graceful-grasshoppers/PySpace/users/models.py
+++ graceful-grasshoppers/PySpace/users/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
+    friends = []
+    friend_requests_pending = []
+    friend_requests_sent = []
 
     def __str__(self):
         return self.email
