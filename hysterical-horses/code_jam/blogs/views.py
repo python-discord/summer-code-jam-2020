01a704a447c6bd4f0257564bfab420c3ca375060
melodicht <marvincht@gmail.com>
2020-08-06 16:44:50 +0800

    Added blog comments
    Note: It doesn't work
    
    - Added more to the comment model, such as date and time created and replies
    - Added to  `blog_detail.html` to show comments and replies

--- hysterical-horses/code_jam/blogs/views.py
+++ hysterical-horses/code_jam/blogs/views.py
@@ -1,9 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .filters import PostFilter
-from .models import Post
+from .forms import CommentForm
+from .models import Comment, Post
 
 
 class HomeView(ListView):
@@ -28,3 +30,36 @@ class HomeView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blog_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.comment_form = CommentForm(request.POST)
+        if self.comment_form.is_valid():
+            parent_obj = None
+            try:
+                parent_id = int(self.request.POST.get('parent_id'))
+            except:
+                parent_id = None
+            if parent_id is not None:
+                parent_obj = Comment.objects.get(id=parent_id)
+                if parent_obj:
+                    reply_comment = self.comment_form.save(commit=False)
+                    reply_comment.parent = parent_obj
+            self.new_comment = self.comment_form.save(commit=False)
+            self.new_comment.post = comment_post  #  Problem here
+            # `comment_post` can only exist in `get_context_data`
+            self.new_comment.save()
+            return HttpResponseRedirect(self.request.path_info)  # Refresh
+
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'GET':
+            self.comment_form = CommentForm()
+
+        context = super().get_context_data(**kwargs)
+
+        comment_post = context['post']
+        comments = comment_post.comments.filter(
+            active=True, parent__isnull=True
+        )
+        context['comments'] = comments
+        context['comment_form'] = self.comment_form
+        return context
