870836d8bdbca7c521a3f7c874698e08c9b79bad
Nishith Showri <58651995+nishithshowri006@users.noreply.github.com>
2020-08-06 19:00:04 +0530

    changed feed to class based view, added mail functionality when forgot password

--- annoyed-alligators/socl_media/apps/feed/views.py
+++ annoyed-alligators/socl_media/apps/feed/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
 
-@login_required
-def feed(request):
+# @login_required
+# def feed(request):
 
-    """
-    This view shows posts
-    for the logged in user.
+#     """
+#     This view shows posts
+#     for the logged in user.
 
-    """
-    posts_object = Post.objects.all()
-    posts_list = []
-    for post in posts_object:
-        post_struct = {'Content': post.post_content,
-                       'Author': post.posted_by,
-                       'Date': post.post_date_posted}
-        posts_list.append(post_struct)
-    context = {"Feed": posts_list}
-    return render(request, 'feed/home.html', context=context)
+#     """
+#     posts_object = Post.objects.all()
+#     posts_list = []
+#     for post in posts_object:
+#         post_struct = {'Content': post.post_content,
+#                        'Author': post.posted_by,
+#                        'Date': post.post_date_posted}
+#         posts_list.append(post_struct)
+#     context = {"Feed": posts_list}
+#     return render(request, 'feed/home.html', context=context)
+
+@method_decorator(login_required, name="dispatch")
+class PostList(ListView):
+    model = Post
+    # template_name = "feed/Post_list.html"
+    queryset = Post.objects.all()
--- annoyed-alligators/socl_media/settings.py
+++ annoyed-alligators/socl_media/settings.py
@@ -139,5 +139,9 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
