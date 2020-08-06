82f8f5bc30b6375353fbf46e899f5465bcd89c31
TruDev <29785998+Tru-Dev@users.noreply.github.com>
2020-08-06 14:18:05 -0700

    Added signup Added some account templates Added CSS for above

--- prickly-pufferfish/arena/accounts/views.py
+++ prickly-pufferfish/arena/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+class SignupView(generic.CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
--- prickly-pufferfish/arena/arena/urls.py
+++ prickly-pufferfish/arena/arena/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     # path('battle/', include('battle.urls')),
     path('', include('main.urls')),
 ]
