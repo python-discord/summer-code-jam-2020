8f24e5f7aaa8617457ca1bc1f1c34c1ecf966198
fliepeltje <donatas@rasiukevicius.dev>
2020-08-06 16:23:51 +0200

    Add search query handler for listing

--- glossy-gorillas/core/views/market.py
+++ glossy-gorillas/core/views/market.py
@@ -14,3 +14,11 @@ class ListingList(ListView):
     paginate_by = 25
     model = Listing
     template_name = "core/pages/listing_list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if "search" in self.request.GET:
+            term = self.request.GET.get("search")
+            return qs.filter(item__product__name__icontains=term)
+        else:
+            return qs
--- glossy-gorillas/tests/integration/core/test_views.py
+++ glossy-gorillas/tests/integration/core/test_views.py
@@ -74,3 +74,14 @@ def test_logout_redirects_to_home(client):
     response = client.post(reverse("logout"))
     assert response.status_code == 302
     assert response.url == reverse("home")
+
+
+@pytest.mark.django_db
+def test_listing_search_filters_queryset_results(client):
+    listing_1 = ListingFactory(item__product__name="Find Me")
+    listing_2 = ListingFactory(item__product__name="Not Found")
+    url = f"{reverse('listings')}?search=find"
+    response = client.get(url)
+    content = str(response.content)
+    assert listing_1.item.product.name in content
+    assert listing_2.item.product.name not in content
