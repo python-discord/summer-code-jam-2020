f2fa97fdc4dbb7f523edf0993d0812f96467c00d
Eliot <eliotpersonal77@gmail.com>
2020-08-06 14:01:33 -0700

    Update the form to use UserRegisterForm instead of UserCreationForm.
    UserRegisterForm is a better version of UserCreationForm. It has extra
    fields for first name, last name, and an email address.

--- glossy-gorillas/core/views/register.py
+++ glossy-gorillas/core/views/register.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from ..forms import UserRegisterForm
 
 
 def register(request):
+    """Saves the form if valid, registering the user. Otherwise, form must be repeated."""
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}!")
             return redirect("home")
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, "core/register.html", {"form": form})
