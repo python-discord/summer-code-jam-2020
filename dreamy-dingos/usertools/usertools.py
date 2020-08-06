9fc6cbcda0b0eb131a486130a816db45102bd9b3
Anthony Webster <ajwebster1@gmail.com>
2020-08-06 12:47:46 -0400

    Added an outline for a method that checks if a given screen name is available.

---
+++ dreamy-dingos/usertools/usertools.py
@@ -0,0 +1,12 @@
+from chat import models
+
+
+def screen_name_is_available(screen_name: str) -> bool:
+    """
+    Checks if the given screen name is available.
+    If the screen name is available, it's not tied to any user in the users table.
+
+    :param screen_name: A screen name.
+    :return: True if the screen name is available, false otherwise.
+    """
+    pass
