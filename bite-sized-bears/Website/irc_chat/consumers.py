5f7eb97f51190aa9068db4d49d1c4dd33ee51852
Ronit-j <pictronit007@gmail.com>
2020-08-07 12:55:32 +0530

    Added Fetch messages functionality to irc backend

--- bite-sized-bears/Website/irc_chat/consumers.py
+++ bite-sized-bears/Website/irc_chat/consumers.py
@@ -1,35 +1,74 @@
-# chat/consumers.py
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Message
+from syndication_app.models import User
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = Message.last_15_messages(self=Message, community=self.room_name)
+        print(messages)
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        author = data['from']
+        author_user = User.objects.get(name=author)
+        message = Message.objects.create(
+            author=author_user,
+            content=data['message'],
+            community=self.room_name)
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.name,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
 
-        # Join room group
-        await self.channel_layer.group_add(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        data['from'] = 'runit'
+        self.commands[data['command']](self, data)
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -37,11 +76,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
--- bite-sized-bears/Website/irc_chat/models.py
+++ bite-sized-bears/Website/irc_chat/models.py
@@ -9,6 +9,11 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    community = models.TextField(default='old_messages')
 
     def __str__(self):
-        return self.author.name
+        return self.author.name + ':' + self.content
+
+    def last_15_messages(self, community):
+        return Message.objects.filter(community=community).order_by('-timestamp').all()[:15]
+
