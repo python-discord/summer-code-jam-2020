897b6c453b8d6db8ae68e7245cc64554ed15f5dd
chris <cdrandin@hotmail.com>
2020-08-06 22:32:11 -0700

    corresponding url/views for CRUD

--- melodic-manatees/early_internet/diary/views.py
+++ melodic-manatees/early_internet/diary/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from diary.forms import DiaryEntryForm
+from diary.models import DiaryEntry
 
 
-def diary_home(request):
-    return HttpResponse(200)
+def diary_entry_list(request):
+    object_list = DiaryEntry.objects.all().select_related('creator')
+    return render(request, 'diary/list.html', {'object_list': object_list})
+
+
+def diary_entry_detail(request, id):
+    obj = get_object_or_404(
+        DiaryEntry.objects.select_related('creator'), id=id)
+    return render(request, 'diary/detail.html', {'object': obj})
+
+
+def diary_entry_create(request):
+    if request.method == 'POST':
+        form = DiaryEntryForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.creator = request.user
+            instance.save()
+            return redirect('diary-list')
+    else:
+        form = DiaryEntryForm()
+
+    return render(request, 'diary/create.html', {'form': form})
+
+
+def diary_entry_update(request, id):
+    obj = get_object_or_404(
+        DiaryEntry.objects.select_related('creator'), id=id)
+    form = DiaryEntryForm(instance=obj, data=request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('diary-list')
+
+    return render(request, 'diary/update.html', {'form': form})
+
+
+def diary_entry_delete(request, id):
+    obj = get_object_or_404(
+        DiaryEntry.objects.select_related('creator'), id=id)
+
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('diary-list')
+
+    return render(request, 'diary/delete.html', {'object': obj})
