f4d221b7d83d70b0cb4f04c5b3af439fe6c291d0
CodeForeverAndEver <reed.manning06>
2020-08-07 16:47:23 -0400

    Made code pep8 compliant, added option for API searches without a category.

--- talented-tigers/spydir/generate_images.py
+++ talented-tigers/spydir/generate_images.py
@@ -3,18 +3,25 @@ import requests
 from .models import PageImage
 
 
-def generate_images(category: str, n: int, query:str = "clip art", image_type: str = "illustration") -> PageImage:
+def generate_images(category, n: int, query: str = "clip art", image_type: str = "illustration") -> list:
     """Use pixabay api to get image links based on image categories."""
 
     """
     Categories can be:
-    backgrounds, fashion, nature, science, education, 
-    feelings, health, people, religion, places, animals, 
-    industry, computer, food, sports, transportation, 
-    travel, buildings, business, music 
+    backgrounds, fashion, nature, science, education,
+    feelings, health, people, religion, places, animals,
+    industry, computer, food, sports, transportation,
+    travel, buildings, business, music
     """
-    response = requests.get(f"https://pixabay.com/api/?q={query}&lang=en&category={category}&safesearch=true&image_type={image_type}&key={os.getenv('API_KEY')}")
 
-    images = [PageImage(image = img['userImageURL']) for img in response.json()['hits'][:5]]
-    
-    return images
+    response = requests.get(
+        "https://pixabay.com/api/?"
+        f"q={query}"
+        "&lang=en"
+        f"{'&category=' + category if category else ''}"
+        "&safesearch=true"
+        f"&image_type={image_type}"
+        f"&key={os.getenv('API_KEY')}"
+    )
+
+    return [PageImage(image=img["userImageURL"]) for img in response.json()["hits"][:5]]
