3186dc820c6edd476ba8f780a3970210a085dab2
François Hardy <snaptraks@gmail.com>
2020-08-06 20:34:58 -0400

    Register Game Models to admin site

--- wily-wolves/mud/game/admin.py
+++ wily-wolves/mud/game/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Location, Player
 
-# Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Player)
