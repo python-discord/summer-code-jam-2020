d92c69f9b0cb18d0a87b24d38aaebcd8ee44c2e0
MushroomMaula <maxrd79@gmail.com>
2020-08-07 13:06:42 +0200

    Add tests for todo model

--- concerned-coyotes/earlyinternet/todo/tests.py
+++ concerned-coyotes/earlyinternet/todo/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import TodoEntry
+
+
+class TodoEntryTest(TestCase):
+
+    def setUp(self) -> None:
+        user = User.objects.create(
+            username='John Doe',
+            password='hunter2'
+        )
+        self.todo = TodoEntry.objects.create(
+            user=user,
+            name="Finish summer code jam",
+        )
+
+    def test_representation(self):
+        self.assertEqual(
+            str(self.todo),
+            'Finish summer code jam - unfinished'
+        )
