5c2b991e2564d2f5442d04219a59dbe8cd45be1d
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-07 08:51:55 +0700

    Create user models

--- acceptable-albatrosses/albatrosses_hub/authentication/models.py
+++ acceptable-albatrosses/albatrosses_hub/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class HubUser(models.Model):
+    """A Class model that will store user information."""
+
+    email = models.EmailField(primary_key=True)
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=512)
+
+    def __str__(self):
+        # Will display user id on Django admin
+        return f"({self.user_id})".zfill(3)
