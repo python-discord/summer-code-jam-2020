6f83d01bcba8fa91d6c14d1ad90723a215518039
aedwardg <aedwardg1@gmail.com>
2020-08-06 18:11:12 -0700

    added modal for journal/diary entry

---
+++ melodic-manatees/early_internet/main/static/main/js/bulma-modal.js
@@ -0,0 +1,17 @@
+let button = document.getElementById('journal-btn');
+let modal = document.getElementById('journal-modal');
+let closeX = document.getElementById('close-x');
+let cancelButton = document.getElementById('cancel-btn');
+
+function openModal() {
+    modal.classList.add('is-active');
+}
+function closeModal() {
+    modal.classList.remove('is-active');
+}
+
+button.onclick = openModal;
+
+closeX.onclick = closeModal;
+
+cancelButton.onclick = closeModal;
