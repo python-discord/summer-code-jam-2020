e8ad3795e3b4720fb191f836a920a0c560b170b1
Vivaan Parashar <parashar.vivaan@gmail.com>
2020-08-07 16:00:46 +0530

    commenting feature

--- graceful-grasshoppers/PySpace/users/models.py
+++ graceful-grasshoppers/PySpace/users/models.py
@@ -2,20 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class ProfileComment(models.Model):
-
-    content = models.TextField()
-
-    def __str__(self):  # Returns a short intro of 20 words
-        return " ".join(self.content.split(" ")[:20])
-
-
 class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
     friends = []
     friend_requests_pending = []
     friend_requests_sent = []
-    comments = models.ManyToManyField(ProfileComment)
 
     def __str__(self):
         return self.email
+
+
+class ProfileComment(models.Model):
+
+    content = models.TextField()
+    user_commented_on = models.ForeignKey(CustomUser, models.CASCADE)
+    user_commented = models.ForeignKey(CustomUser, models.CASCADE)
+
+    def __str__(self):  # Returns a short intro of 20 words
+        return " ".join(self.content.split(" ")[:20])
