9a1052221b6d918d18a1b3409cc94c936d4ad451
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2020-08-08 00:06:51 +0530

    Search Working

--- bouncy-bullfrogs/bouncy/bouncy/urls.py
+++ bouncy-bullfrogs/bouncy/bouncy/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path('', include('mail.urls'))
+    path('search/', include('search.urls', namespace='search')),
+    path('', include('mail.urls')),
 ]
