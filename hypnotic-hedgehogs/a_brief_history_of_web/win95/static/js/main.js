326b2c6a785274efb8825b69184ec7589ad28848
zhaos <zhaoshimeng2015@gmail.com>
2020-08-07 23:55:17 +0200

    orgnize index file; move js scrit to a separate js file

---
+++ hypnotic-hedgehogs/a_brief_history_of_web/win95/static/js/main.js
@@ -0,0 +1,53 @@
+    clippy.load('Clippy', (agent) => {
+    // do anything with the loaded agent
+    agent.show();
+    agentSpeak(agent,"Hey ! Welcome stranger.");
+    setTimeout(() => {
+      agentSpeak(agent,"Before connecting to internet, please enter your username and click Connect.");
+    }, 3000);
+    setTimeout(() => {
+      agent.play("")
+      agent.hide();
+    }, 10000);
+
+});
+
+function hideIt(params) {
+
+  var username = $("#username").val();
+
+        $("#app-mine").hide();
+        $("#app-mine2").show();
+        setTimeout(() => {
+          $("#app-mine2").hide();
+          $(".internet-explorer").css("display","inline");
+          clippy.load("Merlin",(agent) => {
+    agent.show();
+    agentSpeak(agent,"Welcome to world of Internet, "+username+". Ahh... I need to check something quickly..");
+    agent.play("Reading")
+    agentSpeak(agent,"Lorem ip elit. Quam voluptatibus exercitationem  reprehenderit officiis saepe labore vitae, rem eligendi?")
+
+  });
+        }, 5000);
+
+      }
+
+function agentSpeak(agent,text){
+    agent.speak(text);
+}
+
+function searchPage(){
+  var address = $("#address-bar").val();
+  console.log(address);
+  if (address === "https://youtube.com") {
+    console.log("success");
+    $("#google-page").addClass("d-none");
+    $(".404-interface").addClass("d-none");
+    $("#youtube-page").removeClass("d-none");
+  }else{
+    $("#google-page").addClass("d-none");
+    $("#youtube-page").addClass("d-none");
+    $(".404-interface").removeClass("d-none");
+
+  }
+}
