822bdac8c9295d1b0a6117f3fedbc5fd18dc6c3b
Justus Sieweke <justus.sieweke@gmail.com>
2020-08-07 04:22:15 +0200

    Removed help_texts for signup

--- imaginary-iguanas/code_jam/users/forms.py
+++ imaginary-iguanas/code_jam/users/forms.py
@@ -1,11 +1,32 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm as _UserCreationForm, PasswordChangeForm as _PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm as _PasswordChangeForm
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 from .models import Profile as _Profile
 
 
-class UserUpdateForm(_UserCreationForm):
-    pass
+class UserUpdateForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': _('The two password fields didn’t match.'),
+    }
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False
+    )
+
+    class Meta:
+        model = User
+        fields = ("username",)
+        help_texts = {
+            'username': None
+        }
 
 
 class UserPasswordUpdateForm(_PasswordChangeForm):
