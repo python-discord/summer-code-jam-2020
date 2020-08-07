d950f0f2553f7f11e4a934d7dca07f7fb7722654
MushroomMaula <maxrd79@gmail.com>
2020-08-07 13:50:59 +0200

    Add TodoEntryUpdate url

--- concerned-coyotes/earlyinternet/todo/urls.py
+++ concerned-coyotes/earlyinternet/todo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from .views import TodoEntryCreate
+from .views import TodoEntryCreate, TodoEntryUpdate
 
 
 urlpatterns = [
-    path('/add', login_required(TodoEntryCreate.as_view()), name='todo-create')
+    path('add', login_required(TodoEntryCreate.as_view()), name='todo-create'),
+    path('update/<int:pk>', login_required(TodoEntryUpdate.as_view()), name='todo-update')
 ]
