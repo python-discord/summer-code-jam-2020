fe194c46200bb5e30b86e59fc7471739a16f1fa1
Kenny <kfonatoko@gmail.com>
2020-08-07 00:10:09 -0400

    Initialize game

---
+++ mysterious-mice/spacebook/game/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import HighScore
+from .utils import *
+
+
+class GameView(View):
+    # def get_context_data(self, **kwargs):
+    #     context = super(GameView, self).get_context_data(**kwargs)
+    #     return context
+    def get(self, request):
+        game_data = get_game(request)
+        command = self.request.GET.get("command")
+        parse_command(request, game_data, command)
+
+        context = game_data
+        return render(request, "game/game.html", context)
