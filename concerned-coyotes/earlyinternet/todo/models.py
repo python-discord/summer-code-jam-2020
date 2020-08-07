4b1d0d02912457dbff460ffff69042da26d5389a
MushroomMaula <maxrd79@gmail.com>
2020-08-07 13:00:41 +0200

    Add Todo model

--- concerned-coyotes/earlyinternet/todo/models.py
+++ concerned-coyotes/earlyinternet/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TodoEntry(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} - {'finished' if self.done else 'unfinished'}"
