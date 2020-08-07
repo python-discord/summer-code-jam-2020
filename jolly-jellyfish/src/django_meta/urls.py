97a64a0c34f51379ea40632e121d3b95ec4bad79
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2020-08-07 03:41:25 +0200

    Add WebpageViewsTest, TemplateViewsTest, CommentViewsTest

--- jolly-jellyfish/src/django_meta/urls.py
+++ jolly-jellyfish/src/django_meta/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/login/', auth_views.LoginView.as_view(), name='login'),
-    path('users/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin', admin.site.urls),
+    path('users/login', auth_views.LoginView.as_view(), name='login'),
+    path('users/logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('page_maker.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jolly-jellyfish/src/page_maker/tests.py
+++ jolly-jellyfish/src/page_maker/tests.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from pathlib import Path
 import io
 import base64
 
-from .models import User, Webpage, Template
+from .models import User, Webpage, Template, Comment
+from .forms import CommentForm
 
 USERNAME = 'haha69420'
 PASSWORD = 'foobar < xyzzy'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'hard123passwd'
 
 
 class UserRegisterTest(TestCase):
@@ -71,38 +77,88 @@ class UserViewsTest(TestCase):
 
 
 class WebpageViewsTest(TestCase):
-    fixtures = ['users.yaml']
+    fixtures = ['users.yaml', 'templates.yaml', 'webpages.yaml', 'comments.yaml']
+
+    def test_webpage_create_redirect(self):
+        res = self.client.get('/pages/new')
+        self.assertRedirects(res, '/users/login?next=/pages/new')
 
     def test_webpage_create(self):
-        pass
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.get('/pages/new')
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name='page_maker/webpage_create.html')
+        image_path = Path(settings.BASE_DIR) / 'test_media' / 'images' / 'test.png'
+        image1 = SimpleUploadedFile(name='test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        image2 = SimpleUploadedFile(name='test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        image3 = SimpleUploadedFile(name='test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        image4 = SimpleUploadedFile(name='test.png', content=open(image_path, 'rb').read(), content_type='image/jpeg')
+        form_input = {
+            'name': 'asdf',
+            'template_used': '1',
+            'user_title': 'ASDF',
+            'user_text_1': 'zxcvasdfqwer',
+            'user_text_2': 'zxcvasdfqwer',
+            'user_text_3': 'zxcvasdfqwer',
+            'user_image_1': image1,
+            'user_image_2': image2,
+            'user_image_3': image3,
+            'user_image_4': image4,
+        }
+        res = self.client.post('/pages/new', form_input)
+        self.assertRedirects(res, '/pages/asdf/')
+        self.assertEqual(Webpage.objects.count(), 2)
+        webpage = Webpage.objects.get(pk=2)
+        self.assertEqual(webpage.name, form_input['name'])
 
     def test_webpage_preview(self):
-        # TODO test if proper theme is loaded
-        # TODO test if user data is inside
-        pass
+        res = self.client.get('/pages/mypage/')
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, 'page_maker/webpage_view.html')
+        self.assertIsInstance(res.context['webpage'], Webpage)
+        self.assertEqual(res.context['webpage'].user_title, 'thats my page')
 
     def test_webpage_detail(self):
-        # TODO check if form and comments are in context
-        pass
+        res = self.client.get('/pages/mypage/detail')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.context['comment_form'], CommentForm)
+        self.assertIsInstance(res.context['comments'][0], Comment)
 
     def test_webpage_list(self):
-        pass
+        res = self.client.get('/pages/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.context['webpages'][0], Webpage)
 
     def test_webpage_update(self):
-        pass
+        self.client.login(username=ADMIN_USERNAME, password=ADMIN_PASSWORD)
+        res = self.client.get('/pages/mypage/update')
+        self.assertEqual(res.status_code, 200)
+        webpage = Webpage.objects.get(pk=1)
+        form_data = webpage.__dict__
+        form_data['user_title'] = 'abcd test 01234'
+        form_data['template_used'] = '1'
+        res = self.client.post('/pages/mypage/update', form_data)
+        self.assertRedirects(res, '/pages/mypage/')
+        webpage = Webpage.objects.get(pk=1)
+        self.assertEqual(webpage.user_title, 'abcd test 01234')
 
     def test_webpage_delete(self):
-        # TODO test delete permissions
-        pass
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/pages/mypage/delete')
+        self.assertEqual(res.status_code, 403)
+        self.client.login(username=ADMIN_USERNAME, password=ADMIN_PASSWORD)
+        res = self.client.post('/pages/mypage/delete')
+        self.assertRedirects(res, '/')
+        self.assertEqual(Webpage.objects.count(), 0)
 
 
 class TemplateViewsTest(TestCase):
-    fixtures = ['users.yaml']
+    fixtures = ['users.yaml', 'templates.yaml']
 
     def setUp(self):
         self.client.login(username=USERNAME, password=PASSWORD)
 
-    def test_upload_templates(self):
+    def test_template_create(self):
         # what this assumes:
         # - when you create a page the
         #   available templates are
@@ -124,3 +180,47 @@ class TemplateViewsTest(TestCase):
         res = self.client.get('/pages/new')
         template_entries = [entry[1] for entry in res.context['form'].fields['template_used'].choices]
         self.assertIn(template_name, template_entries)
+
+    def test_template_detail(self):
+        res = self.client.get('/templates/1/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.context['template'], Template)
+
+    def test_template_list(self):
+        res = self.client.get('/templates/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.context['templates'][0], Template)
+
+    def test_template_delete(self):
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/templates/1/delete')
+        self.assertEqual(res.status_code, 403)
+        self.client.login(username=ADMIN_USERNAME, password=ADMIN_PASSWORD)
+        res = self.client.post('/templates/1/delete')
+        self.assertRedirects(res, '/')
+        self.assertEqual(Template.objects.count(), 1)
+
+
+class CommentViewsTest(TestCase):
+    fixtures = ['users.yaml', 'webpages.yaml', 'templates.yaml', 'comments.yaml']
+
+    def test_comment_create(self):
+        form_data = {
+            'title': 'test comment',
+            'content': 'another test'
+        }
+        res = self.client.post('/pages/mypage/comment', form_data)
+        self.assertRedirects(res, '/users/login?next=/pages/mypage/detail')
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/pages/mypage/comment', form_data)
+        self.assertRedirects(res, '/pages/mypage/detail')
+        self.assertEqual(Comment.objects.count(), 3)
+
+    def test_comment_delete(self):
+        self.client.login(username=USERNAME, password=PASSWORD)
+        res = self.client.post('/comments/1/delete')
+        self.assertEqual(res.status_code, 403)
+        self.client.login(username=ADMIN_USERNAME, password=ADMIN_PASSWORD)
+        res = self.client.post('/comments/1/delete')
+        self.assertRedirects(res, '/pages/mypage/detail')
+        self.assertEqual(Template.objects.count(), 1)
