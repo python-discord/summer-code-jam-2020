fe103130f6a86ccc3c22308023c4d6d0e07cebca
Cameron Smart <smartcameron@gmail.com>
2020-08-07 00:22:17 -0700

    Added super fancy zoom features!

--- mysterious-mice/spacebook/welcome_page/static/javascript/welcome.js
+++ mysterious-mice/spacebook/welcome_page/static/javascript/welcome.js
@@ -1,3 +1,23 @@
-function pan_up(){
-    document.getElementById('')
+function pan_up() {
+    var stars = document.getElementById("star_holder");
+    var button_zoom = document.getElementsByClassName("button_zoom");
+    var button_size = 100;
+    var star_width = 0;
+    var id = setInterval(frame, 5);
+
+    function frame() {
+        if (star_width >= 100) {
+            clearInterval(id);
+            window.location.replace("/home");
+        } else {
+            star_width += 5;
+            button_size -= 5;
+            stars.style.width = star_width + '%';
+            stars.style.height = star_width + '%';
+            for (i = 0; i < button_zoom.length; i++) {
+                button_zoom[i].style.scale = button_size + "%";
+            }
+        }
+    }
 }
+
