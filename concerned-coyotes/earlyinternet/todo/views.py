afc1e18406417de3f9133551c58b0d57d36bd8ef
MushroomMaula <maxrd79@gmail.com>
2020-08-07 13:40:00 +0200

    Add TodoEntryCreate view

--- concerned-coyotes/earlyinternet/todo/views.py
+++ concerned-coyotes/earlyinternet/todo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import TodoEntry
+
+
+class TodoEntryCreate(CreateView):
+    model = TodoEntry
+    template_name = 'todo_create_form.html'
+    fields = ['name']
