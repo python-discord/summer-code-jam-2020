7241ea42c631fa85dc9e446f1d44957fbb524981
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-07 11:34:22 +0530

    Modifications to the news feed model

--- glossy-gorillas/core/models/news_feed.py
+++ glossy-gorillas/core/models/news_feed.py
@@ -6,12 +6,12 @@ from django.utils.translation import gettext as _
 class NewsFeed(models.Model):
     """A news feed model to be displayed on the dashboard"""
 
-    title = models.CharField(verbose_name=_("Title"), max_length=36)
-    location = models.CharField(verbose_name=_("Location"), max_length=10)
-    datetime = models.DateTimeField(
+    title = models.CharField(verbose_name=_("Title"), max_length=100)
+    location = models.CharField(verbose_name=_("Location"), max_length=50)
+    date_published = models.DateTimeField(
         default=timezone.now, verbose_name=_("Date Published")
     )
-    description = models.TextField(verbose_name=_("Description"), max_length=225)
+    description = models.TextField(verbose_name=_("Description"))
 
     def __str__(self):
         return self.title
