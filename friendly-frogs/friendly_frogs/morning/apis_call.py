a816708bad60e4d62983f223a39e782cf593fd44
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-08-07 17:55:48 +0200

    Removing flake8 fails

--- friendly-frogs/friendly_frogs/morning/apis_call.py
+++ friendly-frogs/friendly_frogs/morning/apis_call.py
@@ -1,21 +1,22 @@
 from newsapi.newsapi_client import NewsApiClient
 
 NEWS_SOURCES = (
-    ('bbc-news','BBC NEWS'),
+    ('bbc-news', 'BBC NEWS'),
 )
+
 
 class News:
 
     api_key = '552022408c394577825bfd63f2d59a42'
 
     def __init__(self):
-        self._news = NewsApiClient(api_key=self.api_key)
-        
-    
-    def get_news(self,source):
-        news = self._news.get_top_headlines(sources=source) 
+        self._news = NewsApiClient(
+            api_key=self.api_key
+            )
+
+    def get_news(self, source):
+        news = self._news.get_top_headlines(sources=source)
         if news['articles'] == []:
             return "There is no article for you"
         else:
             return news
-
