951f4150f38a629b186870a90199c2a17a295d92
makubob <makupi@icloud.com>
2020-08-08 01:27:53 +0200

    Change News.get_news to return List of NewsInterfaces

--- friendly-frogs/friendly_frogs/morning/apis_call.py
+++ friendly-frogs/friendly_frogs/morning/apis_call.py
@@ -1,20 +1,34 @@
+from typing import List
+
 from newsapi.newsapi_client import NewsApiClient
 
-NEWS_SOURCES = (
-    ('bbc-news', 'BBC NEWS'),
-)
+from .interfaces import NewsInterface
+
+NEWS_SOURCES = (("bbc-news", "BBC NEWS"),)
 
 
 class News:
-
-    api_key = '552022408c394577825bfd63f2d59a42'
+    api_key = "552022408c394577825bfd63f2d59a42"
 
     def __init__(self):
         self._news = NewsApiClient(api_key=self.api_key)
 
-    def get_news(self, source):
+    def get_news(self, source: str) -> List[NewsInterface]:
         news = self._news.get_top_headlines(sources=source)
-        if news['articles'] == []:
-            return "There is no article for you"
+        if not news["articles"]:
+            return []
         else:
-            return news
+            articles = list()
+            for article in news["articles"]:
+                source = article.get("source", {}).get("name")
+                articles.append(
+                    NewsInterface(
+                        title=article.get("title"),
+                        description=article.get("description"),
+                        url=article.get("url"),
+                        source=source,
+                        author=article.get("author"),
+                        thumbnail_url=article.get("urlToImage"),
+                    )
+                )
+            return articles
