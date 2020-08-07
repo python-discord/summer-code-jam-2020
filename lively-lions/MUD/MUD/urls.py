70d21fb0412865ae9b72648177b2a1d346cda3f9
whywhyy <kangsan5240@naver.com>
2020-08-08 02:39:23 +0900

    ADD CSRF with view

--- lively-lions/MUD/MUD/urls.py
+++ lively-lions/MUD/MUD/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('dungeon.urls'))
+    path('', include('dungeon.urls'))
 ]
--- lively-lions/MUD/dungeon/urls.py
+++ lively-lions/MUD/dungeon/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, re_path
-
 from .views import example_view
+from .views import frontend_view
 
-# #
-# prefix : api/
-# example : localhost:8080/api/example
+
+##
+# matters that require attention
+# Get method  - localhost:8000/api/example/ (o)
+#             - localhost:8000/api/example  (maybe o)
+# Post method - localhost:8000/api/example/ (o)
+#             - localhost:8000/api/example/ (x) (I checked that it didn't work.)
+##
 urlpatterns = [
-    path('example/', example_view.rest),
-    re_path(r'example/[0-9a-zA-Z]', example_view.rest),
+    path('api/example/', example_view.rest),
+    re_path(r'api/example/[0-9a-zA-Z]', example_view.rest),
+    path('', frontend_view.index, name='index')
 ]
