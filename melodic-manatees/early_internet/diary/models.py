610b7680cab768e576747dfe30280e97878455e5
chris <cdrandin@hotmail.com>
2020-08-06 22:31:44 -0700

    minor change to diary model

--- melodic-manatees/early_internet/diary/models.py
+++ melodic-manatees/early_internet/diary/models.py
@@ -1,13 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.utils import timezone
 
 
 class DiaryEntry(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    content = models.TextField
+    content = models.TextField(blank=True)
     date_created = models.DateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
-        return self.title
+        return f'{self.title} -- {self.content[:50]}'
