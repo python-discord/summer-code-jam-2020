e8846c03281c954e08f184344684b2d9e1ddb45f
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-07 10:31:01 +0700

    Change HubUser model display in django admin

--- acceptable-albatrosses/albatrosses_hub/authentication/admin.py
+++ acceptable-albatrosses/albatrosses_hub/authentication/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import HubUser
 
-# Register your models here.
-admin.site.register(HubUser)
+
+class HubUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "username",)
+
+admin.site.register(HubUser, HubUserAdmin)
