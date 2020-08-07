c108d0fa183aa8b75b09c27f1401b7062445e7f0
eikea <eikeaaa15@gmail.com>
2020-08-06 23:38:48 -0500

    Added user component for directory

--- whimsical-woodpeckers/www/src/py/components/directory/user.py
+++ whimsical-woodpeckers/www/src/py/components/directory/user.py
@@ -1,8 +1,8 @@
-
 from pyvue import Component
 
 class User(Component):
-    data = ["name","last_online","id"]
 
-    def list_name(self):
-        return({ self.data.name })
+    props = ["name","last_online","id"]
+
+##    def list_name(self):
+##        return({ self.data.name })
