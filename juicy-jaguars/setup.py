cda5b9936e11fad3f66c94f76850f82a2b5cdc37
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2020-08-08 13:46:22 +0100

    Updated setup.py

--- juicy-jaguars/setup.py
+++ juicy-jaguars/setup.py
@@ -9,7 +9,6 @@ def supports_color():
     plat = sys.platform
     supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
                                                   'ANSICON' in os.environ)
-    # isatty is not always implemented, #6223.
     is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
     return supported_platform and is_a_tty
 
