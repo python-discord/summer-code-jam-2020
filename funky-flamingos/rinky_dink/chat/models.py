9e5b3fd47e01ff893bd684731e0b79540d9b890a
volt9801 <pranshuj73@gmail.com>
2020-08-08 21:25:10 +0530

    created message model

--- funky-flamingos/rinky_dink/chat/models.py
+++ funky-flamingos/rinky_dink/chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Messages(models.Model):
+    message = models.CharField(max_length=500)
+    time = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    recipent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipent')
