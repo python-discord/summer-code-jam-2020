def879dcec62685ffc64f4bf52693906ce3e867a
aedwardg <aedwardg1@gmail.com>
2020-08-08 12:26:53 -0700

    added newsfeed to dashboard

--- melodic-manatees/early_internet/main/views.py
+++ melodic-manatees/early_internet/main/views.py
@@ -1,17 +1,36 @@
+import requests
+from decouple import config
 from django.shortcuts import render
 from users.models import UserPreferences
-from news.views import news_feed
 
 
 def main(request):
+    news_key = config('NEWS_KEY')
+    url = 'https://api.nytimes.com/svc/topstories/v2/us.json?api-key=' + news_key
+    nyt_usnews = requests.get(url).json()
+    nyt_usnews_list = []
+
+    for top_story in range(0, 4):
+        nyt_usnews_list.append(
+            {'title': nyt_usnews['results'][top_story]['title'],
+             'abstract': nyt_usnews['results'][top_story]['abstract'],
+             'url': nyt_usnews['results'][top_story]['url'],
+             'img_small': nyt_usnews['results'][top_story]['multimedia'][0]['url']
+             }
+        )
+
+    context = {
+        'news': nyt_usnews_list,
+    }
+
     if request.user.is_authenticated:
-        # MAYBE MOVE NEWS VIEW INTO HERE
-        # news_data = news_feed(request)
-        context = {
-            'pref': UserPreferences.objects.get(user=request.user)
-        }
+        context.update(
+            {
+                'pref': UserPreferences.objects.get(user=request.user)
+            }
+        )
         return render(request, 'main/dashboard.html', context)
-    return render(request, 'main/dashboard.html')
+    return render(request, 'main/dashboard.html', context)
 
 
 def about(request):
