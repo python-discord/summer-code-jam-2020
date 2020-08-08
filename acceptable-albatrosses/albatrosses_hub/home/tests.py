fa533d36e71a4c50dfb9751a6758b991d61e5b89
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-08 18:24:56 +0700

    Implement unit tests for home app

--- acceptable-albatrosses/albatrosses_hub/home/tests.py
+++ acceptable-albatrosses/albatrosses_hub/home/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class HomeTestCase(TestCase):
+    """Class to perform unit tests of home app."""
+
+    def test_home_url(self):
+        response = Client().get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_use_template(self):
+        response = Client().get("/")
+        self.assertTemplateUsed(response, "home.html")
