1663fa2d249f7d13ceb3a90ce112a7c8003de593
fliepeltje <donatas@rasiukevicius.dev>
2020-08-08 19:50:07 +0200

    Change static urls to reversable urls

--- glossy-gorillas/core/views/inventoryrecord.py
+++ glossy-gorillas/core/views/inventoryrecord.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core import models
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 
 class InventoryAdditionForm(LoginRequiredMixin, CreateView):
     model = models.InventoryRecord
     template_name = "core/inventory_form.html"
-    success_url = "../dashboard"
+    success_url = reverse_lazy("dashboard")
     fields = ["product", "quantity", "quantity_type"]
 
     def form_valid(self, form):
--- glossy-gorillas/core/views/review.py
+++ glossy-gorillas/core/views/review.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from core import models
 
 
@@ -7,7 +8,7 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
     model = models.Review
     fields = ["trade", "rating", "description"]
     template_name = "core/review_create_form.html"
-    success_url = "../dashboard"
+    success_url = reverse_lazy("dashboard")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
