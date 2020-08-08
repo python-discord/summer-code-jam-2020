6364b60227cc26e3e0f27209a7a961bca1d1c4a2
Alexandre Pinto <alexj@MacBook-Pro.local>
2020-08-08 12:18:51 +0100

    provide template for blog

--- hypnotic-hedgehogs/a_brief_history_of_web/a_brief_history_of_web/urls.py
+++ hypnotic-hedgehogs/a_brief_history_of_web/a_brief_history_of_web/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('', views.index, name='main-page'),
     path('admin/', admin.site.urls),
     path('web_portal', include('web_portal.urls', namespace='web_portal')),
-    path('ninetys_blog', include('ninetys_blog.urls', namespace='ninetys_blog')),
+    path('nineties_blog', include('nineties_blog.urls', namespace='nineties_blog')),
     path('first_google', include('first_google.urls', namespace='first_google')),
     path('first_twitter', include('first_twitter.urls', namespace='first_twitter')),
     path('first_youtube', include('first_youtube.urls', namespace='first_youtube')),
---
+++ hypnotic-hedgehogs/a_brief_history_of_web/nineties_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NinetiesBlogConfig(AppConfig):
+    name = 'nineties_blog'
