b7962b0a9ab98f66fdd76fb9627d9a877758515f
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2020-08-09 00:03:44 +0100

    Linting

--- juicy-jaguars/setup_beta.py
+++ juicy-jaguars/setup_beta.py
@@ -2,7 +2,9 @@
 import os
 import sys
 import subprocess
-subprocess.call(["pip3", "install", "scprint"], stdout=open(os.devnull, 'w')); from scprint import print
+subprocess.call(["pip3", "install", "scprint"], stdout=open(os.devnull, 'w'))
+from scprint import print  # noqa: E402
+
 
 def install(Verbose=False):
     """Install Web95."""
@@ -12,7 +14,7 @@ def install(Verbose=False):
     else:
         stdout = open(os.devnull, 'w')
         stderr = open(os.devnull, 'w')
-    
+
     print("Cloning Repository...", color="Orange1")
     result = subprocess.call(["git", "clone",
                               "https://github.com/Juicy-Jaguars/\
@@ -49,6 +51,7 @@ summer-code-jam-2020"], stdout=stdout, stderr=stderr)
     print("Successfully installed. Run with 'python Web95/manage.py\
  runserver' from summer-code-jam-2020/juicy-jaguars/", color="LightGreen")
 
+
 if __name__ == "__main__":
     try:
         if sys.argv[1] == "install":
@@ -56,10 +59,13 @@ if __name__ == "__main__":
                 if sys.argv[2] == "-v":
                     install(True)
                 else:
-                    print("Incorrect usage of ", os.path.basename(__file__), "! (3)", file=sys.stderr, sep="", color="Red")
+                    print("Incorrect usage of ", os.path.basename(__file__),
+                          "! (3)", file=sys.stderr, sep="", color="Red")
             except IndexError:
                 install()
         else:
-            print("Incorrect usage of ", os.path.basename(__file__), "! (2)", file=sys.stderr, sep="", color="Red")
+            print("Incorrect usage of ", os.path.basename(__file__), "! (2)",
+                  file=sys.stderr, sep="", color="Red")
     except IndexError:
-        print("Incorrect usage of ", os.path.basename(__file__), "! (1)", file=sys.stderr, sep="", color="Red")
+        print("Incorrect usage of ", os.path.basename(__file__), "! (1)",
+              file=sys.stderr, sep="", color="Red")
