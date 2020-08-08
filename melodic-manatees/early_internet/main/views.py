cf75217f17249875b77a75ca271ebaeacfc0daf5
aedwardg <aedwardg1@gmail.com>
2020-08-08 02:57:25 -0700

    added diary functionality to main dashboard

--- melodic-manatees/early_internet/main/views.py
+++ melodic-manatees/early_internet/main/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from users.models import UserPreferences
 from diary.models import DiaryEntry
+from diary.forms import DiaryEntryForm
 
 
 def main(request):
     if request.user.is_authenticated:
+        try:
+            entries = DiaryEntry.objects.filter(creator=request.user)
+        except ObjectDoesNotExist:
+            entries = None
+
+        if request.method == 'POST':
+            form = DiaryEntryForm(request.POST)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.creator = request.user
+                instance.save()
+                return redirect('/')
+        else:
+            form = DiaryEntryForm()
+
         context = {
             'pref': UserPreferences.objects.get(user=request.user),
-            'entries': DiaryEntry.objects.get(user=request.user)
+            'entries': entries,
+            'form': form
         }
         return render(request, 'main/dashboard.html', context)
     return render(request, 'main/dashboard.html')
