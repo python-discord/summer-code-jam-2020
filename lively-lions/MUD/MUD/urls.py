02db82880f9efc43a40fbc0bed7c806f0c632bfa
Zorakinezear <alexaderviera@gmail.com>
2020-08-08 12:37:18 -0700

    main branch update

--- lively-lions/MUD/MUD/urls.py
+++ lively-lions/MUD/MUD/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dungeon.urls'))
+    path('', include('dungeon.urls')),
+    path('items/', include('objects.urls')),
 ]
