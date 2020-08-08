541e322bebeb0657f269d8e14cd0781e0193d05a
Peter DeVita <mewtwo2643@yahoo.com>
2020-08-08 14:43:15 -0400

    Added websocket wrapper

---
+++ whimsical-woodpeckers/www/src/py/websocket.py
@@ -0,0 +1,33 @@
+# __pragma__('alias', 'js_websocket', 'WebSocket')
+
+
+# __pragma__("kwargs")
+class WebSocket:
+    def __init__(self, url, protocols=None):
+        self._socket = __new__(js_websocket(url, protocols))
+        self._socket.onmessage = lambda event: self.on_message(event)
+        self._socket.onerror = lambda: self.on_error()
+        self._socket.onclose = lambda event: self.on_close(event)
+        self._socket.onopen = lambda event: self.on_open(event)
+
+    def send(self, data):
+        self._socket.send(data)
+
+    def close(self, code=None, reason=None):
+        self._socket.close()
+
+    def on_message(self, event):
+        pass
+
+    def on_error(self):
+        pass
+
+    def on_open(self, event):
+        pass
+
+    def on_close(self, event):
+        pass
+
+# __pragma__("nokwargs")
+
+
