ad17d54e0c6a36d20d3e110fe5e1c9e4996a2b02
MushroomMaula <maxrd79@gmail.com>
2020-08-08 19:27:02 +0200

    Make key constant

--- concerned-coyotes/earlyinternet/weather/utils.py
+++ concerned-coyotes/earlyinternet/weather/utils.py
@@ -3,15 +3,17 @@ from datetime import datetime
 import requests
 
 
+WEATHER_KEY = '0a2c03e9dee2533389d7b459d0f8595e'
+
+
 def get_weather(lat: float, lon: float) -> dict:
     """
     Take latitude and longitude to get weather data
     using openweather api and return data in dict format
     """
-    weather_key = '0a2c03e9dee2533389d7b459d0f8595e'
     # source contain JSON data from API
     source = requests.get('http://api.openweathermap.org/data/2.5/weather?'
-                          f'lat={lat}&lon={lon}&appid={weather_key}')
+                          f'lat={lat}&lon={lon}&appid={WEATHER_KEY}')
     # list_of_data contain json encoded content
     data = source.json()
     result = {"weather": {
