2c1fcd950bb6429f1c1090699e30bd70ff6bc34f
aedwardg <aedwardg1@gmail.com>
2020-08-08 00:03:34 -0700

    added DiaryEntry to main view

--- melodic-manatees/early_internet/main/views.py
+++ melodic-manatees/early_internet/main/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from users.models import UserPreferences
+from diary.models import DiaryEntry
 
 
 def main(request):
     if request.user.is_authenticated:
         context = {
             'pref': UserPreferences.objects.get(user=request.user),
+            'entries': DiaryEntry.objects.get(user=request.user)
         }
         return render(request, 'main/dashboard.html', context)
     return render(request, 'main/dashboard.html')
