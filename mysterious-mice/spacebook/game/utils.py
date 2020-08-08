3de026eb34984fed2ad3608c441add78583ba71c
Kenny <kfonatoko@gmail.com>
2020-08-07 22:14:22 -0400

    Game development

--- mysterious-mice/spacebook/game/utils.py
+++ mysterious-mice/spacebook/game/utils.py
@@ -1,23 +1,66 @@
-import re
+# All coordinates are stored as tuples of (x, y)
+# x is east-west with east being positive
+# y is north-south with north being positive
 
 
 def new_game():
-    # coordinates tuples of (x, y)
-    # x is east-west with east being positive
-    # y is north-south with north being positive
+    """
+    Generates a new game, resetting the location of all components and the rover.
+    """
 
     game_data = {
+        "initials": "",
         "rover": (0, 0),
+        "battery": 100,
         "plutonium": (7, 5),
+        "has_plutonium": False,
         "solar_panels": (-3, 2),
+        "has_solar_panels": False,
+        "dust_storm": (-3, 2),
         "wind": (-1, 0),
         "small_crater": (-4, -3),
-        "messages": ["Welcome to Crash Landing!"],
+        "messages": [
+            {"from_rover": False, "message": "Crash Landing!"},
+            {"from_rover": False, "message": ""},
+            {
+                "from_rover": False,
+                "message": "You play as a rover that has just had a crash landing on Mars.",
+            },
+            {
+                "from_rover": False,
+                "message": "The rover lost its two power sources in the crash.",
+            },
+            {
+                "from_rover": False,
+                "message": "Plutonium is the rover's main power source.",
+            },
+            {
+                "from_rover": False,
+                "message": "The goal of the game is to retrieve the plutonium before the battery depletes.",
+            },
+            {
+                "from_rover": False,
+                "message": "Retrieving the rover's solar panels will allow you to travel more efficiently.",
+            },
+            {
+                "from_rover": False,
+                "message": 'Type "help" at any time to review the controls.',
+            },
+            {"from_rover": False, "message": "Good luck!",},
+            {"from_rover": False, "message": ""},
+        ],
+        "game_over": False,
+        "victorious": False,
     }
     return game_data
 
 
 def get_game(request):
+    """
+    Attempt to retrieve the current game from sessions. If no game is found,
+    generate a new game.
+    """
+
     is_cached = "game_data" in request.session
     if not is_cached:
         game_data = new_game()
@@ -27,8 +70,50 @@ def get_game(request):
     return game_data
 
 
+def command_help(game_data):
+    """
+    Provide text commands to control the rover.
+    """
+
+    help_messages = [
+        {"from_rover": False, "message": "Help:"},
+        {"from_rover": False, "message": ""},
+        {"from_rover": False, "message": "Movement:"},
+        {
+            "from_rover": False,
+            "message": "You can move by clicking the buttons with the cardinal directions.",
+        },
+        {
+            "from_rover": False,
+            "message": "You can also move through text commands. Examples of valid commands:",
+        },
+        {"from_rover": False, "message": ">> move n",},
+        {"from_rover": False, "message": ">> move north",},
+        {"from_rover": False, "message": ""},
+        {
+            "from_rover": False,
+            "message": "Look around for hints of where the components might be. Examples of valid commands:",
+        },
+        {"from_rover": False, "message": ">> look n",},
+        {"from_rover": False, "message": ">> look north",},
+        {"from_rover": False, "message": ""},
+        {
+            "from_rover": False,
+            "message": 'Type "new game" at any point to start a new game.',
+        },
+    ]
+    game_data["messages"] = game_data["messages"] + help_messages
+    return game_data
+
+
 def command_move(game_data, direction):
+    """
+    Moves the rover, depletes the battery, and picks up a component if one is found.
+    """
     old_position = game_data["rover"]
+    success = True
+
+    # Attempt to move the rover
     if direction in ["n", "north"]:
         new_position = (old_position[0], old_position[1] + 1)
     elif direction in ["e", "east"]:
@@ -39,54 +124,149 @@ def command_move(game_data, direction):
         new_position = (old_position[0] - 1, old_position[1])
     else:
         new_position = old_position
-    game_data["messages"].append(f"Your new location is {new_position}")
+        success = False
+
+    # Deplete battery if successfully moved
+    if success:
+        game_data["battery"] = game_data["battery"] - 5
+        if game_data["has_solar_panels"]:
+            game_data["battery"] = game_data["battery"] + 2
+
+    # Move components with rover
+    if game_data["has_solar_panels"]:
+        game_data["solar_panels"] = new_position
+    if game_data["has_plutonium"]:
+        game_data["plutonium"] = new_position
+
+    # Pick up components
+    if (
+        new_position[0] == game_data["solar_panels"][0]
+        and new_position[1] == game_data["solar_panels"][1]
+        and not game_data["has_solar_panels"]
+    ):
+        game_data["has_solar_panels"] = True
+        game_data["messages"].append(
+            {"from_rover": False, "message": f"You have retrieved the solar panels!"}
+        )
+        game_data["messages"].append(
+            {
+                "from_rover": False,
+                "message": f"It will take less energy to traverse the terrain of Mars now!",
+            }
+        )
+    if (
+        new_position[0] == game_data["plutonium"][0]
+        and new_position[1] == game_data["plutonium"][1]
+        and not game_data["has_plutonium"]
+    ):
+        game_data["has_plutonium"] = True
+        game_data["messages"].append(
+            {"from_rover": False, "message": f"You have retrieved the plutonium!"}
+        )
+
+    game_data["messages"].append(
+        {"from_rover": False, "message": f"Your new location is {new_position}"}
+    )
     game_data["rover"] = new_position
     return game_data
 
+
 def command_look(game_data, direction):
-    rover = game_data.get('rover')
-    plutonium = game_data.get('plutonium')
-    solar_panels = game_data.get('solar_panels')
+    rover = game_data.get("rover")
+    plutonium = game_data.get("plutonium")
+    solar_panels = game_data.get("solar_panels")
     plutonium_message = "There is a plume of smoke in the distance."
-    panel_message = "There is a plume of smoke in the distance."
+    panel_message = "There is an object reflecting light in the distance."
+    new_message = []
+
     if direction in ["n", "north"]:
-        if rover[0] == plutonium[0] and rover[1] < plutonium[1]: #if the rover is inline wih plutonium and plutonium is north of rover
-            new_message = plutonium_message
-        if rover[0] == solar_panels[0] and rover[1] < solar_panels[1]:#if the rover is inline wih solar panels and solar panels is north of rover
-            new_message = panel_message
+        if (
+            rover[0] == plutonium[0] and rover[1] < plutonium[1]
+        ):  # if the rover is inline wih plutonium and plutonium is north of rover
+            new_message.append({"from_rover": False, "message": plutonium_message})
+        if (
+            rover[0] == solar_panels[0] and rover[1] < solar_panels[1]
+        ):  # if the rover is inline wih solar panels and solar panels is north of rover
+            new_message.append({"from_rover": False, "message": panel_message})
     elif direction in ["s", "south"]:
         if rover[0] == plutonium[0] and rover[1] > plutonium[1]:
-            new_message = plutonium_message
+            new_message.append({"from_rover": False, "message": plutonium_message})
         if rover[0] == solar_panels[0] and rover[1] > solar_panels[1]:
-            new_message = panel_message
+            new_message.append({"from_rover": False, "message": panel_message})
     elif direction in ["w", "west"]:
-        if rover[1] == plutonium[1] and rover[1] < plutonium[1]:
-            new_message = plutonium_message
-        if rover[1] == solar_panels[1] and rover[1] < solar_panels[1]:
-            new_message = panel_message
+        if rover[1] == plutonium[1] and rover[0] > plutonium[0]:
+            new_message.append({"from_rover": False, "message": plutonium_message})
+        if rover[1] == solar_panels[1] and rover[0] > solar_panels[0]:
+            new_message.append({"from_rover": False, "message": panel_message})
     elif direction in ["e", "east"]:
-        if rover[1] == plutonium[1] and rover[1] > plutonium[1]:
-            new_message = plutonium_message
-        if rover[1] == solar_panels[1] and rover[1] > solar_panels[1]:
-            new_message = panel_message
-    game_data.messages.append(new_message)
+        if rover[1] == plutonium[1] and rover[0] < plutonium[0]:
+            new_message.append({"from_rover": False, "message": plutonium_message})
+        if rover[1] == solar_panels[1] and rover[0] < solar_panels[0]:
+            new_message.append({"from_rover": False, "message": panel_message})
+
+    if new_message == []:
+        new_message = [
+            {"from_rover": False, "message": "Nothing of interest was found."}
+        ]
+
+    game_data["messages"] = game_data["messages"] + new_message
     return game_data
 
 
 def parse_command(request, game_data, command):
-    game_data["messages"].append(command)
-    if command != "" and command != None:
-        command = command.replace(" ", "").lower()
+    """
+    Parses the user's input and runs the applicable command.
+    Returns game_data to the view to be displayed.
+    """
+    game_data["messages"].append({"from_rover": True, "message": command})
+    command = command.replace(" ", "").lower()
+
+    if not game_data["game_over"]:
         if command.startswith("move"):
             game_data = command_move(game_data, command[4:])
         elif command.startswith("look"):
             game_data = command_look(game_data, command[4:])
         elif command == "help":
-            pass
-        elif command == "newgame":
-            game_data = new_game()
-            request.session["game_data"] = new_game()
+            game_data = command_help(game_data)
         else:
-            game_data["messages"].append(f"Command not understood: {command}")
+            game_data["messages"].append(
+                {"from_rover": False, "message": f"Command not understood: {command}"}
+            )
+    elif command == "newgame":
+        game_data = new_game()
+        request.session["game_data"] = new_game()
+
+    # Win if plutonium was retrieved
+    if game_data["has_plutonium"] and not game_data["victorious"]:
+        game_data["victorious"] = True
+        game_data["game_over"] = True
+        game_data["messages"].append({"from_rover": False, "message": ""})
+        game_data["messages"].append(
+            {"from_rover": False, "message": "CONGRATULATIONS!"}
+        )
+        game_data["messages"].append(
+            {
+                "from_rover": False,
+                "message": "Your rover was able to reconnect its power supply!",
+            }
+        )
+
+    # Lose if battery reaches 0, unless plutonium was found this move
+    if (
+        game_data["battery"] <= 0
+        and not game_data["game_over"]
+        and not game_data["victorious"]
+    ):
+        game_data["battery"] = 0
+        game_data["messages"].append({"from_rover": False, "message": ""})
+        game_data["messages"].append({"from_rover": False, "message": "GAME OVER!"})
+        game_data["game_over"] = True
+
+    # Direct player to start new game if
+    if game_data["game_over"]:
+        game_data["messages"].append(
+            {"from_rover": False, "message": 'Type "new game" to start a new game.'}
+        )
+
     request.session["game_data"] = game_data
     return game_data
