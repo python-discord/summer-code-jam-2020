afe35041ab582b1a51ef7a4c54953b78af32a168
MushroomMaula <maxrd79@gmail.com>
2020-08-08 17:47:25 +0200

    Add tests TodoEntryCreate view

--- concerned-coyotes/earlyinternet/todo/tests.py
+++ concerned-coyotes/earlyinternet/todo/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from .models import TodoEntry
 
@@ -25,3 +25,31 @@ class TestTodoEntry(TestCase):
             str(self.todo),
             'Finish summer code jam - unfinished'
         )
+
+
+class TestTodoCreateView(TestCase):
+
+    def setUp(self) -> None:
+        self.user = test_user()
+
+    def test_login_required(self):
+        resp = self.client.get(
+            '/todo/add/'
+        )
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.url, '/account/login?next=/todo/add/')
+
+    def test_add_entry(self):
+        logged_in = self.client.login(username='John Doe', password='hunter2')
+        self.assertEqual(logged_in, True)
+
+        resp = self.client.post(
+            '/todo/add/', {'name': 'test-todo-entry'}
+        )
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.url, '/')
+
+        created = TodoEntry.objects.get(name='test-todo-entry')
+        self.assertIsNotNone(created)
+        self.assertEqual(created.user, self.user)
