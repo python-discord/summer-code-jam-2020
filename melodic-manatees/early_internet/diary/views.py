66f2ea53bbf3ba524f19089aea5154afda5b1c83
chris <cdrandin@hotmail.com>
2020-08-07 22:00:17 -0700

    added login_required decorator

--- melodic-manatees/early_internet/diary/views.py
+++ melodic-manatees/early_internet/diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
@@ -7,17 +8,20 @@ from diary.forms import DiaryEntryForm
 from diary.models import DiaryEntry
 
 
+@login_required
 def diary_entry_list(request):
     object_list = DiaryEntry.objects.all().select_related('creator')
     return render(request, 'diary/list.html', {'object_list': object_list})
 
 
+@login_required
 def diary_entry_detail(request, id):
     obj = get_object_or_404(
         DiaryEntry.objects.select_related('creator'), id=id)
     return render(request, 'diary/detail.html', {'object': obj})
 
 
+@login_required
 def diary_entry_create(request):
     if request.method == 'POST':
         form = DiaryEntryForm(request.POST)
@@ -32,6 +36,7 @@ def diary_entry_create(request):
     return render(request, 'diary/create.html', {'form': form})
 
 
+@login_required
 def diary_entry_update(request, id):
     obj = get_object_or_404(
         DiaryEntry.objects.select_related('creator'), id=id)
@@ -44,6 +49,7 @@ def diary_entry_update(request, id):
     return render(request, 'diary/update.html', {'form': form})
 
 
+@login_required
 def diary_entry_delete(request, id):
     obj = get_object_or_404(
         DiaryEntry.objects.select_related('creator'), id=id)
