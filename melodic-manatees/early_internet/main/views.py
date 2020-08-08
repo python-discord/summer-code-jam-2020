230903a230e4daee8793092029fdb52be005e70e
aedwardg <aedwardg1@gmail.com>
2020-08-08 00:21:20 -0700

    tried to use include

--- melodic-manatees/early_internet/main/views.py
+++ melodic-manatees/early_internet/main/views.py
@@ -6,10 +6,9 @@ from news.views import news_feed
 def main(request):
     if request.user.is_authenticated:
         # MAYBE MOVE NEWS VIEW INTO HERE
-        news_data = news_feed(request)
+        # news_data = news_feed(request)
         context = {
-            'pref': UserPreferences.objects.get(user=request.user),
-            'news': news_data
+            'pref': UserPreferences.objects.get(user=request.user)
         }
         return render(request, 'main/dashboard.html', context)
     return render(request, 'main/dashboard.html')
