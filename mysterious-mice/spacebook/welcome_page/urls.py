1bf94a279131ed44b05e3095db4a1e019ed844a8
Kenny <kfonatoko@gmail.com>
2020-08-08 14:55:44 -0400

    Add under construction pages and an exit site link

--- mysterious-mice/spacebook/welcome_page/urls.py
+++ mysterious-mice/spacebook/welcome_page/urls.py
@@ -5,5 +5,5 @@ from .views import Construction
 app_name = "welcome_page"
 urlpatterns = [
     path("", Welcome.as_view(), name="welcome_page"),
-    path("test/", Construction.as_view(), name="under_construction"),
+    path("underconstruction/", Construction.as_view(), name="under_construction"),
 ]
