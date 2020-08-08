2f6c155854f49da8d4bf5bf824cbdfe395e42451
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-08-08 21:27:16 +0700

    Remove unused  model

--- acceptable-albatrosses/albatrosses_hub/authentication/models.py
+++ acceptable-albatrosses/albatrosses_hub/authentication/models.py
@@ -1,22 +1,3 @@
-from django.db import models
-from django.contrib.auth.hashers import make_password
-
+# from django.db import models
 
 # Create your models here.
-class HubUser(models.Model):
-    """A Class model that will store user information."""
-
-    email = models.EmailField(primary_key=True)
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=512)
-
-    def __str__(self):
-        # Will display username on Django admin
-        return self.username
-
-    def process(self, form):
-        """Function to store form data into django model."""
-
-        self.email = form.cleaned_data["email"]
-        self.username = form.cleaned_data["username"]
-        self.password = make_password(form.cleaned_data["password"])
