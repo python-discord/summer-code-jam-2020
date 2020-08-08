47936996d7a88a08c0c3684b976255014bd7845a
Antsthebul <anthony.allen.srt@gmail.com>
2020-08-08 16:51:08 -0400

    fixed views

--- rubbery-raccoons/wired/wired_app/urls.py
+++ rubbery-raccoons/wired/wired_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views as wired_views
 
 urlpatterns = [
-    path("article/<slug:slug>/", views.detail.ArticleDetailView.as_view(), name="article-detail"),
-    path("", views.homepage.HomepageView.as_view(), name="homepage"),
-    path("search", views.search.SearchResultsView.as_view(), name="search-results"),
-    path("<int:year>/", views.articleyear.ArticleYearArchiveView.as_view(), name="article-archive-year"),
+    path("article/<slug:slug>/", wired_views.detail.ArticleDetailView.as_view(), name="article-detail"),
+    path("", wired_views.homepage.HomepageView.as_view(), name="homepage"),
+    path("search", wired_views.search.SearchResultsView.as_view(), name="search-results"),
+    path("<int:year>/", wired_views.articleyear.ArticleYearArchiveView.as_view(), name="article-archive-year"),
 ]
