5757487c3e6abbab898bf57b516e227c13a1713a
Justus Sieweke <justus.sieweke@gmail.com>
2020-08-08 13:01:38 +0200

    Removed help_texts for signup

--- imaginary-iguanas/code_jam/users/forms.py
+++ imaginary-iguanas/code_jam/users/forms.py
@@ -7,19 +7,17 @@ from .models import Profile as _Profile
 
 
 class UserUpdateForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
     error_messages = {
-        'password_mismatch': _('The two password fields didn’t match.'),
+        'password_mismatch': _("The two password fields didn't match."),
     }
-    password1 = forms.CharField(
-        label=_("Password"),
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-    )
-    password2 = forms.CharField(
-        label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        strip=False
-    )
+    password1 = forms.CharField(label=_("Password"),
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+                                widget=forms.PasswordInput,)
 
     class Meta:
         model = User
@@ -27,6 +25,23 @@ class UserUpdateForm(forms.ModelForm):
         help_texts = {
             'username': None
         }
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserUpdateForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 class UserPasswordUpdateForm(_PasswordChangeForm):
