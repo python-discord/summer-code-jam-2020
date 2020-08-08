9ab7f279d7304d73f6a1e428df419f5624f21304
mjfernez <mjfernez@gmail.com>
2020-08-08 18:41:14 -0400

    Added team functionality

--- practical-penguins/trivia_tavern/trivia_runner/models.py
+++ practical-penguins/trivia_tavern/trivia_runner/models.py
@@ -10,12 +10,22 @@ from trivia_builder.models import TriviaQuiz
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=24)
     team_name = models.CharField(max_length=24, default='')
     phone_number = models.CharField(max_length=12)
     # Model name needs to be in quotes according to
     # https://docs.djangoproject.com/en/3.0/ref/models/fields/#foreignkey
     active_quiz = models.ForeignKey('ActiveTriviaQuiz', on_delete=models.CASCADE)
+    correct_answers = []
+    wrong_answers = []
+
+    def get_answers(self):
+        answers = 'Here\'s what you got right:\n'
+        for c in self.correct_answers:
+            answers += f'Question {c[0]}, your answer: {c[1]}\n'
+        answers += '\nHere\'s what you got wrong:\n'
+        for w in self.wrong_answers:
+            answers += f'Question {w[0]}, your answer: {w[1]}\n'
+        return answers
 
     def __str__(self):
         return f'{self.name} playing {self.active_quiz.trivia_quiz.name}'
