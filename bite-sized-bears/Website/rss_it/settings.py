5a876d8c4d7fdafc8eff050c0930d728fc4a9c12
Ronit-j <pictronit007@gmail.com>
2020-08-09 04:19:47 +0530

    Revert back to In-memory channels

--- bite-sized-bears/Website/rss_it/settings.py
+++ bite-sized-bears/Website/rss_it/settings.py
@@ -83,20 +83,20 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'rss_it.wsgi.application'
 ASGI_APPLICATION = "rss_it.routing.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(env('REDIS_HOST'), 6379)],
-        },
-    },
-}
-
 # CHANNEL_LAYERS = {
 #     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer"
-#     }
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [(env('REDIS_HOST'), 6379)],
+#         },
+#     },
 # }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 
 
 # Database
