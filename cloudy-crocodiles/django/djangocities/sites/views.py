31fc7598fe4cf04c0771ca764e6ed4518545f9ad
Ansh Saini <ansh@lestransformations.com>
2020-08-08 22:15:46 +0530

    feat: add field file_name to page model

--- cloudy-crocodiles/django/djangocities/sites/views.py
+++ cloudy-crocodiles/django/djangocities/sites/views.py
@@ -7,6 +7,6 @@ from djangocities.pages.models import Page
 def index(request, slug, address):
     city = City.objects.get(slug=slug)
     site = Site.objects.get(city=city, address=address)
-    page = Page.objects.get(site=site, filename="index.html")
+    page = Page.objects.get(site=site, file_name="index.html")
     html = page.content
     return HttpResponse(html)
