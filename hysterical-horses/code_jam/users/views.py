fbe0eb7fa9e3543de4e0ff73771f9f4f2e5489a3
melodicht <marvincht@gmail.com>
2020-08-08 11:11:02 +0800

    Add profile model, template and view
    NOTE: The media settings are NOT for the production environment.
    
    - Installed Pillow library for the ImageField
    - The avatar uploaded by the user will be resized
    - Established media root for debugging purposes
    - Added `default.png` for the default avatar (Kudos to SaxyCat)
    - Added signals so that every user created and updated
    will create and update the user's profile accordingly
    - User and profile update form created
    - `profile.html` created
    - Note that the css for signups has been used

--- hysterical-horses/code_jam/users/views.py
+++ hysterical-horses/code_jam/users/views.py
@@ -2,12 +2,29 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
-from .forms import AccountCreationForm
+from .forms import AccountCreationForm, UserUpdateForm, ProfileUpdateForm
 
 
 @login_required()
-def profile(request):
-    context = {"user_form": None, "profile_form": None}
+def profile_view(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES,
+                                         instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+        "user": request.user
+    }
+
     return render(request, "users/profile.html", context)
 
 
@@ -24,11 +41,3 @@ def signup_view(request):
 
     context = {'form': form}
     return render(request, 'users/signup.html', context)
-
-# def login_user
-
-
-def profile_view(request):
-    context = {'user': None}
-
-    return render(request, "users/profile.html", context)
