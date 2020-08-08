73f32625c5e386b317fcd7651e12b2da7018f9db
MushroomMaula <maxrd79@gmail.com>
2020-08-08 17:42:30 +0200

    Fix urls

--- concerned-coyotes/earlyinternet/todo/urls.py
+++ concerned-coyotes/earlyinternet/todo/urls.py
@@ -5,6 +5,6 @@ from .views import TodoEntryCreate, TodoEntryUpdate
 
 
 urlpatterns = [
-    path('add', login_required(TodoEntryCreate.as_view()), name='todo-create'),
+    path('add/', login_required(TodoEntryCreate.as_view()), name='todo-create'),
     path('update/<int:pk>', login_required(TodoEntryUpdate.as_view()), name='todo-update')
 ]
