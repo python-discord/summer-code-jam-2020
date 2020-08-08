09c6e8087b5929e50c316c8009ca18b5d071f0ad
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-08 12:37:43 +0530

    Include inventory form in init.py

--- glossy-gorillas/core/views/__init__.py
+++ glossy-gorillas/core/views/__init__.py
@@ -2,3 +2,9 @@ from .market import *  # noqa
 from .review import *  # noqa
 from .trader import *  # noqa
 from .home import *  # noqa
+from .inventoryrecord import *  # noqa
+from django.views.generic import TemplateView
+
+
+class Home(TemplateView):
+    template_name = "core/pages/home.html"
