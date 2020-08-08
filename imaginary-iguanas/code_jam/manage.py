python-discord/summer-code-jam-2020

commit 822bdac8c9295d1b0a6117f3fedbc5fd18dc6c3b

Removed help_texts for signup

imaginary-iguanas/code_jam/users/forms.py
@@ -1,11 +1,32 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm as _UserCreationForm, PasswordChangeForm as _PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm as _PasswordChangeForm
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 from .models import Profile as _Profile
 
 
-class UserUpdateForm(_UserCreationForm):
-    pass
+class UserUpdateForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': _('The two password fields didn’t match.'),
+    }
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False
+    )
+
+    class Meta:
+        model = User
+        fields = ("username",)
+        help_texts = {
+            'username': None
+        }
 
 
 class UserPasswordUpdateForm(_PasswordChangeForm):

commit 76a4c3763b385ec75d74402d067b9c3cad366641

Fixed Signup Commit 822bdac8c9295d1b0a6117f3fedbc5fd18dc6c3b introduced a signup bug that is now fixed.

imaginary-iguanas/code_jam/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys

imaginary-iguanas/code_jam/users/forms.py
@@ -7,16 +7,14 @@ from .models import Profile as _Profile
 
 
 class UserUpdateForm(forms.ModelForm):
-    """
-    A form that creates a user, with no privileges, from the given username and
-    password.
-    """
     error_messages = {
         'password_mismatch': _("The two password fields didn't match."),
     }
     password1 = forms.CharField(label=_("Password"),
+                                strip=False,
                                 widget=forms.PasswordInput)
     password2 = forms.CharField(label=_("Password confirmation"),
+                                strip=False,
                                 widget=forms.PasswordInput,)
 
     class Meta: