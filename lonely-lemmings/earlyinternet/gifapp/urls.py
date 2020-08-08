9f48a3027e76280247a8c56244e3595b6d5429b9
Shaw Kagawa <skagawa2@illinois.edu>
2020-08-08 11:18:24 -0700

    Fix: Missing home page path and project path conflict - Changed my project/<project_id>/ paths to feedproject/<project_id>/ for now - Moved feed home to feed/ instead of ""

--- lonely-lemmings/earlyinternet/gifapp/urls.py
+++ lonely-lemmings/earlyinternet/gifapp/urls.py
@@ -6,27 +6,28 @@ from . import userviews
 
 urlpatterns = [
     path("", views.paint, name="paint"),
-    path("save", views.parse_save_request, name="save"),
-    path("back", views.return_home, name="back"),
-    path("project/<str:project_name>/render", views.parse_render_request, name="render"),
-    path("project/<str:project_name>/post", views.parse_image_request, name="images"),
-    path("project/<str:project_name>", views.parse_image_request, name="images"),
+    path("save/", views.parse_save_request, name="save"),
+    path("back/", views.return_home, name="back"),
+    path("project/<str:project_name>/render/", views.parse_render_request, name="render"),
+    path("project/<str:project_name>/post/", views.parse_image_request, name="images"),
+    path("project/<str:project_name>/", views.parse_image_request, name="images"),
 
-    path("about", userviews.about, name="about"),
+    path("feed/", userviews.home, name="home"),
+    path("about/", userviews.about, name="about"),
 
     # template is project_form.html
-    path("project/new/",
+    path("feedproject/new/",
          userviews.ProjectCreateView.as_view(template_name="project_form.html"),
          name="project-create"),
     # template is project_detail.html
-    path("project/<int:pk>/",
+    path("feedproject/<int:pk>/",
          userviews.ProjectDetailView.as_view(template_name="project_detail.html"),
          name="project-detail"),
     # template is project_confirm_delete.html
-    path("project/<int:pk>/delete",
+    path("feedproject/<int:pk>/delete/",
          userviews.ProjectDeleteView.as_view(template_name="project_confirm_delete.html"), name="project-delete"),
     # template is project_detail.html
-    path("project/<int:pk>/update", userviews.ProjectUpdateView.as_view(), name="project-update"),
+    path("feedproject/<int:pk>/update/", userviews.ProjectUpdateView.as_view(), name="project-update"),
 
     # user authentication paths
     path("profile/", userviews.profile, name="profile"),
