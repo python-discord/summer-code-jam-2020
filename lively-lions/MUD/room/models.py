15b22ef82f11c63b3ebf6511fb5c8c8e8eb1666f
Gabriel Ruiz <eliduberry@gmail.com>
2020-08-08 18:57:35 +1200

    room app is not available in MUD app

--- lively-lions/MUD/room/models.py
+++ lively-lions/MUD/room/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    # location coordinates of the room
+    location_x = models.IntegerField()
+    location_y = models.IntegerField()
+    location_z = models.IntegerField()
+
+    at_capacity = models.BooleanField(default=True)
+
+    current_characters = []
+    current_npcs = []
+
+    def get_current_characters(self):
+        return self.current_characters
+
+    def character_in(self, character):
+        self.current_characters.append(character)
+
+    def get_floor(self):
+        return self.location_z
+
+    def character_out(self, character):
+        char_index = self.current_characters.index(character)
+        return self.current_characters.pop(char_index)
+
+
