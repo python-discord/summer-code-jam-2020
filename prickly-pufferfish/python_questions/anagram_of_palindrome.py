866e0acf7a6a63f106dc0647560d16e72111d789
Lakshya <lakshya@Shrutis-MacBook-Air.local>
2020-08-07 18:44:21 -0700

    displayed question

---
+++ prickly-pufferfish/python_questions/anagram_of_palindrome.py
@@ -0,0 +1,22 @@
+"""
+A palindrome is a word that reads the same forward and backwards /
+(eg, "racecar", "tacocat"). /
+An anagram is a rescrambling of a word. /
+eg (for "racecar",you could rescramble this as "arceace"). /
+Write a function that, given a string as a parameter, /
+returns True if the string is an anagram of a palindrome. /
+>>> is_anagram_of_palindrome("a") /
+True /
+
+>>> is_anagram_of_palindrome("ab") /
+False /
+
+>>> is_anagram_of_palindrome("aab") /
+True /
+
+>>> is_anagram_of_palindrome("arceace") /
+True /
+
+>>> is_anagram_of_palindrome("arceaceb") /
+False /
+"""
---
+++ prickly-pufferfish/python_questions/make_change.py
@@ -0,0 +1,21 @@
+"""
+Write a function that, given: /
+1. an amount of money /
+2. a list of coin denominations /
+computes the number of ways to make the amount of money /
+with coins of the available denominations. /
+
+>>> make_change(amount=4, denominations=[1,2,3]) /
+4 /
+[1,1,1,1] /
+[1,1,2] /
+[1,3] /
+[2,2] /
+
+>>> make_change(amount=20, denominations=[5, 10]) /
+3 /
+[5,5,5,5] /
+[5,5,10] /
+[10,10] /
+
+"""
