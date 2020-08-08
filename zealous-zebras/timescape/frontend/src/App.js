412ed9edb7d1743fc19c61737ee532088341c63a
beldin222 <beldin222@gmail.com>
2020-08-08 04:04:40 +0300

    Implement Redux, create "year" in store.
    Added "year" reducer, wrapped "App" element in <Provider>, and wrote actions for "year".

--- zealous-zebras/timescape/frontend/src/App.js
+++ zealous-zebras/timescape/frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { getYear,setYear } from './actions/year'
 
 function App() {
+  const test = useSelector(state => state.year);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <p>TimeScape goes brrrrr</p>
+      <p>Current Year: {test}</p>
+      <input type="text" id="test"/>
+      <button onClick={()=>{dispatch(setYear(document.getElementById('test').value))}}>Set Year</button>
     </div>
   );
 }
--- zealous-zebras/timescape/frontend/src/index.js
+++ zealous-zebras/timescape/frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Import React-Redux, and import redux store from store.js
+import { Provider } from 'react-redux';
+import store from './store';
+
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 // bootstrap js -> uncomment if necesary
@@ -12,7 +16,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
