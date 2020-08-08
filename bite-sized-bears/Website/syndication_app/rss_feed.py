8f14fe173da6dfbb59a3f6fc56bab0566127f0e0
Ronit-j <pictronit007@gmail.com>
2020-08-09 03:16:58 +0530

    Sort rss feed as latest first 20 items

--- bite-sized-bears/Website/syndication_app/rss_feed.py
+++ bite-sized-bears/Website/syndication_app/rss_feed.py
@@ -14,7 +14,7 @@ class LatestEntriesFeed(Feed):
         return Post.objects.filter(publisher__in=Community.objects
                                    .prefetch_related('subscribers')
                                    .filter(subscribers__name__exact=obj.name)
-                                   .values('id'))
+                                   .values('id')).order_by('-creation_date')[:20]
 
     def item_title(self, item):
         return item.title
@@ -25,16 +25,11 @@ class LatestEntriesFeed(Feed):
     def item_author_name(self, item):
         return item.author.name
 
-    def item_author_email(self):
-        return 'ww@ww.com'
-
     def item_author_link(self, item):
-        print("http://localhost:8000/" + item.author.get_absolute_url())
         return item.author.get_absolute_url()
 
     def item_pubdate(self, item):
         return item.creation_date
 
     def item_categories(self, item):
-        print(item.publisher.name)
         return [item.publisher.name]
