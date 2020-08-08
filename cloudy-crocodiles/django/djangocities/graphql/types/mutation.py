b41d3e66690c5f039e011fae15c859aee14136a5
Ansh Saini <ansh@lestransformations.com>
2020-08-08 23:21:10 +0530

    feat: add create page mutation

--- cloudy-crocodiles/django/djangocities/graphql/types/mutation.py
+++ cloudy-crocodiles/django/djangocities/graphql/types/mutation.py
@@ -1,4 +1,5 @@
 from ariadne import MutationType
+from djangocities.pages import mutation as page_mutations
 from djangocities.user import mutation as user_mutations
 
 mutation = MutationType()
@@ -7,3 +8,6 @@ mutation = MutationType()
 mutation.set_field("login", user_mutations.resolve_login)
 mutation.set_field("register", user_mutations.resolve_register)
 
+# Page Mutations
+mutation.set_field("createPage", page_mutations.resolve_create_page)
+# mutation.set_field("updatePage", page_mutations.resolve_update_page)
--- cloudy-crocodiles/django/djangocities/pages/mutation.py
+++ cloudy-crocodiles/django/djangocities/pages/mutation.py
@@ -6,12 +6,40 @@ from djangocities.sites.models import Site
 
 
 def resolve_create_page(_, info, data):
-    # request = info.context["request"]
-    # user = load_user(info)
-    # if not user.is_authenticated:
-    #     raise Exception("You can't do that!")
+    request = info.context["request"]
+    user = load_user(info)
+    if not user.is_authenticated:
+        raise Exception("You can't do that!")
 
-    pass
+    site = data.get("site", None)
+    version = data.get("version", None)
+    content = data.get("content", None)
+    file_name = data.get("file_name", None)
+
+    if not site:
+        logging.debug("Site is missing")
+        raise Exception("Site is missing")
+    if not version:
+        logging.debug("Version is missing")
+        raise Exception("Version is missing")
+    if not content:
+        logging.debug("Content is missing")
+        raise Exception("Content is missing")
+    if not file_name:
+        logging.debug("File Name is missing")
+        raise Exception("File Name is missing")
+
+    try:
+        site_obj = Site.objects.get(id=site, user=user)
+    except Site.DoesNotExist:
+        logging.debug("Site not found")
+        raise Exception("Site not found")
+
+    page = Page.objects.create(
+        site=site_obj, version=version, content=content, file_name=file_name
+    )
+
+    return page
 
 
 def resolve_update_page(_, info, data):
