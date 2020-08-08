05c7464429f67935644a9817b9152ca79f7f8a48
MushroomMaula <maxrd79@gmail.com>
2020-08-08 19:42:41 +0200

    Fix default value

--- concerned-coyotes/earlyinternet/weather/models.py
+++ concerned-coyotes/earlyinternet/weather/models.py
@@ -1,5 +1,4 @@
-from datetime import date
-
+from django.utils import timezone
 from django.db import models
 
 
@@ -10,4 +9,4 @@ class Weather(models.Model):
     city = models.CharField(max_length=512)
     sunrise = models.DateTimeField()
     sunset = models.DateTimeField()
-    day = models.DateField(default=date.today())
+    day = models.DateField(default=timezone.now)
