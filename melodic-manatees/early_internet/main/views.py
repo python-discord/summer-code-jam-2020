ddd3bce9e23ee9d977f97a70534e0b21ad16ca88
aedwardg <aedwardg1@gmail.com>
2020-08-07 23:53:05 -0700

    changed dashboard.html

--- melodic-manatees/early_internet/main/views.py
+++ melodic-manatees/early_internet/main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from users.models import UserPreferences
+from news.views import news_feed
 
 
 def main(request):
     if request.user.is_authenticated:
+        # MAYBE MOVE NEWS VIEW INTO HERE
+        news_data = news_feed(request)
         context = {
             'pref': UserPreferences.objects.get(user=request.user),
+            'news': news_data
         }
         return render(request, 'main/dashboard.html', context)
     return render(request, 'main/dashboard.html')
