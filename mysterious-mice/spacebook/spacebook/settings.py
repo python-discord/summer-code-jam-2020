db5416f356958ef24b186558afed14ba643af24b
Kenny <kfonatoko@gmail.com>
2020-08-08 02:10:01 -0400

    Fix static files in Docker

--- mysterious-mice/spacebook/spacebook/settings.py
+++ mysterious-mice/spacebook/spacebook/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+    # "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
