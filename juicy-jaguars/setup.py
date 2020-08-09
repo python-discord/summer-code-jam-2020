469d8bab5701e22c86ffaf019616add335272814
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2020-08-09 18:54:40 +0100

    Linting

--- juicy-jaguars/setup.py
+++ juicy-jaguars/setup.py
@@ -22,36 +22,36 @@ def install(color=False):
     else:
         green, yellow, red, reset = "", "", "", ""
 
-    print(yellow+"Cloning Repository..."+reset)
+    print(yellow+"Cloning Repository..." + reset)
     result = subprocess.call(["git", "clone", "https://github.com/Juicy-Jaguars/summer-code-jam-2020"])
     os.chdir("summer-code-jam-2020/juicy-jaguars")
 
     if result != 0:  # Checks the returned error code. 0=OK !0=Not OK
-        print(red+"Error cloning Repository. Exiting...")
+        print(red + "Error cloning Repository. Exiting...")
         sys.exit()
 
-    print(green+"Repository successfully cloned."+reset)
+    print(green + "Repository successfully cloned." + reset)
     print()
-    print(yellow+"Installing dependencies..."+reset)
+    print(yellow + "Installing dependencies..." + reset)
     result = subprocess.call(["pip3", "install", "-r", "requirements.txt"])
 
     if result != 0:  # Checks the returned error code. 0=OK !0=Not OK
-        print(red+"Error installing Dependencies. Exiting...")
+        print(red + "Error installing Dependencies. Exiting...")
         sys.exit()
 
-    print(green+"Dependencies successfully installed."+reset)
+    print(green + "Dependencies successfully installed." + reset)
     print()
-    print(yellow+"Making migrations..."+reset)
+    print(yellow + "Making migrations..." + reset)
     result = subprocess.call(["python", os.path.join("Web95", "manage.py"), "migrate"])
 
     if result != 0:  # Checks the returned error code. 0=OK !0=Not OK
-        print(red+"Error making migrations. Exiting...")
+        print(red + "Error making migrations. Exiting...")
         sys.exit()
 
-    print(green+"Made migrations successfully."+reset)
+    print(green + "Made migrations successfully." + reset)
     print()
-    print(green+"Successfully installed. Run with 'python Web95/manage.py runserver' from summer-code-jam-2020/\
-        juicy-jaguars/"+reset)
+    print(green + "Successfully installed. Run with 'python Web95/manage.py runserver' from summer-code-jam-2020/\
+        juicy-jaguars/" + reset)
 
 
 if __name__ == "__main__":
