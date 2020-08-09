2f404d0dd31c8a01b04e89e3ac90c34830b9aba4
Oneirocritica <maxxkiller@hotmail.com>
2020-08-09 17:47:11 +0200

    clean up list view

--- fancy-falcons/fancy_falcons-proj/earls/views.py
+++ fancy-falcons/fancy_falcons-proj/earls/views.py
@@ -5,14 +5,8 @@ def earl_list_view(request):
     queryset = Account.objects.all()
     context = {
         "earl_list": queryset,
+        "users": queryset.filter(is_user=True),
+        "ficthist": queryset.filter(is_user=False),
         "active_page": "browse",
     }
     return render(request, "earls/earllist.html", context)
-
-def earl_grid_view(request):
-    queryset = Account.objects.all()
-    context = {
-        "earl_list": queryset,
-        "active_page": "browse",
-    }
-    return render(request, "earls/earlgrid.html", context)
