3fde37be6e24345452e5fa5c09842b1b2593899e
Sadiq <mail@sadiq-pasha.com>
2020-08-09 07:04:56 -0700

    fixed linting errors

--- melodic-manatees/early_internet/early_internet/urls.py
+++ melodic-manatees/early_internet/early_internet/urls.py
@@ -26,7 +26,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='users/login.html'
+            ),
+        name='login'
+    ),
     path('logout/', user_views.user_logout, name='logout'),
     path('profile/', user_views.profile, name='profile'),
     path('img2ascii/', include('img2ascii.urls')),
