59a01887a0de2c754813cac224d6a727a06154da
Oneirocritica <maxxkiller@hotmail.com>
2020-08-09 11:43:25 +0200

    added duplicate earldom test

--- fancy-falcons/fancy_falcons-proj/account/tests.py
+++ fancy-falcons/fancy_falcons-proj/account/tests.py
@@ -7,3 +7,6 @@ class QuestionModelTests(TestCase):
         accounts = Account.objects.all()
         earls = Account.objects.get(title='Earl')
         self.assertIs(accounts == earls, True)
+
+    def no_duplicate_earldoms(self):
+        self.assertEquals(Account.objects.all().values('earldom').annotate(Count('earldom')).count(), 1)
