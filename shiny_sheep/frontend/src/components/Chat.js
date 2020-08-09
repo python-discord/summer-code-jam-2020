5f86a230ba5e1ac872466711edded0605617b8bd
Yaro Kifor <yarokifor@fastmail.com>
2020-08-09 12:54:52 -0400

    Worked on chat components

--- shiny_sheep/frontend/src/components/Chat.js
+++ shiny_sheep/frontend/src/components/Chat.js
@@ -1,29 +1,16 @@
 import React, { Component } from "react";
 
-const chatSocket = new WebSocket(
-  "ws://" + window.location.host + "/ws/intro/"
-);
-
 class Chat extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     
-    this.state = { 
-      chatLogs: "",
-      input: ""
-    };
+   this.state = {
+      input: "" 
+    }
   }
   componentDidMount(){
-    chatSocket.onmessage = (event) => { 
-      const data = JSON.parse(event.data);
-      this.appendChatLogs(data.message);
-    };
-    chatSocket.onclose = (event) => {
-      this.appendChatLogs("Disconnected!");
-      console.error("Chat socket closed unexpectedly");
-    };
   }
   handleChange(event){
     this.setState({input: event.target.value});
@@ -34,17 +21,16 @@ class Chat extends Component {
     const data = JSON.stringify({
       'message': this.state.input
     });
-    chatSocket.send(data);
+    this.props.chat.websocket.send(data);
     this.setState({ input: "" });
-  }
-  appendChatLogs(message){
-    this.setState({ chatLogs: this.state.chatLogs + message + '\n'})
   }
 
   render() {
     return (
       <div>
-        <textarea value={this.state.chatLogs} id="chat-log" cols="100" rows="20"></textarea>
+        <div>
+          {this.props.chat.chatLogs}
+        </div>
         <br />
         <form onSubmit={this.handleSubmit}>
           <input value={this.state.input} onChange={this.handleChange} id="chat-message-input" type="text" size="100" autocomplete="off" />
--- shiny_sheep/frontend/src/components/Home.js
+++ shiny_sheep/frontend/src/components/Home.js
@@ -1,42 +1,104 @@
-import React, { useEffect, useState, useCallback, Component } from "react";
-import { Tabs, Tab, TabBody } from "react95";
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  Component,
+  createContext,
+} from "react";
+import {
+  Tabs,
+  Tab,
+  TabBody,
+  Window,
+  WindowHeader,
+  WindowContent,
+} from "react95";
 import Chat from "./Chat";
 import "./Home.css";
-
-const HOME = 0;
-const LOBBY = 1;
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.onTabChange = this.onTabChange.bind(this);
     this.state = {
-      activeTab: HOME,
+      activeChat: "#intro",
+      chats: undefined,
     };
   }
-  onTabChange(event, value){
-    this.setState({activeTab: value});
+  updateRoom(roomName, chatRoom) {
+    const chats = { ...this.state.chats };
+    chats[roomName] = chatRoom;
+    this.setState({
+      ...this.state,
+      chats: chats,
+    });
+  }
+  setupWebsockets(chatWebSocket, roomName, chatRoom) {
+    chatWebSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      chatRoom.chatLogs.push((<span>User: {data.message}</span>), (<br/>));
+      this.updateRoom(roomName, chatRoom);
+    };
+    chatWebSocket.onclose = (event) => {
+      chatRoom.chatLogs.push(
+        (<span class="websocketMessage">WebSocket: Disconnected!</span>), (<br/>)
+      );
+      this.updateRoom(roomName, chatRoom);
+    };
+  }
+
+  joinChatRoom(roomName) {
+    // TODO: Check if chatRoom exists
+    const chatWebSocket = new WebSocket(
+      "ws://" + window.location.host + "/ws/chat/" + roomName + "/"
+    );
+    const chatRoom = {
+      websocket: chatWebsocket,
+      chatLogs: [],
+    };
+    this.updateRoom(roomName, chatRoom);
+    this.setupWebsockets(chatWebSocket, roomName, chatRoom);
+  }
+  componentDidMount() {
+    const chatWebSocket = new WebSocket(
+      "ws://" + window.location.host + "/ws/intro/"
+    );
+    const chatRoom = {
+      websocket: chatWebSocket,
+      chatLogs: [],
+    };
+    this.updateRoom("#intro", chatRoom);
+    this.setupWebsockets(chatWebSocket, "#intro", chatRoom);
+  }
+  onTabChange(event, value) {
+    this.setState({ ...this.state, activeTab: value });
+  }
+  componentWillUnmount() {
+    for (const [roomName, chatRoom] of Object.entries(this.state.chats)) {
+      chatRoom.websocket.close();
+    }
   }
   render() {
+    const chatTabs = [];
+    if (this.state.chats !== undefined) {
+      for (let chat of Object.keys(this.state.chats)) {
+        chatTabs.push(<Tab key={chat} value={chat}>{chat}</Tab>);
+      }
+    }
     return (
-      <div>
-        <Tabs value={this.state.activeTab} onChange={this.onTabChange}>
-          <Tab value={HOME}>Home</Tab>
-          <Tab value={LOBBY}>#Lobby</Tab>
-        </Tabs>
-        <TabBody>
-          {this.state.activeTab === HOME && <Chat />}
-          {this.state.activeTab === LOBBY && <div>Test</div>}
-        </TabBody>
-        <div className="main-Content">
-          <div class="row mt-5">
-            <div class="col offset-md-3"></div>
-            <div class="col">
-              <p>List goes here</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Window>
+        <WindowHeader>IRC</WindowHeader>
+        <WindowContent>
+          <Tabs value={this.state.activeChat} onChange={this.onTabChange}>
+            {chatTabs}
+          </Tabs>
+          <TabBody>
+            {this.state.chats !== undefined && (
+              <Chat chat={this.state.chats[this.state.activeChat]} />
+            )}
+          </TabBody>
+        </WindowContent>
+      </Window>
     );
   }
 }
