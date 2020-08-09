99d5ef4e5a2e3a920c7692fb85c9fd0d230c81f6
Darren Kam <darrenmkam4150@gmail.com>
2020-08-09 03:35:04 -1000

    Add string representation to WikiArticle models

--- nimble-newts/askgrieves/chatbot/models.py
+++ nimble-newts/askgrieves/chatbot/models.py
@@ -12,3 +12,6 @@ class WikiArticle(models.Model):
     name = models.CharField(max_length=500)
     summary = models.TextField(max_length=100)
     full_page = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"name={self.name} summary={self.summary}"
