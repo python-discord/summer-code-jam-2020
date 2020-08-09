3eaba10c8df9b319e456906ee7e8c790abaf6993
MushroomMaula <maxrd79@gmail.com>
2020-08-09 13:15:38 +0200

    Moved to commands\management\commands\setup_tasks.py

--- concerned-coyotes/earlyinternet/weather/apps.py
+++ concerned-coyotes/earlyinternet/weather/apps.py
@@ -3,15 +3,3 @@ from django.apps import AppConfig
 
 class WeatherConfig(AppConfig):
     name = 'weather'
-
-    def ready(self):
-        from django.conf import settings
-        if settings.TESTING:
-            return
-
-        from django_q.models import Schedule
-        Schedule.objects.get_or_create(
-            func='weather.tasks.fetch_weather',
-            schedule_type='D',
-            repeats=-1
-        )
