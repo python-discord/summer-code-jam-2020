6eb54e43a6c2906bdc020a012f2640ccda657805
kkawabat <kakwabat@asu.edu>
2020-08-08 21:01:57 -0700

    updated trivia_quiz form aestetics

--- practical-penguins/trivia_tavern/trivia_tavern/settings.py
+++ practical-penguins/trivia_tavern/trivia_tavern/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'twilio_messenger.apps.TwilioMessengerConfig',
     'crispy_forms',
+    'jquery',
+    'djangoformsetjs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
