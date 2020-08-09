21f9045ad91ab4cbe20a91b0fa1d96e678651db1
Artem Potemkin <avpotemkin@icloud.com>
2020-08-09 14:16:48 +0300

    Work in progress

--- dreamy-dingos/chat/urls.py
+++ dreamy-dingos/chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('rooms/', views.all_rooms, name="rooms"),
     path('rooms/create/', views.create_room, name="create_room"),
     # path('<str:room_name>/', views.room, name="room"),
--- dreamy-dingos/chat/views.py
+++ dreamy-dingos/chat/views.py
@@ -2,12 +2,24 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Room
+from .models import SimpleUser
 from .forms import RoomNameForm
+from .forms import LoginForm
 from .services import MessageService, RoomService
 
 def index(request):
     return render(request, "chat/index.html", context={"rooms": Room.objects.all()})
   
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            SimpleUser(username=form.cleaned_data).save()
+            return HttpResponseRedirect('/chat/login/')
+    else:
+        form = LoginForm()
+    return render(request, 'chat/login.html', {'form': form})
+
 def room(request, room_id: int):
     # If room exists then give it and its messages to context
     room_from_db = RoomService.get_room_by_id(room_id)
