4f2b1edd119988223d13fc611b1d63644f0cf882
CodeForeverAndEver <reed.manning06>
2020-08-09 11:19:52 -0400

    Updated Docker - Fully Working

--- talented-tigers/spydirweb/settings.py
+++ talented-tigers/spydirweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "glda-7eb6wkd!c+^9bcc_+ogka%zox%6uoxd*_9vi3q1hjks(k"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
