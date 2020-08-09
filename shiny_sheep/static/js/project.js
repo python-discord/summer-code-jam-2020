fba5b95e87e8c01c50275daed14664ae376f7c08
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-08-09 13:38:44 -0400

    Added menu stuff for extra pages

--- shiny_sheep/static/js/project.js
+++ shiny_sheep/static/js/project.js
@@ -1,4 +1,18 @@
 /* Project specific Javascript goes here. */
+
+logoutUrl = "/accounts/logout/";
+changePwd = "/accounts/password/change/"
+
+
+function changeToIfNotEqual(url) {
+    if ( window.location.href.endsWith(url)) {
+         alert("Already on that page!");
+
+    } else {
+       window.location.href = url;
+    }
+}
+
 function goBack() {
   window.history.back();
 }
