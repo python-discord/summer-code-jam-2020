dbfbecd4a2048c02dbd9db475b72a0e532738c0b
ryuga-hideki <vaishnav2353@gmail.com>
2020-08-10 00:07:15 +0530

    chat frontend

--- bite-sized-bears/Website/irc_chat/views.py
+++ bite-sized-bears/Website/irc_chat/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils.safestring import mark_safe
 import json
+
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username': mark_safe(json.dumps(request.user.username)),
+        'room_title': room_name,
     })
