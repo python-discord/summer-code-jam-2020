455780c6e6a4c42fe0164f2b384edb278e959e46
Braindead <albertopoljak@gmail.com>
2020-08-09 06:57:35 +0200

    Reduced height of comment box.

--- imaginary-iguanas/code_jam/myplace/forms.py
+++ imaginary-iguanas/code_jam/myplace/forms.py
@@ -8,9 +8,11 @@ class ProfileCommentForm(forms.ModelForm):
     class Meta:
         model = _ProfileComment
         fields = ['content']
+        widgets = {'content': forms.Textarea(attrs={'rows': 4})}
 
 
 class BlogCommentForm(forms.ModelForm):
     class Meta:
         model = _BlogComment
         fields = ['content']
+        widgets = {'content': forms.Textarea(attrs={'rows': 4})}
