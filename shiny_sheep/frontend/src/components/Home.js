4e9da811126a2f8dc0832cfe7e009618a9198b9c
Yaro Kifor <yarokifor@fastmail.com>
2020-08-09 00:16:14 -0400

    Fixed tabing

--- shiny_sheep/frontend/src/components/Home.js
+++ shiny_sheep/frontend/src/components/Home.js
@@ -1,33 +1,43 @@
-import React, {useEffect, useState, useCallback} from 'react';
-import {Tabs,Tab } from '@react95/core';
+import React, { useEffect, useState, useCallback, Component } from "react";
+import { Tabs, Tab, TabBody } from "react95";
 import Chat from "./Chat";
 import "./Home.css";
 
+const HOME = 0;
+const LOBBY = 1;
 
-const Home = () =>{
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.onTabChange = this.onTabChange.bind(this);
+    this.state = {
+      activeTab: HOME,
+    };
+  }
+  onTabChange(event, value){
+    this.setState({activeTab: value});
+  }
+  render() {
     return (
-        <div>
-            <Tabs active Tab="Home" >
-                <Tab title = "Home">
-                    <div className ="main-Content">
-                       Placeholder
-                    </div>
-                </Tab>
-                <Tab title ="#lobby">
-                    <div className = "main-Content">
-                        <div class="row mt-5">
-                            <div class="col offset-md-3">
-                                <Chat/>
-                            </div>
-                            <div class="col">
-                                <p>List goes here</p>
-                            </div>
-                        </div>
-                    </div>
-                </Tab>
-            </Tabs>
+      <div>
+        <Tabs value={this.state.activeTab} onChange={this.onTabChange}>
+          <Tab value={HOME}>Home</Tab>
+          <Tab value={LOBBY}>#Lobby</Tab>
+        </Tabs>
+        <TabBody>
+          {this.state.activeTab === HOME && <Chat />}
+          {this.state.activeTab === LOBBY && <div>Test</div>}
+        </TabBody>
+        <div className="main-Content">
+          <div class="row mt-5">
+            <div class="col offset-md-3"></div>
+            <div class="col">
+              <p>List goes here</p>
+            </div>
+          </div>
         </div>
+      </div>
     );
+  }
 }
-
 export default Home;
