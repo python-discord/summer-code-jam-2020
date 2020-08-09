8a5dfaa313b99422a35a56612fda759b8230dc70
cswil <cswilson@zoho.com>
2020-08-09 16:02:30 +0100

    ok

--- graceful-grasshoppers/PySpace/PySpace/settings.py
+++ graceful-grasshoppers/PySpace/PySpace/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'corsheaders',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -60,7 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.common.CommonMiddleware', 
 ]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'PySpace.urls'
 
