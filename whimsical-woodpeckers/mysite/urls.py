5bb4d4a171c98e3e8baea6be1fabed98818ba5c5
Evan <eokeefe3@wisc.edu>
2020-08-09 11:43:54 -0400

    Removed extraneous code + flake8 linting

--- whimsical-woodpeckers/mysite/urls.py
+++ whimsical-woodpeckers/mysite/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path #, include
 from django.conf import settings
 from django.conf.urls.static import static
 # from users import views as user_views
@@ -24,16 +23,6 @@ from anon import views as anon_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('test/', user_views.test, name='test'),
-    # path('register/', user_views.register, name='register'),
-    # path('profile/', user_views.profile, name='profile'),
-    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    # path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    # path('', include('chat.urls')),
     path('anonuser/', anon_views.test),
     path('auth/', anon_views.get_auth_token, name='get-auth'),
     path('data/', anon_views.get_user_data, name='get-data')
