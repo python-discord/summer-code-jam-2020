029bf551ae9e04918a7bba98d3c08086c1004299
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-08-09 12:31:45 -0400

    Revert "Register endpoint at /api/register/"
    This reverts commit cd3b1f55d8b59a4b912fb5b15cff6a238d448f4c.

--- config/api_router.py
+++ config/api_router.py
@@ -1,19 +1,15 @@
 from django.conf import settings
-from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from shiny_sheep.users.api.views import CreateUserView, UserViewSet
+from shiny_sheep.users.api.views import UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-router.register('users', UserViewSet)
+router.register("users", UserViewSet)
 
 
 app_name = "api"
-urlpatterns = [
-    path('register/', CreateUserView.as_view())
-]
-urlpatterns += router.urls
+urlpatterns = router.urls
--- shiny_sheep/users/api/serializers.py
+++ shiny_sheep/users/api/serializers.py
@@ -5,21 +5,10 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ["username", "email", "name", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
