e4f8ea30937bf5b3ff004aa800dcaeee2e2f3c64
Ronit-j <pictronit007@gmail.com>
2020-08-09 15:12:49 +0530

    Add docker setup for app

--- bite-sized-bears/Website/rss_it/settings.py
+++ bite-sized-bears/Website/rss_it/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 
 # Application definition
