74abe25047da257e5adf2bb27c39e457f5e9d5ba
nkesedre <petar.janastasievski@gmail.com>
2020-08-09 13:33:43 +0200

    cleaned up the event edit page

--- fancy-falcons/fancy_falcons-proj/earlcal/forms.py
+++ fancy-falcons/fancy_falcons-proj/earlcal/forms.py
@@ -14,3 +14,4 @@ class EventForm(ModelForm):
     super(EventForm, self).__init__(*args, **kwargs)
     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M:%S',)
     self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M:%S',)
+    self.fields['desc'].label = "Description"
