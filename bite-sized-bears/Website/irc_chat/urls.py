651ab3525674c57c864384e18ceaf062d79fb72b
ryuga-hideki <vaishnav2353@gmail.com>
2020-08-09 23:35:03 +0530

    chat feature commit

--- bite-sized-bears/Website/irc_chat/urls.py
+++ bite-sized-bears/Website/irc_chat/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<room_name>\w+)/', views.room, name='chat'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
--- bite-sized-bears/Website/irc_chat/views.py
+++ bite-sized-bears/Website/irc_chat/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.utils.safestring import mark_safe
+import json
 
 @login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
     })
