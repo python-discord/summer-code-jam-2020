f9e2ba4e030415ba0a659f65d0c2cfcc9860fa7f
Terje Espedal <terje.m.espedal@gmail.com>
2020-08-09 15:26:10 +0200

    add app

--- lumpy-llamas/lammas/settings.py
+++ lumpy-llamas/lammas/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'forum.apps.ForumConfig',
+    'mailmessages.apps.MailmessagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
