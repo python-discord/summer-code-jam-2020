4e8c07c94bcbe3563e7892dd158c36cd1c1cf38d
Braindead <albertopoljak@gmail.com>
2020-08-09 03:50:51 +0200

    Better age validator.
    Now has error message for dates in future.
    Bette typehints.

--- imaginary-iguanas/code_jam/users/validators.py
+++ imaginary-iguanas/code_jam/users/validators.py
@@ -7,12 +7,13 @@ def calculate_age(born: date):
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 
-def valid_age_validator(arg):
+def valid_age_validator(born: date):
     min_age = 13
     max_age = 100
-
-    age = calculate_age(arg)
-    if age <= min_age:
+    age = calculate_age(born)
+    if age < 0:
+        raise ValidationError(f'You cannot use date from future.')
+    elif age <= min_age:
         raise ValidationError(f'You have to be older than {min_age}.')
     elif age > max_age:
         raise ValidationError('Please provide a valid age.')
