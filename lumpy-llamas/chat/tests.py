813b987df27eb9bf707c17780ef1e1e381801173
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-08-09 13:37:17 +0100

    Add test for valid room names for chat app

--- lumpy-llamas/chat/tests.py
+++ lumpy-llamas/chat/tests.py
@@ -30,3 +30,29 @@ class ChatLobbyTest(TestCase):
                     json.loads(res.content.decode()),
                     content,
                 )
+
+    @patch('chat.views.check_chat_room_name')
+    def test_valid_names(self, data):
+        User.objects.create_user('fake', password='test')
+
+        client = Client()
+        client.login(username='fake', password='test')
+
+        tests = [
+            ('myroom', 200, {"valid": True, "message": "Ok"}),
+            ('myRoom', 200, {"valid": True, "message": "Ok"}),
+            ('123', 200, {"valid": True, "message": "Ok"}),
+            ('HowRU111', 200, {"valid": True, "message": "Ok"})
+        ]
+
+        for (valid_name, expected_status_code, content) in tests:
+            res = client.post('/api/chat/checkname/', json.dumps({
+                'roomName': valid_name
+            }), content_type='application/json')
+
+            self.assertEqual(res.status_code, expected_status_code)
+            if content:
+                self.assertEqual(
+                    json.loads(res.content.decode()),
+                    content,
+                )
