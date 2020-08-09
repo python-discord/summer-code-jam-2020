0312e57f10fa2ee647d362cc9ef44f5b60d9b7e0
Oneirocritica <maxxkiller@hotmail.com>
2020-08-09 18:21:24 +0200

    earl public pages

--- fancy-falcons/fancy_falcons-proj/earls/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from django.apps import apps
-from PIL import Image
-from account.models import Account
--- fancy-falcons/fancy_falcons-proj/earls/urls.py
+++ fancy-falcons/fancy_falcons-proj/earls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.earl_list_view, name='earl-list'),
+    path('<int:pk>/', views.earl_public_page, name='earldetail'),
 ]
