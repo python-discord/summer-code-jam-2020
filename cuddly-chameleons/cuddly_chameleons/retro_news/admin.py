58c801bfd96b354c4926987f22bf0bd42a0f36a3
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-09 14:28:15 +0300

    Implement comments feature to backend (Django)

--- cuddly-chameleons/cuddly_chameleons/retro_news/admin.py
+++ cuddly-chameleons/cuddly_chameleons/retro_news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser, BlogArticle
+from .models import ArticleComment, CustomUser, BlogArticle
 
 admin.site.register(CustomUser)
 admin.site.register(BlogArticle)
+admin.site.register(ArticleComment)
