1b123ffa2370a8f9962a23183a71f47339e6d932
nkesedre <petar.janastasievski@gmail.com>
2020-08-09 19:53:25 +0200

    fix homepage birthdays

--- fancy-falcons/fancy_falcons-proj/fancy_falcons/views.py
+++ fancy-falcons/fancy_falcons-proj/fancy_falcons/views.py
@@ -6,7 +6,8 @@ from datetime import datetime
 def home(request):
     # Editing Earl of the Day ID should update all data on home page
     earl_of_the_day_id = 2
-    upcoming_birthdays = Account.objects.filter(birthday__gte=datetime.now()).order_by('birthday')[:3]
+    month = datetime.today().month
+    upcoming_birthdays = Account.objects.filter(birthday__month=month).order_by('birthday')
     context = {
         "earl_of_the_day": Account.objects.get(pk=earl_of_the_day_id),
         "upcoming": upcoming_birthdays,
