d104aeca43aff3eebc9306addcc6fc8d2af02938
Rob Harrigan <rob.harrigan89@gmail.com>
2020-08-09 01:23:58 -0500

    Fix 500 on duplicate titles

--- rubbery-raccoons/wired/author_tools/views.py
+++ rubbery-raccoons/wired/author_tools/views.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db import IntegrityError
 from django.shortcuts import get_object_or_404, render
 from django.utils.decorators import method_decorator
 from django.utils.text import slugify
@@ -22,9 +23,12 @@ def compose(request):
             art.slug = "-".join(
                 (slugify(x) for x in [art.author.username, art.title, art.publication_date])
             )
-            art.save()
-            messages.success(request, "Article submitted!")
-            form = ArticleForm()
+            try:
+                art.save()
+                messages.success(request, "Article submitted!")
+                form = ArticleForm()
+            except IntegrityError:
+                messages.error(request, "Invalid article slug, try changing the title")
         else:
             messages.error(request, "Form input was invalid.")
 
@@ -44,8 +48,11 @@ def update(request, slug):
             art.headline = form.cleaned_data.get("headline")
             art.body = form.cleaned_data.get("body")
             art.category = form.cleaned_data.get("category")
-            art.save()
-            messages.success(request, "Article updated!")
+            try:
+                art.save()
+                messages.success(request, "Article updated!")
+            except IntegrityError:
+                messages.error(request, "Invalid article slug, try changing the title")
     else:
         form = ArticleForm(instance=art)
     return render(request, "author_tools/update.html", {"form": form})
