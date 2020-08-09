ad60457a87c8be27a86bd70847334c8df329e6b5
Kenny <kfonatoko@gmail.com>
2020-08-09 12:56:41 -0400

    Remove page field from guestbook

--- mysterious-mice/spacebook/guestbook/models.py
+++ mysterious-mice/spacebook/guestbook/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Guestbook(models.Model):
-    page = models.TextField(default='home')
     author = models.CharField(max_length=100)
     text = models.TextField()
     email = models.EmailField()
