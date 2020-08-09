6ed4af6b53ff3fc65b18be04d811b321fb601cda
Ajay-Ratnam <ajayratnam.satheesh@gmail.com>
2020-08-09 15:28:01 +0530

    Added 127.0.0.1

--- bite-sized-bears/Website/rss_it/settings.py
+++ bite-sized-bears/Website/rss_it/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
