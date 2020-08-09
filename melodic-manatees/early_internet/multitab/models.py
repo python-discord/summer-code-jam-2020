f9dee7bb9462090c1d2005c94c54d4260951f4c4
Sadiq <mail@sadiq-pasha.com>
2020-08-09 03:13:10 -0700

    need to add js functions

--- melodic-manatees/early_internet/multitab/models.py
+++ melodic-manatees/early_internet/multitab/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import UserProfile
 
 
 class MultiTab(models.Model):
     multitab_owner = models.ForeignKey(
-        User,
+        UserProfile,
         on_delete=models.CASCADE,
     )
     title = models.CharField(
@@ -13,13 +13,17 @@ class MultiTab(models.Model):
     )
     tab_one = models.CharField(
         max_length=200,
-        default=''
+        default='',
+        blank=True
     )
     tab_two = models.CharField(
         max_length=200,
-        default=''
+        default='',
+        blank=True
+
     )
     tab_three = models.CharField(
         max_length=200,
-        default=''
+        default='',
+        blank=True
     )
--- melodic-manatees/early_internet/multitab/views.py
+++ melodic-manatees/early_internet/multitab/views.py
@@ -4,9 +4,54 @@ from django.forms import modelformset_factory
 from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def multitab_home(request):
-    multitabs = MultiTab.objects.filter()
-    return render(request, 'multitab/multitab.html')
+    multitabs = MultiTab.objects.filter(multitab_owner=request.user.profile)
+    if multitabs:
+        if len(multitabs) == 1:
+            title_1 = multitabs[0].title
+            site_1_1 = multitabs[0].tab_one
+            site_1_2 = multitabs[0].tab_two
+            site_1_3 = multitabs[0].tab_three
+            title_2 = 'Multitab'
+            site_2_1 = multitabs[1].tab_one
+            site_2_2 = multitabs[1].tab_two
+            site_2_3 = multitabs[1].tab_three
+        elif len(multitabs) == 2:
+            title_1 = multitabs[0].title
+            site_1_1 = multitabs[0].tab_one
+            site_1_2 = multitabs[0].tab_two
+            site_1_3 = multitabs[0].tab_three
+            title_2 = multitabs[1].title
+            site_2_1 = multitabs[1].tab_one
+            site_2_2 = multitabs[1].tab_two
+            site_2_3 = multitabs[1].tab_three
+    else:
+        title_1 = 'Multitab'
+        site_1_1 = ''
+        site_1_2 = ''
+        site_1_3 = ''
+        title_2 = 'Multitab'
+        site_2_1 = ''
+        site_2_2 = ''
+        site_2_3 = ''
+    number_of_multitabs = list('_' for x in range(len(multitabs)))
+    print(number_of_multitabs)
+    return render(request,
+        'multitab/multitab.html',
+        {
+            'title_1': title_1,
+            'site_1_1': site_1_1,
+            'site_1_2': site_1_2,
+            'site_1_3': site_1_3,
+            'title_2': title_2,
+            'site_2_1': site_2_1,
+            'site_2_2': site_2_2,
+            'site_2_3': site_2_3,
+            'number_of_multitabs': number_of_multitabs
+        }
+        )
+        
 
 
 @login_required(login_url='login')
@@ -19,19 +64,19 @@ def add_multitab(request):
             'tab_two',
             'tab_three'
             ),
-        extra=4,
-        max_num=4
+        extra=2,
+        max_num=2,
         )
     if request.method == 'POST':
-        formset = multitab_form(request.POST, queryset=MultiTab.objects.filter(multitab_owner=request.user))
+        formset = multitab_form(request.POST, queryset=MultiTab.objects.filter(multitab_owner=request.user.profile))
         if formset.is_valid():
             instances = formset.save(commit=False)
             for instance in instances:
-                instance.multitab_owner = request.user
+                instance.multitab_owner = request.user.profile
                 instance.save()
             return redirect('multitab-home')
     else:
-        formset = multitab_form()
+        formset = multitab_form(queryset=MultiTab.objects.filter(multitab_owner=request.user.profile))
     return render(
         request,
         'multitab/edit_multitab.html',
