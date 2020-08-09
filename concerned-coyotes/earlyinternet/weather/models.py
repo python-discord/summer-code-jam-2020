2fe7ad76f54e60ed64b99a09e9de1fe9222347e3
MushroomMaula <maxrd79@gmail.com>
2020-08-09 13:33:44 +0200

    Add Weather manager
    This should make querying the weather at a give location easier

--- concerned-coyotes/earlyinternet/weather/models.py
+++ concerned-coyotes/earlyinternet/weather/models.py
@@ -2,7 +2,17 @@ from django.utils import timezone
 from django.db import models
 
 
+class WeatherManager(models.Manager):
+
+    def get_weather_at(self, lat, lon):
+        return super().get_queryset().filter(
+            location__lat=lat, location__lon=lon
+        ).order_by("-day").first()
+
+
 class Weather(models.Model):
+
+    objects = WeatherManager()
 
     celsius = models.FloatField()
     fahrenheit = models.FloatField()
