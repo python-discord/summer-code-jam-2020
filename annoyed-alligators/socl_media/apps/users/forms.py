7d8cb9e071fcb30fd52aae2c0d581623b69d831d
Aksh Gupta <akshgpt7@gmail.com>
2020-08-09 09:00:49 +0530

    Extend first_name and last_name to UserUpdateForm

--- annoyed-alligators/socl_media/apps/users/forms.py
+++ annoyed-alligators/socl_media/apps/users/forms.py
@@ -18,7 +18,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
 class ProfileUpdateForm(forms.ModelForm):
