8a859da7e4527e2401b7c3b3345622b18e3fb946
Darren Kam <darrenmkam4150@gmail.com>
2020-08-09 03:36:28 -1000

    Fix the wikipedia endpoint

--- nimble-newts/askgrieves/askgrieves/urls.py
+++ nimble-newts/askgrieves/askgrieves/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('chat/', views.chat_page, name='chat'),
-    # path('exp'), views.exper
+    path('getwiki/', views.get_wikipedia)
 ]
--- nimble-newts/askgrieves/chatbot/views.py
+++ nimble-newts/askgrieves/chatbot/views.py
@@ -1,6 +1,10 @@
-import wikipediaapi
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms.models import model_to_dict
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from .models import WikiArticle
+
+from wikipediaapi import Wikipedia
 
 
 class Message:
@@ -14,36 +18,31 @@ class Message:
         Message.send = not self.send
         self.text = text
 
-# Create your views here.
-
 
 def home(request):
-    return render(
-        request, 'home.html'
-    )
+    return render(request, 'home.html')
 
 
 def chat_page(request, bot_name):
-    return render(
-        request, 'home.html', {'bot': bot_name}
-    )
+    return render(request, 'home.html', {'bot': bot_name})
 
 
 def experimental(request):
-    return render(
-        request, 'landing_page.html'
-    )
+    return render(request, 'landing_page.html')
 
 
 def get_wikipedia(request):
     article_name = request.POST.get('article_name')
-    if article := WikiArticle.objects.filter(name=article_name):
-        # return article
-        pass
-    else:
-        wiki_wiki = wikipediaapi.Wikipedia('en')
+    try:
+        article = WikiArticle.objects.get(name=article_name)
+        serialied_article = model_to_dict(article, fields=('name', 'summary'))
+    except ObjectDoesNotExist:
+        wiki_wiki = Wikipedia('en')
         wiki_page = wiki_wiki.page(article_name)
-        new_article = WikiArticle(article_name, wiki_wiki.extracts(wiki_page, excsentences=3), wiki_page.text)
-        new_article.save()
-        # return article
-        pass
+        if not wiki_page.exists():
+            raise Http404('Wiki page does not exist')
+        new_article = WikiArticle.objects.create(name=article_name,
+                                                 summary=wiki_wiki.extracts(wiki_page, excsentences=3),
+                                                 full_page=wiki_page.text)
+        serialied_article = model_to_dict(new_article, fields=('name', 'summary'))
+    return JsonResponse(serialied_article)
