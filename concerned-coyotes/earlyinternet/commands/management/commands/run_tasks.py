a44bee738769bffa5f98420c6886f15afeb6d727
Jeremy <jeremyregitz@gmail.com>
2020-08-09 16:08:37 +0200

    remove while loop
    Removed a while loop

--- concerned-coyotes/earlyinternet/commands/management/commands/run_tasks.py
+++ concerned-coyotes/earlyinternet/commands/management/commands/run_tasks.py
@@ -10,15 +10,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         tasks = Schedule.objects.all()
-        while True:
-            for i, task in enumerate(tasks, start=1):
-                task: Schedule
 
-                module, func_name = task.func.rsplit(".", 1)
-                module_obj = importlib.import_module(module)
-                func = getattr(module_obj, func_name)
-                print(f"Now running {task.func} {i}/{len(tasks)}")
-                start = time.time()
-                func()
-                end = time.time()
-                print(f"Finished the task in {end-start:.2f} seconds")
+        for i, task in enumerate(tasks, start=1):
+            task: Schedule
+
+            module, func_name = task.func.rsplit(".", 1)
+            module_obj = importlib.import_module(module)
+            func = getattr(module_obj, func_name)
+            print(f"Now running {task.func} {i}/{len(tasks)}")
+            start = time.time()
+            func()
+            end = time.time()
+            print(f"Finished the task in {end-start:.2f} seconds")
