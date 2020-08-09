abcf9d88c8b73453be8a61ffb4195ad8fd70314e
Dino Angelov <dino@dangelov.com>
2020-08-09 10:38:29 +0200

    static homepage + quincy svg

--- fancy-falcons/fancy_falcons-proj/fancy_falcons/settings.py
+++ fancy-falcons/fancy_falcons-proj/fancy_falcons/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
