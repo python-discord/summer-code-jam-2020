5e44aa4234e2893f661bd2aa99f45f6852397ac0
AnixDrone <tomislavignjatov@rocketmail.com>
2020-08-09 17:12:57 +0200

    front end is 75% done

--- festive-ferrets/frontend/code-jam/src/app/app-routing.module.ts
+++ festive-ferrets/frontend/code-jam/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import {MainSiteComponent} from './main-site/main-site.component';
 import {BoardViewComponent} from './board-view/board-view.component';
 import {PostViewComponent} from './post-view/post-view.component';
 import {CreatePostComponent} from './create-post/create-post.component';
+import {BoardComponent} from './board/board.component';
 
 const routes: Routes = [
   {path: 'main-page', component: MainSiteComponent},
   {path: '', redirectTo: 'main-page', pathMatch: 'full'},
   {path: 'main-page/:boardId', component: BoardViewComponent},
-  {path: 'main-page/:boardId/create-post' , component:CreatePostComponent},
-  {path: 'main-page/:boardId/:postId', component:PostViewComponent}
-
+  {path: 'main-page/:boardId/create-post', component: CreatePostComponent},
+  {path: 'main-page/:boardId/:postId', component: PostViewComponent},
+  {path: 'tic-tac-toe', component: BoardComponent}
 
 ];
 
