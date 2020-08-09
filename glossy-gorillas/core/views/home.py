0877006b16a96a6ac741484c08c85ae0a48c84ce
fliepeltje <donatas@rasiukevicius.dev>
2020-08-09 12:02:47 +0200

    Fix single quotes

--- glossy-gorillas/core/views/home.py
+++ glossy-gorillas/core/views/home.py
@@ -12,5 +12,5 @@ class Home(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['trades'] = Trade.objects.newest_10()
+        context["trades"] = Trade.objects.newest_10()
         return context
