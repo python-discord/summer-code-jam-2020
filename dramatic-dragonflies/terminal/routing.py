9571066f50400e79b9f5f4917607d536ed30ce41
lavirlifiliol <adam.hrbac@gmail.com>
2020-08-09 19:52:56 +0200

    ids are not numeric

--- dramatic-dragonflies/terminal/routing.py
+++ dramatic-dragonflies/terminal/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
 from . import consumers
 websocket_patterns = [
-    re_path(r"^ws/terminal/(?P<storage_id>\d+)/(?P<vm_id>\d+)$", consumers.TerminalConsumer)
+    re_path(r"^ws/terminal/(?P<storage_id>.+)/(?P<vm_id>.+)$", consumers.TerminalConsumer)
 ]
