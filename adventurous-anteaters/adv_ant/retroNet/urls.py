d29a308d313ba1f6540f18f714d41632a4109952
beefykenny <hoftkenny@gmail.com>
2020-08-09 10:32:15 -0300

    made post btn blue and retro in create_tweet

--- adventurous-anteaters/adv_ant/retroNet/urls.py
+++ adventurous-anteaters/adv_ant/retroNet/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', include("django.contrib.auth.urls")),
     path('post', views.createpost, name='post'),
     path('posted', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('updateprofile', views.update_profile, name='updateprofile'),
     path('updateprofiledisplay', views.update_profile_display, name='updateprofiledisplay'),
     path('profile', views.my_profile, name="my_profile"),
