751e2640ec9aa3470c712e378a60808b08a4ff81
Braindead <albertopoljak@gmail.com>
2020-08-09 05:01:50 +0200

    Urls in base.html menu should not be hardcoded.

--- imaginary-iguanas/code_jam/chat/urls.py
+++ imaginary-iguanas/code_jam/chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', views.index, name='chat-index'),
+    path('<str:room_name>/', views.room, name='chat-room'),
 ]
