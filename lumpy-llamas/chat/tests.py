ae4af0a9acf9079b043018eb24f303a998bfd520
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-08-09 13:25:44 +0100

    Adds test on /api/chat/checkname/ endpoint

--- lumpy-llamas/chat/tests.py
+++ lumpy-llamas/chat/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import json
+from unittest.mock import patch
+from django.test import TestCase, Client
+from chat.models import User
 
-# Create your tests here.
+
+class ChatLobbyTest(TestCase):
+
+    @patch('chat.views.check_chat_room_name')
+    def test_invalid_names(self, data):
+        User.objects.create_user('fake', password='test')
+
+        client = Client()
+        client.login(username='fake', password='test')
+
+        tests = [
+            ('', 400, None),
+            ('hello!', 200, {"valid": False, "message": "Invalid chat room name. Names must be alphanumeric."}),
+            ('fartoolonganame'*10, 400, None)
+        ]
+
+        for (invalid_name, expected_status_code, content) in tests:
+            res = client.post('/api/chat/checkname/', json.dumps({
+                'roomName': invalid_name
+            }), content_type='application/json')
+
+            self.assertEqual(res.status_code, expected_status_code)
+            if content:
+                self.assertEqual(
+                    json.loads(res.content.decode()),
+                    content,
+                )
