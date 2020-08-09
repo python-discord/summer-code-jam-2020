43f52548f51dc54ddaaf203c5920461c7729df14
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-08-09 19:38:09 +0200

    Add start_vm

--- dramatic-dragonflies/the_htvms/runner/constants.py
+++ dramatic-dragonflies/the_htvms/runner/constants.py
@@ -5,3 +5,18 @@ DISK_BLANK_SIZE = '50M'
 
 DISK_STAGING_PATH = '/var/htvms/disk_staging'
 DISK_ROM_PATH = '/rom'
+
+VM_MAX_RAM = 1073741824  # 1Gb
+
+MINIJAIL_VM_BASE_CONFIG = (
+    '-b', '/build/boot,/boot,0',  # Add ro boot folder
+    '-d',  # Base /dev folder
+    '-e',  # New network space
+    '-p',  # New PID namespace
+    '-P', '/var/empty',  # Pivot to empty chroot
+    '-R', f'RLIMIT_AS,{VM_MAX_RAM},{VM_MAX_RAM}',  # Limit memory
+    '-R', 'RLIMIT_NPROC,90,100',  # Limit processes
+    '-t',  # Mount tmpfs
+    '--uts=htvm',  # Hostname
+    '/bin/bash', '-c', 'bash'  # Hacky bash invocation
+)
--- dramatic-dragonflies/the_htvms/runner/vm.py
+++ dramatic-dragonflies/the_htvms/runner/vm.py
@@ -1,4 +1,7 @@
+from subprocess import run
+
 from the_htvms.runner.config import VMConfig
+from the_htvms.runner.constants import MINIJAIL_VM_BASE_CONFIG, DISK_PATH
 
 
 def start_vm(config: VMConfig) -> str:
@@ -7,7 +10,23 @@ def start_vm(config: VMConfig) -> str:
 
     The runner ID will be returned.
     """
-    raise NotImplementedError
+    arguments = list(MINIJAIL_VM_BASE_CONFIG)
+
+    # Add the rootfs
+    arguments = ['-k', f'{DISK_PATH}/{config.attached_disks[0]},/,ext2'] + arguments
+
+    # Add the disks to the VM
+    if len(config.attached_disks) > 1:
+        for i, disk in enumerate(config.attached_disks[1:]):
+            arguments.extend(
+                ('-b', f'{DISK_PATH}/{disk},/dev/fd{i}')
+            )
+
+    # Add the shell
+    arguments.append(f'/bin/{config.default_shell}')
+
+    # Start the VM
+    run(('../build/process_watch', 'minijail0', *arguments))
 
 
 def stop_vm(id_: str) -> bool:
